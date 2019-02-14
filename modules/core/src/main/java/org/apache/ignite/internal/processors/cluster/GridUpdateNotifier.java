a193f69a1dcb2c972b4cb485b32a728ae6ee659a
Peter Ivanov <mr.weider@gmail.com>
2019-02-14 22:21:08 +0300

    Uber patch for CE (based on 8.7.3 changes):
    GridGain Community Edition 2.x => 8.x
    
    GridGain Community Edition 2.x => 8.x typo fix
    
    (cherry picked from commit a55a4a1)
    
    replace license
    
    (cherry picked from commit b88c1ac)
    
    change groupId
    
    (cherry picked from commit fb94da6)
    
    change dependency plugin settings + flatten-plugin version
    
    (cherry picked from commit c3e3a02)
    
    Fixed assembly
    
    (cherry picked from commit c9d30fc)
    
    README.md + README.txt
    
    (cherry picked from commit 28baa0b)
    
    fix examples
    
    (cherry picked from commit eae0303)
    
    Fixed examples GroupID
    
    (cherry picked from commit d706c89)
    
    Revert "Fixed examples GroupID"
    
    This reverts commit d706c891
    
    (cherry picked from commit 5284007)
    
    fix repository
    
    (cherry picked from commit 74510d4)
    
    GG-14775 fix lgpl examples
    
    (cherry picked from commit 68369fe)
    
    Test changes
    
    (cherry picked from commit e2e2b8a)
    
    GG-14775 fix dependencies
    
    (cherry picked from commit 4a71043)
    
    readme.txt fix
    
    (cherry picked from commit 3f9e276)
    
    fix README-LGPL.txt
    
    (cherry picked from commit 2ec9c26)
    
    fix README-LGPL.txt
    
    (cherry picked from commit 950cd14)
    
    Fixed excessive dependency libs
    
    (cherry picked from commit 22cdc45)
    
    remove ignite-ml-xgboost-model-parser and ignite-ml-spark-model-parser
    
    (cherry picked from commit 01de553)
    
    GG-14782 enable monitor by default
    
    (cherry picked from commit ef40ace)
    
    fix licenses and notices
    
    (cherry picked from commit 63acdc9)
    
    examples & platforms README.md
    
    (cherry picked from commit 3170d64)
    
    GG-14796: NPE in GridUpdateNotifier.java:176
    
    (cherry picked from commit ab9b7aa)
    
    GG-14778 GGMonitor print collected data to log
    
    (cherry picked from commit fef94d1)
    
    GG-14780: GGMonitor version monitoring (GridUpdateNotifier) isn't working
    
    fixed
    
    (cherry picked from commit c49d1fd)
    
    GG-14803: 8.7.3 CE/EE/UE - fix information in post message
    of GridUpdateNotifier
    
    fixed
    
    (cherry picked from commit 9d34447)
    
    GG-14808: duplicate product field in update request
    
    fixed
    
    (cherry picked from commit b29fe84)
    
    GG-14811 8.7.3 missing /libs/ignite-shmem-1.0.0.jar in all binary EE/UE/CE
    
    (cherry picked from commit e073a2f)
    
    fix licenses
    
    (cherry picked from commit 222c56a)
    
    fix licenses
    
    (cherry picked from commit 093e512)

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cluster/GridUpdateNotifier.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cluster/GridUpdateNotifier.java
@@ -1,18 +1,35 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ *                   GridGain Community Edition Licensing
+ *                   Copyright 2019 GridGain Systems, Inc.
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License") modified with Commons Clause
+ * Restriction; you may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * Commons Clause Restriction
+ *
+ * The Software is provided to you by the Licensor under the License, as defined below, subject to
+ * the following condition.
+ *
+ * Without limiting other conditions in the License, the grant of rights under the License will not
+ * include, and the License does not grant to you, the right to Sell the Software.
+ * For purposes of the foregoing, “Sell” means practicing any or all of the rights granted to you
+ * under the License to provide to third parties, for a fee or other consideration (including without
+ * limitation fees for hosting or consulting/ support services related to the Software), a product or
+ * service whose value derives, entirely or substantially, from the functionality of the Software.
+ * Any license notice or attribution required by the License must also include this Commons Clause
+ * License Condition notice.
+ *
+ * For purposes of the clause above, the “Licensor” is Copyright 2019 GridGain Systems, Inc.,
+ * the “License” is the Apache License, Version 2.0, and the Software is the GridGain Community
+ * Edition software provided with this notice.
  */
 
 package org.apache.ignite.internal.processors.cluster;
@@ -23,6 +40,7 @@ import java.io.StringWriter;
 import java.io.UnsupportedEncodingException;
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Properties;
 import java.util.Set;
 import java.util.concurrent.RejectedExecutionException;
@@ -52,6 +70,15 @@ import static java.net.URLEncoder.encode;
  * TODO GG-14736 rework this for GridGain Community versions.</p>
  */
 class GridUpdateNotifier {
+    /** Community edition. */
+    private static final String COMMUNITY_EDITION = "ce";
+
+    /** Enterprise edition. */
+    private static final String ENTERPRISE_EDITION = "ee";
+
+    /** Ultimate edition. */
+    private static final String ULTIMATE_EDITION = "ue";
+
     /** Default encoding. */
     static final String CHARSET = "UTF-8";
 
@@ -99,6 +126,9 @@ class GridUpdateNotifier {
 
     /** Number of server nodes in topology. */
     private int srvNodes = 0;
+
+    /** Edition of GridGain */
+    private String product;
 
     /** */
     private long lastLog = -1;
@@ -156,13 +186,15 @@ class GridUpdateNotifier {
 
             for (PluginProvider provider : pluginProviders)
                 pluginsBuilder.a("&").a(provider.name() + "-plugin-version").a("=").
-                    a(encode(provider.version(), CHARSET));
+                    a(encode(Optional.ofNullable(provider.version()).orElse("UNKNOWN"), CHARSET));
 
             pluginsVers = pluginsBuilder.toString();
 
             this.reportOnlyNew = reportOnlyNew;
 
             vmProps = getSystemProperties();
+
+            product = checkProduct();
 
             discoSpi = discovery;
 
@@ -192,6 +224,31 @@ class GridUpdateNotifier {
         catch (UnsupportedEncodingException e) {
             throw new IgniteCheckedException("Failed to encode.", e);
         }
+    }
+
+    /**
+     * Check product version.
+     *
+     * @return CE, EE or UE
+     */
+    private String checkProduct() {
+        String res = COMMUNITY_EDITION;
+
+        try {
+            Class c = Class.forName("org.gridgain.grid.internal.processors.cache.database.SnapshotsMessageFactory");
+            res = ULTIMATE_EDITION;
+        }
+        catch (ClassNotFoundException e) {
+            try {
+                Class c = Class.forName("org.gridgain.grid.persistentstore.GridSnapshot");
+                res = ENTERPRISE_EDITION;
+            }
+            catch (ClassNotFoundException e1) {
+                // NO-OP.
+            }
+        }
+
+        return res;
     }
 
     /**
@@ -360,11 +417,10 @@ class GridUpdateNotifier {
                     "igniteInstanceName=" + encode(igniteInstanceName, CHARSET) +
                         (!F.isEmpty(updStatusParams) ? "&" + updStatusParams : "") +
                         "&srvNodes=" + srvNodes +
+                        "&product=" + product +
                         (!F.isEmpty(stackTrace) ? "&stackTrace=" + encode(stackTrace, CHARSET) : "") +
                         (!F.isEmpty(vmProps) ? "&vmProps=" + encode(vmProps, CHARSET) : "") +
                         pluginsVers;
-
-                System.out.println(postParams);
 
                 if (!isCancelled()) {
                     try {
--- modules/core/src/test/java/org/apache/ignite/internal/processors/cluster/GridUpdateNotifierSelfTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cluster/GridUpdateNotifierSelfTest.java
@@ -1,33 +1,55 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ *                   GridGain Community Edition Licensing
+ *                   Copyright 2019 GridGain Systems, Inc.
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License") modified with Commons Clause
+ * Restriction; you may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * Commons Clause Restriction
+ *
+ * The Software is provided to you by the Licensor under the License, as defined below, subject to
+ * the following condition.
+ *
+ * Without limiting other conditions in the License, the grant of rights under the License will not
+ * include, and the License does not grant to you, the right to Sell the Software.
+ * For purposes of the foregoing, “Sell” means practicing any or all of the rights granted to you
+ * under the License to provide to third parties, for a fee or other consideration (including without
+ * limitation fees for hosting or consulting/ support services related to the Software), a product or
+ * service whose value derives, entirely or substantially, from the functionality of the Software.
+ * Any license notice or attribution required by the License must also include this Commons Clause
+ * License Condition notice.
+ *
+ * For purposes of the clause above, the “Licensor” is Copyright 2019 GridGain Systems, Inc.,
+ * the “License” is the Apache License, Version 2.0, and the Software is the GridGain Community
+ * Edition software provided with this notice.
  */
 
 package org.apache.ignite.internal.processors.cluster;
 
+import java.lang.reflect.Field;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Properties;
+import org.apache.ignite.IgniteCheckedException;
 import org.apache.ignite.IgniteSystemProperties;
 import org.apache.ignite.cluster.ClusterNode;
 import org.apache.ignite.internal.GridKernalContext;
+import org.apache.ignite.internal.GridKernalGateway;
 import org.apache.ignite.internal.IgniteProperties;
 import org.apache.ignite.internal.managers.discovery.GridDiscoveryManager;
 import org.apache.ignite.internal.processors.affinity.AffinityTopologyVersion;
 import org.apache.ignite.internal.util.typedef.internal.U;
 import org.apache.ignite.lang.IgniteProductVersion;
+import org.apache.ignite.plugin.PluginProvider;
 import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
 import org.apache.ignite.testframework.junits.common.GridCommonTest;
 import org.junit.Test;
@@ -86,7 +108,7 @@ public class GridUpdateNotifierSelfTest extends GridCommonAbstractTest {
 
         // Return current node version and some other info
         Mockito.when(updatesCheckerMock.getUpdates(anyString()))
-                .thenReturn("meta=meta" + "\n" + "version=" + nodeVer + "\n" + "downloadUrl=url");
+            .thenReturn("meta=meta" + "\n" + "version=" + nodeVer + "\n" + "downloadUrl=url");
 
         GridKernalContext ctx = Mockito.mock(GridKernalContext.class);
         GridDiscoveryManager discovery = Mockito.mock(GridDiscoveryManager.class);
@@ -96,7 +118,7 @@ public class GridUpdateNotifierSelfTest extends GridCommonAbstractTest {
         Mockito.when(discovery.serverNodes(Mockito.any(AffinityTopologyVersion.class))).thenReturn(srvNodes);
         Mockito.when(ctx.discovery()).thenReturn(discovery);
 
-        GridUpdateNotifier ntf = new GridUpdateNotifier(null, nodeVer,null, ctx.discovery(), Collections.emptyList(), false, updatesCheckerMock);
+        GridUpdateNotifier ntf = new GridUpdateNotifier(null, nodeVer, null, ctx.discovery(), Collections.emptyList(), false, updatesCheckerMock);
 
         ntf.checkForNewVersion(log);
 
@@ -121,5 +143,30 @@ public class GridUpdateNotifierSelfTest extends GridCommonAbstractTest {
             (nodeMaintenance == 0 && lastMaintenance == 0) || (nodeMaintenance > 0 && lastMaintenance > 0));
 
         ntf.reportStatus(log);
+    }
+
+    /**
+     * @throws IgniteCheckedException if failed.
+     * @throws NoSuchFieldException if failed.
+     * @throws IllegalAccessException if failed.
+     */
+    @Test
+    public void testInitializationWithNullPluginProviderVersion() throws IgniteCheckedException, NoSuchFieldException, IllegalAccessException {
+        PluginProvider pp = Mockito.mock(PluginProvider.class);
+        Mockito.when(pp.version()).thenReturn(null);
+        Mockito.when(pp.name()).thenReturn("my-cool-name");
+
+        GridUpdateNotifier notifier = new GridUpdateNotifier(
+            "", "",
+            Mockito.mock(GridKernalGateway.class),
+            Mockito.mock(GridDiscoveryManager.class),
+            Arrays.asList(pp),
+            true, Mockito.mock(HttpIgniteUpdatesChecker.class)
+        );
+
+        Field vers = notifier.getClass().getDeclaredField("pluginsVers");
+        vers.setAccessible(true);
+        String versionsString = (String)vers.get(notifier);
+        assertTrue(versionsString.contains("my-cool-name-plugin-version=UNKNOWN"));
     }
 }
