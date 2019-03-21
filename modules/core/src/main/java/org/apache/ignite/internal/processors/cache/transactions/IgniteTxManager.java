073a8a1a92b7fdb1869d4d242d70986cc7e0461f
Ivan Rakov <irakov@apache.org>
2019-03-21 17:13:52 +0300

    tdr-219

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/transactions/IgniteTxManager.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/transactions/IgniteTxManager.java
@@ -73,6 +73,7 @@ import org.apache.ignite.internal.processors.cache.distributed.near.GridNearOpti
 import org.apache.ignite.internal.processors.cache.distributed.near.GridNearTxLocal;
 import org.apache.ignite.internal.processors.cache.mvcc.MvccCoordinator;
 import org.apache.ignite.internal.processors.cache.mvcc.msg.MvccRecoveryFinishedMessage;
+import org.apache.ignite.internal.processors.cache.persistence.wal.FileWALPointer;
 import org.apache.ignite.internal.processors.cache.transactions.TxDeadlockDetection.TxDeadlockFuture;
 import org.apache.ignite.internal.processors.cache.version.GridCacheVersion;
 import org.apache.ignite.internal.processors.cluster.BaselineTopology;
@@ -2587,7 +2588,7 @@ public class IgniteTxManager extends GridCacheSharedManagerAdapter {
             TransactionState txState = tx.state();
 
             if (txState == PREPARED)
-                cctx.tm().pendingTxsTracker().onTxPrepared(tx.nearXidVersion());
+                cctx.tm().pendingTxsTracker().onTxPrepared(tx.nearXidVersion(), (FileWALPointer)ptr);
             else if (txState == ROLLED_BACK)
                 cctx.tm().pendingTxsTracker().onTxRolledBack(tx.nearXidVersion());
             else
