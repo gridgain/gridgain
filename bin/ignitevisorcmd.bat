39a368abb0cac1f24c5e14a3fa486e850c76d478
Dmitriy Pavlov <dpavlov@apache.org>
2019-03-22 19:16:35 +0300

    IGNITE-11600: Fix launch scripts for Java 12 (#6326)
    (cherry picked from commit 47a49cd)

--- bin/ignitevisorcmd.bat
+++ bin/ignitevisorcmd.bat
@@ -194,7 +194,7 @@ if %MAJOR_JAVA_VER% GEQ 9 if %MAJOR_JAVA_VER% LSS 11 (
     %JVM_OPTS_VISOR%
 )
 
-if %MAJOR_JAVA_VER% == 11 (
+if %MAJOR_JAVA_VER% GEQ 11 (
     set JVM_OPTS_VISOR= ^
     --add-exports=java.base/jdk.internal.misc=ALL-UNNAMED ^
     --add-exports=java.base/sun.nio.ch=ALL-UNNAMED ^
