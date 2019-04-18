e485913f9edf368909d944c3fd0399f7064e0fe1
Anton Kalashnikov <kaa.dev@yandex.ru>
2019-04-18 18:25:32 +0300

    IGNITE-11730 Discovery Compression check fails when nodes reconnect to cluster - Fixes #6438.
    Signed-off-by: Dmitriy Govorukhin <dmitriy.govorukhin@gmail.com>
    
    (cherry picked from commit ec228b1)
    
    (cherry picked from commit 7136025)

--- modules/core/src/main/java/org/apache/ignite/spi/discovery/tcp/ServerImpl.java
+++ modules/core/src/main/java/org/apache/ignite/spi/discovery/tcp/ServerImpl.java
@@ -4021,7 +4021,6 @@ class ServerImpl extends TcpDiscoveryImpl {
                         spi.marshaller(),
                         U.resolveClassLoader(spi.ignite().configuration()),
                         false,
-                        isDiscoDataPaketCompressed(node),
                         log));
 
                 if (err != null) {
@@ -4331,15 +4330,6 @@ class ServerImpl extends TcpDiscoveryImpl {
                 if (sendMessageToRemotes(msg))
                     sendMessageAcrossRing(msg);
             }
-        }
-
-        /**
-         * @param node Joined node.
-         * @return <code>true</code> if packet is compressed.
-         */
-        private boolean isDiscoDataPaketCompressed(TcpDiscoveryNode node) {
-            return IgniteFeatures.nodeSupports(node, IgniteFeatures.DATA_PACKET_COMPRESSION) &&
-                allNodesSupport(IgniteFeatures.DATA_PACKET_COMPRESSION);
         }
 
         /**
