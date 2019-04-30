00b89da56aacf6c32cf308078ee9aa6180f209d3
sanpwc <lapin1702@gmail.com>
2019-04-30 17:21:11 +0300

    gg-17427 one more test added, minors

--- modules/clients/src/test/java/org/apache/ignite/jdbc/thin/JdbcThinAffinityAwarenessReconnectionSelfTest.java
+++ modules/clients/src/test/java/org/apache/ignite/jdbc/thin/JdbcThinAffinityAwarenessReconnectionSelfTest.java
@@ -140,7 +140,7 @@ public class JdbcThinAffinityAwarenessReconnectionSelfTest extends JdbcThinAbstr
     }
 
     /**
-     * Test eager connection failover.
+     * Test eager connection failover:
      * <ol>
      * <li>Check that at the beginning of test {@code INITIAL_NODES_CNT} connections are established.</li>
      * <li>Stop all nodes, invalidate dead connections (jdbc thin, won't detect that node has gone,
@@ -181,7 +181,24 @@ public class JdbcThinAffinityAwarenessReconnectionSelfTest extends JdbcThinAbstr
     }
 
     /**
-     * Check that reconnection thread increases delay between unsuccessful connection attempts.
+     * Check that reconnection thread increases delay between unsuccessful connection attempts:
+     * <ol>
+     * <li>Specify two inet addresses one valid and one inoperative.</li>
+     * <li>Wait for specific amount of time. The reconnection logic suppose to increase delays between reconnection
+     *   attempts. The basic idea is very simple: delay is doubled on evey connection failure until connection succeeds
+     *   or until delay exceeds predefined maximum value {@code JdbcThinConnection.RECONNECTION_MAX_DELAY}
+     *   <pre>
+     *   |_|_ _|_ _ _ _|_ _ _ _ _ _ _ _|
+     *   where: '|' is connection attempt;
+     *          '_' is an amount of time that reconnection tread waits, equal to JdbcThinConnection.RECONNECTION_DELAY;
+     *
+     *   so if we wait for 9 * RECONNECTION_DELAY, we expect to see exact four connection attempts:
+     *   |_|_ _|_ _ _ _|_ _^_ _ _ _ _ _|
+     *   </pre>
+     *   </li>
+     * <li>Check that there were exact four reconnection attempts. In order to do this, we check logs, expecting to see
+     * four warning messages there.</li>
+     * </ol>
      *
      * @throws Exception If failed.
      */
@@ -213,6 +230,109 @@ public class JdbcThinAffinityAwarenessReconnectionSelfTest extends JdbcThinAbstr
     }
 
     /**
+     * Check that reconnection thread selectively increases delay between unsuccessful connection attempts:
+     * <ol>
+     * <li>Create {@code JdbcThinConnection} with two valid inet addresses.</li>
+     * <li>Stop one node and invalidate corresponding connection. Ensure that only one connection left.</li>
+     * <li>Wait for specific amount of time. The reconnection logic suppose to increase delays between reconnection
+     *   attempts. The basic idea is very simple: delay is doubled on evey connection failure until connection succeeds
+     *   or until delay exceeds predefined maximum value {@code JdbcThinConnection.RECONNECTION_MAX_DELAY}
+     *   <pre>
+     *   |_|_ _|_ _ _ _|_ _ _ _ _ _ _ _|
+     *   where: '|' is connection attempt;
+     *          '_' is an amount of time that reconnection tread waits, equal to JdbcThinConnection.RECONNECTION_DELAY;
+     *
+     *   so if we wait for 9 * RECONNECTION_DELAY, we expect to see exact four connection attempts:
+     *   |_|_ _|_ _ _ _|_ _^_ _ _ _ _ _|
+     *   </pre>
+     *   </li>
+     * <li>Check that there were exact four reconnection attempts. In order to do this, we check logs, expecting to see
+     *   four warning messages there.</li>
+     * <li>Start previously stopped node.</li>
+     * <li>Wait until next reconnection attempt.</li>
+     * <li>Check that both connections are established and that there are no warning messages within logs.</li>
+     * <li>One more time: stop one node and invalidate corresponding connection.
+     *   Ensure that only one connection left.</li>
+     * <li>Wait for some time.</li>
+     * <li>Ensure that delay between reconnection was reset to initial value.
+     *   In other words, we again expect four warning messages within logs.</li>
+     * </ol>
+     *
+     * @throws Exception If failed.
+     */
+    @Test
+    public void testReconnectionDelaySelectiveIncreasing() throws Exception {
+        Logger log = Logger.getLogger(JdbcThinConnection.class.getName());
+        LogHandler hnd = new LogHandler();
+        hnd.setLevel(Level.ALL);
+        log.setUseParentHandlers(false);
+        log.addHandler(hnd);
+        log.setLevel(Level.ALL);
+
+        try (Connection conn = DriverManager.getConnection(
+            "jdbc:ignite:thin://127.0.0.1:10800..10801?affinityAwareness=true")) {
+            // Stop one node and invalidate corresponding connection. Ensure that only one connection left.
+            stopGrid(0);
+
+            invalidateConnectionToStoppedNode(conn);
+
+            Map<UUID, JdbcThinTcpIo> ios = GridTestUtils.getFieldValue(conn, "ios");
+
+            assertEquals("Unexpected connections count.", 1, ios.size());
+
+            hnd.records.clear();
+
+            // Wait for some specific amount of time and ensure that there were exact four reconnection attempts.
+            doSleep(9 * JdbcThinConnection.RECONNECTION_DELAY);
+
+            assertEquals("Unexpected log records count.", 4, hnd.records.size());
+
+            String expRecordMsg = "Failed to connect to Ignite node [url=jdbc:ignite:thin://127.0.0.1:10800..10801]." +
+                " address = [localhost/127.0.0.1:10800].";
+
+            for (LogRecord record: hnd.records) {
+                assertEquals("Unexpected log record text.", expRecordMsg, record.getMessage());
+                assertEquals("Unexpected log level", Level.WARNING, record.getLevel());
+            }
+
+            // Start previously stopped node.
+            startGrid(0);
+
+            hnd.records.clear();
+
+            // Waiting until next reconnection attempt.
+            doSleep(9 * JdbcThinConnection.RECONNECTION_DELAY);
+
+            // Checking that both connections are established and that there are no warning messages within logs.
+            assertEquals("Unexpected log records count.", 0, hnd.records.size());
+
+            assertEquals("Unexpected connections count.", 2, ios.size());
+
+            // One more time: stop one node, invalidate corresponding connection and ensure that only one connection
+            // left.
+            stopGrid(0);
+
+            invalidateConnectionToStoppedNode(conn);
+
+            assertEquals("Unexpected connections count.", 1, ios.size());
+
+            hnd.records.clear();
+
+            // Wait for some time and ensure that delay between reconnection was reset to initial value.
+            doSleep(9 * JdbcThinConnection.RECONNECTION_DELAY);
+
+            assertEquals("Unexpected log records count.", 4, hnd.records.size());
+
+            for (LogRecord record: hnd.records) {
+                assertEquals("Unexpected log record text.", expRecordMsg, record.getMessage());
+                assertEquals("Unexpected log level", Level.WARNING, record.getLevel());
+            }
+
+            startGrid(0);
+        }
+    }
+
+    /**
      * Assert connections count.
      *
      * @param ios Map that holds connections.
@@ -228,7 +348,7 @@ public class JdbcThinAffinityAwarenessReconnectionSelfTest extends JdbcThinAbstr
 
     /**
      * Invalidate connection to stopped node. Jdbc thin, won't detect that node has gone, until it tries to touch it.
-     * So sending simple query to randomly chosed connection(socket), sooner or later, will touch dead one,
+     * So sending simple query to randomly chosen connection(socket), sooner or later, will touch dead one,
      * and thus invalidate it.
      *
      * @param conn Connections.
@@ -275,5 +395,4 @@ public class JdbcThinAffinityAwarenessReconnectionSelfTest extends JdbcThinAbstr
             return records;
         }
     }
-
 }
