e4f1dd66710b82064eadd3475de1b0ade524cbb9
Ilya Borisov <iborisov@gridgain.com>
2019-05-13 16:43:45 +0700

    WC-1041 Cover form-field-errors with tests.

--- modules/web-console/frontend/app-angular/components/form-field/errors.component.spec.ts
+++ modules/web-console/frontend/app-angular/components/form-field/errors.component.spec.ts
@@ -15,18 +15,76 @@
  */
 
 import 'app/../test/angular-testbed-init';
+import {FormFieldErrors} from './errors.component';
+import {assert} from 'chai';
+import {TestBed, async, ComponentFixture, tick, fakeAsync} from '@angular/core/testing';
+import {Component, NO_ERRORS_SCHEMA, Input, ViewChild, ElementRef} from '@angular/core';
 
-// import {
-//     BrowserDynamicTestingModule,
-//     platformBrowserDynamicTesting
-// } from '@angular/platform-browser-dynamic/testing';
+suite('Angular form-field-errors component', () => {
+    let fixture: ComponentFixture<HostComponent>;
 
-// import {FormFieldErrors} from './errors.component';
-// import {assert} from 'chai';
-// import {TestBed, async, ComponentFixture, tick, fakeAsync} from '@angular/core/testing';
-// import {Component, Directive, NO_ERRORS_SCHEMA, Input, TemplateRef} from '@angular/core';
-// import { By } from '@angular/platform-browser';
+    @Component({
+        template: `
+            <form-field-errors
+                errorStyle='inline'
+                errorType='required'
+                #inline
+            ></form-field-errors>
+            <form-field-errors
+                errorStyle='icon'
+                [errorType]='errorType'
+                [extraErrorMessages]='{unique: unique}'
+                #icon
+            ></form-field-errors>
+            <ng-template #unique>Value should be unique</ng-template>
+        `
+    })
+    class HostComponent {
+        @ViewChild('inline', {read: ElementRef})
+        inline: HTMLElement
+        @ViewChild('icon', {read: ElementRef})
+        icon: HTMLElement
+        errorType = 'unique'
+    }
 
-suite.only('Angular form-field-errors component', () => {
-
+    setup(fakeAsync(async() => {
+        await TestBed.configureTestingModule({
+            declarations: [
+                FormFieldErrors,
+                HostComponent
+            ],
+            schemas: [NO_ERRORS_SCHEMA]
+        }).compileComponents();
+        fixture = TestBed.createComponent(HostComponent);
+        fixture.detectChanges();
+    }));
+    test('Error style', () => {
+        assert.ok(
+            fixture.debugElement.componentInstance.inline.nativeElement.querySelector('.inline'),
+            'It can show inline errors'
+        );
+        assert.ok(
+            fixture.debugElement.componentInstance.icon.nativeElement.querySelector('.icon'),
+            'It can show icon errors'
+        );
+    });
+    test('Validation message', () => {
+        assert.equal(
+            'Value is required',
+            fixture.debugElement.componentInstance.inline.nativeElement.textContent,
+            'It shows default message'
+        );
+        assert.equal(
+            'Value should be unique',
+            fixture.debugElement.componentInstance.icon.nativeElement.textContent,
+            'It shows custom message'
+        );
+        fixture.componentInstance.errorType = 'foo';
+        fixture.detectChanges();
+        assert.equal(
+            'Value is invalid: foo',
+            fixture.debugElement.componentInstance.icon.nativeElement.textContent,
+            'It shows placeholder message'
+        );
+    });
 });
