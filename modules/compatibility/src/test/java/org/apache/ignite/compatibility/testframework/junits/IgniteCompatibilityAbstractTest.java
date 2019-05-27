4ca143b9aae555c22a1c23b1be25ba549a31d869
mstepachev <maksim.stepachev@gmail.com>
2019-05-27 15:24:31 +0300

    GG-18930 exclude org.gridgain for PDS compatibility.

--- modules/compatibility/src/test/java/org/apache/ignite/compatibility/testframework/junits/IgniteCompatibilityAbstractTest.java
+++ modules/compatibility/src/test/java/org/apache/ignite/compatibility/testframework/junits/IgniteCompatibilityAbstractTest.java
@@ -242,7 +242,7 @@ public abstract class IgniteCompatibilityAbstractTest extends GridCommonAbstract
 
         dependencies.add(new Dependency("core", "ignite-core", false));
         dependencies.add(new Dependency("core", "ignite-core", true));
-        dependencies.add(new Dependency("core", "org.gridgain", "ignite-core", null, true));
+        dependencies.add(new Dependency("core", "org.gridgain", "ignite-core", null, false));
         dependencies.add(new Dependency("core", "org.gridgain", "ignite-core", null, true));
 
         return dependencies;
