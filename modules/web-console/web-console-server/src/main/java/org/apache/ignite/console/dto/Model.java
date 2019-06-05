66ef9ae269c2b872e00fed475ad1bc88e454f64c
Andrey Novikov <anovikov@gridgain.com>
2019-06-05 15:25:21 +0700

    GG-17577 Changed TODO.

--- modules/web-console/web-console-server/src/main/java/org/apache/ignite/console/dto/Model.java
+++ modules/web-console/web-console-server/src/main/java/org/apache/ignite/console/dto/Model.java
@@ -46,7 +46,7 @@ public class Model extends DataObject {
 
         return new Model(
             id,
-            false, // TODO IGNITE-5617 DETECT INDEXES !!!
+            false, // TODO GG-19220 DETECT INDEXES !!!
             json.getString("keyType"),
             json.getString("valueType"),
             toJson(json)
