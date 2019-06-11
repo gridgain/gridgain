af127300526ff90e7125667015ea75f58d47ed12
Alexey Kuznetsov <akuznetsov@apache.org>
2019-06-11 12:46:03 +0700

    GG-18366 WIP.

--- modules/web-console/web-console-server/src/main/java/org/apache/ignite/console/config/AccountConfiguration.java
+++ modules/web-console/web-console-server/src/main/java/org/apache/ignite/console/config/AccountConfiguration.java
@@ -16,7 +16,13 @@
 
 package org.apache.ignite.console.config;
 
+import java.nio.charset.StandardCharsets;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
 import org.apache.ignite.internal.util.typedef.internal.S;
+import org.apache.ignite.internal.util.typedef.internal.U;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -37,48 +43,7 @@ public class AccountConfiguration {
      */
     @Bean
     public PasswordEncoder passwordEncoder() {
-        // Pbkdf2PasswordEncoder is compatible with passport.js, but BCryptPasswordEncoder is recommended by Spring.
-        // We can return to Pbkdf2PasswordEncoder if we decided to import old users.
-        //  Pbkdf2PasswordEncoder encoder = new Pbkdf2PasswordEncoder("", 25000, HASH_WIDTH); // HASH_WIDTH = 512
-        //
-        //  encoder.setAlgorithm(PBKDF2WithHmacSHA256);
-        //  encoder.setEncodeHashAsBase64(true);
-
-
-//        /** TODO IGNITE-5617 Revert to 25000 before release. */
-//        private static final int ITERATIONS = 10;
-//
-//        /** */
-//        private static final int KEY_LEN = 512 * 8;
-
-//        /**
-//         * @return Salt for hashing.
-//         */
-//        private String generateSalt() {
-//            byte[] salt = new byte[32];
-//
-//            rnd.nextBytes(salt);
-//
-//            return U.byteArray2HexString(salt);
-//        }
-//
-
-//        private String computeHash(String pwd, String salt) throws Exception {
-//            // TODO IGNITE-5617: How about to re-hash on first successful compare?
-//            PBEKeySpec spec = new PBEKeySpec(
-//                pwd.toCharArray(),
-//                salt.getBytes(StandardCharsets.UTF_8), // For compatibility with hashing data imported from NodeJS.
-//                ITERATIONS,
-//                KEY_LEN);
-//
-//            SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
-//
-//            byte[] hash = skf.generateSecret(spec).getEncoded();
-//
-//            return U.byteArray2HexString(hash);
-//        }
-
-        return new BCryptPasswordEncoder();
+        return new CustomPasswordEncoder();
     }
 
     /**
@@ -101,5 +66,62 @@ public class AccountConfiguration {
     /** {@inheritDoc} */
     @Override public String toString() {
         return S.toString(AccountConfiguration.class, this);
+    }
+
+    /**
+     * Custom password encoder that encode new passwords with BCrypt and can verify old passwords imported from MongoDB.
+     */
+    private static class CustomPasswordEncoder implements PasswordEncoder {
+        /** */
+        private static final Logger log = LoggerFactory.getLogger(CustomPasswordEncoder.class);
+
+        /** Number of hash iterations, taken from passportjs */
+        private static final int ITERATIONS = 25000;
+
+        /** Key length, taken from passportjs */
+        private static final int KEY_LEN = 512 * 8;
+
+        /** */
+        private final PasswordEncoder bcrypt = new BCryptPasswordEncoder();
+
+        /** {@inheritDoc} */
+        @Override public String encode(CharSequence rawPassword) {
+            return bcrypt.encode(rawPassword);
+        }
+
+        /** {@inheritDoc} */
+        @Override public boolean matches(CharSequence rawPassword, String encodedPassword) {
+            if (encodedPassword.startsWith("PBKDF2:")) {
+                String[] parts = encodedPassword.split(":");
+
+                if (parts.length != 3) {
+                    log.error("Invalid hash in PBKDF2 format: " + encodedPassword);
+
+                    return false;
+                }
+
+                String salt = parts[1];
+                String hash = parts[2];
+
+                PBEKeySpec spec = new PBEKeySpec(
+                    rawPassword.toString().toCharArray(),
+                    salt.getBytes(StandardCharsets.UTF_8),
+                    ITERATIONS,
+                    KEY_LEN);
+
+                try {
+                    SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+
+                    String encoded = U.byteArray2HexString(skf.generateSecret(spec).getEncoded());
+
+                    return hash.equalsIgnoreCase(encoded);
+                }
+                catch (Throwable e) {
+                    log.error("Failed to check password", e);
+                }
+            }
+
+            return bcrypt.matches(rawPassword, encodedPassword);
+        }
     }
 }
