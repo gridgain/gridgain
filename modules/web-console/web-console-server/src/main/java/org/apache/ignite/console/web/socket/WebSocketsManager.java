cda0a9c6a1234db9e157a21adeb0000027b8d856
Andrey Novikov <anovikov@gridgain.com>
2019-06-14 10:26:41 +0700

    GG-19218 Fixed review notes.

--- modules/web-console/web-console-server/src/main/java/org/apache/ignite/console/web/socket/WebSocketsManager.java
+++ modules/web-console/web-console-server/src/main/java/org/apache/ignite/console/web/socket/WebSocketsManager.java
@@ -34,7 +34,6 @@ import org.apache.ignite.internal.util.typedef.F;
 import org.jsr166.ConcurrentLinkedHashMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.socket.CloseStatus;
@@ -56,7 +55,6 @@ import static org.springframework.web.util.UriComponentsBuilder.fromUri;
  * Web sockets manager.
  */
 @Service
-@EnableScheduling
 public class WebSocketsManager {
     /** */
     private static final Logger log = LoggerFactory.getLogger(WebSocketsManager.class);
