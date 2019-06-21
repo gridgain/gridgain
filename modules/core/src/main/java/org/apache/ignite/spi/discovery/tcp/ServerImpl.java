126d28cbe0425b42f06b77d52ff13f9bff1c50e2
denis-chudov <dchudov@gridgain.com>
2019-06-21 20:08:55 +0300

    GG-17447: conflicts resolved

--- modules/core/src/main/java/org/apache/ignite/spi/discovery/tcp/ServerImpl.java
+++ modules/core/src/main/java/org/apache/ignite/spi/discovery/tcp/ServerImpl.java
@@ -642,7 +642,7 @@ class ServerImpl extends TcpDiscoveryImpl {
     ) {
         TcpDiscoveryStatusCheckMessage msg;
 
-        if (allNodesSupports(ring.allNodes(), TCP_DISCOVERY_MESSAGE_NODE_SERIALIZATION_OPTIMIZATION)) {
+        if (allNodesSupport(TCP_DISCOVERY_MESSAGE_NODE_SERIALIZATION_OPTIMIZATION)) {
             boolean sameMacs = creatorNode != null && targetNode != null && U.sameMacs(creatorNode, targetNode);
 
             msg = new TcpDiscoveryStatusCheckMessage(
@@ -679,7 +679,7 @@ class ServerImpl extends TcpDiscoveryImpl {
     private TcpDiscoveryDuplicateIdMessage createTcpDiscoveryDuplicateIdMessage(UUID creatorNodeId, TcpDiscoveryNode node) {
         TcpDiscoveryDuplicateIdMessage msg;
 
-        if (allNodesSupports(ring.allNodes(), TCP_DISCOVERY_MESSAGE_NODE_SERIALIZATION_OPTIMIZATION))
+        if (allNodesSupport(TCP_DISCOVERY_MESSAGE_NODE_SERIALIZATION_OPTIMIZATION))
             msg = new TcpDiscoveryDuplicateIdMessage(creatorNodeId, node.id());
         else
             msg = new TcpDiscoveryDuplicateIdMessage(creatorNodeId, node);
