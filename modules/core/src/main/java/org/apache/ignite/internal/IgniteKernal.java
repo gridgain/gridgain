8bb621f48d01e8172d75996efc41d2c415e9a047
Alexey Kuznetsov <akuznetsov@apache.org>
2019-07-01 23:18:25 +0700

    GG-19573 WIP.

--- modules/core/src/main/java/org/apache/ignite/internal/IgniteKernal.java
+++ modules/core/src/main/java/org/apache/ignite/internal/IgniteKernal.java
@@ -239,6 +239,7 @@ import static org.apache.ignite.internal.GridKernalState.STARTING;
 import static org.apache.ignite.internal.GridKernalState.STOPPED;
 import static org.apache.ignite.internal.GridKernalState.STOPPING;
 import static org.apache.ignite.internal.IgniteComponentType.COMPRESSION;
+import static org.apache.ignite.internal.IgniteComponentType.MANAGEMENT_CONSOLE_AGENT;
 import static org.apache.ignite.internal.IgniteComponentType.SCHEDULE;
 import static org.apache.ignite.internal.IgniteNodeAttributes.ATTR_BUILD_DATE;
 import static org.apache.ignite.internal.IgniteNodeAttributes.ATTR_BUILD_VER;
@@ -1108,6 +1109,7 @@ public class IgniteKernal implements IgniteEx, IgniteMXBean, Externalizable {
             // Start processors before discovery manager, so they will
             // be able to start receiving messages once discovery completes.
             try {
+                startProcessor(MANAGEMENT_CONSOLE_AGENT.createOptional(ctx));
                 startProcessor(COMPRESSION.createOptional(ctx));
                 startProcessor(new PdsConsistentIdProcessor(ctx));
                 startProcessor(new MvccProcessorImpl(ctx));
