e1727b996bf34edb35fb70112d699ec7611bd06a
Anton Kalashnikov <kaa.dev@yandex.ru>
2019-07-03 14:49:56 +0300

    GG-20784 Changed default concurrency level in .net implementation

--- modules/platforms/dotnet/Apache.Ignite.Core/Configuration/DataStorageConfiguration.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core/Configuration/DataStorageConfiguration.cs
@@ -161,7 +161,7 @@ namespace Apache.Ignite.Core.Configuration
         /// <summary>
         /// The default concurrency level.
         /// </summary>
-        public const int DefaultConcurrencyLevel = 0;
+        public const int DefaultConcurrencyLevel = Environment.ProcessorCount;
 
         /// <summary>
         /// Default value for <see cref="MaxWalArchiveSize"/>.
