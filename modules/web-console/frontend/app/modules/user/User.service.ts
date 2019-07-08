6b53479a7b12887785e08e52c7d16ffc010259e0
vsisko <vsisko@gridgian.com>
2019-07-08 10:18:55 +0300

    GG-18554 Clear refresh rate on logout.

--- modules/web-console/frontend/app/modules/user/User.service.ts
+++ modules/web-console/frontend/app/modules/user/User.service.ts
@@ -79,6 +79,7 @@ export default function UserFactory(
             delete Demo.enabled;
 
             sessionStorage.removeItem('demoMode');
+            sessionStorage.removeItem('refreshRate');
         },
 
         async save(user: Partial<User>): Promise<User> {
