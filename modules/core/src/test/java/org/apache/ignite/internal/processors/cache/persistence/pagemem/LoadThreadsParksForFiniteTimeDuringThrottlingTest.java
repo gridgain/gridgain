c9b814d49d058ab5cf9a9c53b46c47f43d5a3c41
Sergey Antonov <antonovsergey93@gmail.com>
2019-07-11 18:16:28 +0300

    GG-21123 More comments in test

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/persistence/pagemem/LoadThreadsParksForFiniteTimeDuringThrottlingTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/persistence/pagemem/LoadThreadsParksForFiniteTimeDuringThrottlingTest.java
@@ -163,9 +163,10 @@ public class LoadThreadsParksForFiniteTimeDuringThrottlingTest extends GridCommo
             final int numOfLoops = (int)(getTestTimeout() / sleepTime);
             PageMemory pm = crd.context().cache().context().database().dataRegion(DFLT_DATA_REGION_NAME).pageMemory();
 
-            // throttling will be enabled, if 2/3 of checkpoint buffer size is busy. We have 256mb checkpoint buffer
-            // (i.e. 2^16 4kb pages). (0.67-2/3)*2^16 = 218. Load threads will be parked 4000*1.05^x/10^-6 ms, where
-            // x >= 198. For x=198 park time is 6.2739522e+13 ms. It's more then enough for test.
+            // Throttling will be enabled, if 2/3 of checkpoint buffer size is busy. We have 256mb checkpoint buffer
+            // (i.e. 2^16 4kb pages). (0.67-2/3)*2^16 = 218. So we must mark dirty at least 218 pages. Load threads
+            // will be parked 4000*1.05^x/10^-6 ms, where x >= 198. For x=198 park time is 6.2739522e+13 ms. It's more
+            // then enough for this test.
             final double limit = (0.67 * Math.ceil((CP_BUFFER_SIZE + 0.) / pm.pageSize()));
 
             for (int i = 0; i < numOfLoops; i++) {
