ca8c88f45376d98940c93527dd081b4ec6f0ac44
Sergey Moldachev <sergeymoldachev@gmail.com>
2019-07-26 22:31:11 +0300

    GG-21922: Fix RestExecutorSelfTest test.

--- modules/web-console/web-agent/src/test/java/org/apache/ignite/console/agent/rest/RestExecutorSelfTest.java
+++ modules/web-console/web-agent/src/test/java/org/apache/ignite/console/agent/rest/RestExecutorSelfTest.java
@@ -33,7 +33,6 @@ import org.apache.ignite.configuration.ConnectorConfiguration;
 import org.apache.ignite.configuration.IgniteConfiguration;
 import org.apache.ignite.console.json.JsonObject;
 import org.apache.ignite.console.utils.Utils;
-import org.apache.ignite.internal.util.IgniteUtils;
 import org.apache.ignite.internal.util.typedef.F;
 import org.apache.ignite.spi.discovery.tcp.TcpDiscoverySpi;
 import org.apache.ignite.spi.discovery.tcp.ipfinder.TcpDiscoveryIpFinder;
@@ -56,11 +55,13 @@ import static org.mockserver.model.HttpResponse.response;
  * Test for RestExecutor.
  */
 public class RestExecutorSelfTest {
+    static {
+        System.getProperties().setProperty("IGNITE_JETTY_KEY_STORE_PATH", resolvePath("server.jks"));
+        System.getProperties().setProperty("IGNITE_JETTY_TRUST_STORE_PATH", resolvePath("ca.jks"));
+    }
+
     /** Name of the cache created by default in the cluster. */
     private static final String DEFAULT_CACHE_NAME = "default";
-
-    /** Path to certificates and configs. */
-    private static final String PATH_TO_RESOURCES = "modules/web-console/web-agent/src/test/resources/";
 
     /** JSON object mapper. */
     private static final ObjectMapper MAPPER = new ObjectMapper();
@@ -90,21 +91,21 @@ public class RestExecutorSelfTest {
     private static final List<String> CIPHER_0 = Collections.singletonList("TLS_DH_anon_WITH_AES_256_GCM_SHA384");
 
     /** */
-    private static final List<String> CIPHER_1 = Collections.singletonList("TLS_RSA_WITH_NULL_SHA256");
+    private static final List<String> CIPHER_1 = Collections.singletonList("TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384");
 
     /** */
-    private static final List<String> CIPHER_2 = Collections.singletonList("TLS_ECDHE_ECDSA_WITH_NULL_SHA");
+    private static final List<String> CIPHER_2 = Collections.singletonList("TLS_EMPTY_RENEGOTIATION_INFO_SCSV");
 
     /** */
     private static final List<String> COMMON_CIPHERS = Arrays.asList(
-        "TLS_RSA_WITH_NULL_SHA256",
-        "TLS_ECDHE_ECDSA_WITH_NULL_SHA"
+        "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384",
+        "TLS_EMPTY_RENEGOTIATION_INFO_SCSV"
     );
 
     /** */
     @SuppressWarnings("PublicField")
     @Rule
-    public final ExpectedException ruleForExpectedException = ExpectedException.none();
+    public final ExpectedException ruleForExpEx = ExpectedException.none();
 
     /**
      * @param jettyCfg Optional path to file with Jetty XML config.
@@ -161,8 +162,8 @@ public class RestExecutorSelfTest {
      * @param file File name.
      * @return Path to file.
      */
-    private String resolvePath(String file) {
-        return IgniteUtils.resolveIgnitePath(PATH_TO_RESOURCES + file).getAbsolutePath();
+    private static String resolvePath(String file) {
+        return RestExecutorSelfTest.class.getClassLoader().getResource(file).getPath();
     }
 
     /**
@@ -234,7 +235,7 @@ public class RestExecutorSelfTest {
     /** */
     @Test
     public void nodeNoSslAgentWithSsl() throws Throwable {
-        ruleForExpectedException.expect(Is.isA(SSLException.class));
+        ruleForExpEx.expect(Is.isA(SSLException.class));
         checkRest(
             nodeConfiguration(""),
             HTTPS_URI,
@@ -248,7 +249,7 @@ public class RestExecutorSelfTest {
     /** */
     @Test
     public void nodeWithSslAgentNoSsl() throws Throwable {
-        ruleForExpectedException.expect(Is.isA(HttpResponseException.class));
+        ruleForExpEx.expect(Is.isA(HttpResponseException.class));
         checkRest(
             nodeConfiguration(JETTY_WITH_SSL),
             HTTP_URI,
@@ -275,7 +276,7 @@ public class RestExecutorSelfTest {
     /** */
     @Test
     public void nodeNoCiphersAgentWithCiphers() throws Throwable {
-        ruleForExpectedException.expect(Is.isA(SSLHandshakeException.class));
+        ruleForExpEx.expect(Is.isA(SSLHandshakeException.class));
         checkRest(
             nodeConfiguration(JETTY_WITH_SSL),
             HTTPS_URI,
@@ -289,7 +290,7 @@ public class RestExecutorSelfTest {
     /** */
     @Test
     public void nodeWithCiphersAgentNoCiphers() throws Throwable {
-        ruleForExpectedException.expect(Is.isA(SSLException.class));
+        ruleForExpEx.expect(Is.isA(SSLException.class));
         checkRest(
             nodeConfiguration(JETTY_WITH_CIPHERS_0),
             HTTPS_URI,
@@ -316,7 +317,7 @@ public class RestExecutorSelfTest {
     /** */
     @Test
     public void differentCiphers1() throws Throwable {
-        ruleForExpectedException.expect(Is.isA(SSLHandshakeException.class));
+        ruleForExpEx.expect(Is.isA(SSLException.class));
         checkRest(
             nodeConfiguration(JETTY_WITH_CIPHERS_1),
             HTTPS_URI,
@@ -330,7 +331,7 @@ public class RestExecutorSelfTest {
     /** */
     @Test
     public void differentCiphers2() throws Throwable {
-        ruleForExpectedException.expect(Is.isA(SSLException.class));
+        ruleForExpEx.expect(Is.isA(SSLException.class));
         checkRest(
             nodeConfiguration(JETTY_WITH_CIPHERS_2),
             HTTPS_URI,
@@ -410,7 +411,7 @@ public class RestExecutorSelfTest {
             mockSrv = startClientAndServer(8080);
 
             char[] chars = new char[3 * 1024 * 1024];
-            
+
             Arrays.fill(chars, 'a');
 
             String err = new String(chars);
