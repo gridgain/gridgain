2180ff38994e55450fa92fecdba07fa81849a65c
Sergey Chugunov <sergey.chugunov@gmail.com>
2019-07-29 11:06:04 +0300

    GG-18992 null and empty string check

--- modules/core/src/main/java/org/apache/ignite/internal/cluster/IgniteClusterImpl.java
+++ modules/core/src/main/java/org/apache/ignite/internal/cluster/IgniteClusterImpl.java
@@ -616,7 +616,10 @@ public class IgniteClusterImpl extends ClusterGroupAdapter implements IgniteClus
 
     /** {@inheritDoc} */
     @Override public void tag(String tag) throws IgniteCheckedException {
-        if (tag != null && tag.length() > MAX_TAG_LENGTH)
+        if (tag == null || tag.isEmpty())
+            throw new IgniteCheckedException("Please provide not-null and not empty string for cluster tag");
+
+        if (tag.length() > MAX_TAG_LENGTH)
             throw new IgniteCheckedException("Maximum tag length is exceeded, max length is " +
                 MAX_TAG_LENGTH +
                 " symbols, provided value has " +
