38374d521ed0d6e3eff92c782803d0a8251ccbe8
Anton Kalashnikov <kaa.dev@yandex.ru>
2019-08-14 11:20:27 +0300

    GG-22656 Resovling work dir to current working dir by default
    (cherry picked from commit eb7e82a)

--- modules/core/src/test/java/org/apache/ignite/internal/util/GridStartupWithUndefinedIgniteHomeSelfTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/util/GridStartupWithUndefinedIgniteHomeSelfTest.java
@@ -17,11 +17,12 @@
 package org.apache.ignite.internal.util;
 
 import junit.framework.TestCase;
-import org.apache.ignite.IgniteException;
+import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteLogger;
 import org.apache.ignite.IgniteSystemProperties;
 import org.apache.ignite.configuration.IgniteConfiguration;
 import org.apache.ignite.internal.util.typedef.G;
+import org.apache.ignite.internal.util.typedef.X;
 import org.apache.ignite.internal.util.typedef.internal.U;
 import org.apache.ignite.logger.java.JavaLogger;
 import org.apache.ignite.spi.discovery.tcp.TcpDiscoverySpi;
@@ -33,8 +34,6 @@ import org.junit.Test;
 
 import static org.apache.ignite.IgniteSystemProperties.IGNITE_HOME;
 import static org.apache.ignite.internal.util.IgniteUtils.nullifyHomeDirectory;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 /**
  * Checks that node can be started without operations with undefined IGNITE_HOME.
@@ -96,13 +95,14 @@ public class GridStartupWithUndefinedIgniteHomeSelfTest {
             cfg.setDiscoverySpi(disc);
             cfg.setConnectorConfiguration(null);
 
-            try {
-                G.start(cfg);
+            try (Ignite ignite = G.start(cfg)) {
+                assert ignite != null;
 
-                fail("Starting should fail due to working directory is not defined");
-            }
-            catch (IgniteException e) {
-                assertTrue(e.getMessage().contains("Failed to resolve Ignite work directory"));
+                igniteHome0 = U.getIgniteHome();
+
+                assert igniteHome0 == null;
+
+                X.println("Stopping grid " + ignite.cluster().localNode().id());
             }
         }
     }
