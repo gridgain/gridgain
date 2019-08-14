2138e8d10f495ecdcb139bfe39b08a710a5714e1
Sergey Chugunov <sergey.chugunov@gmail.com>
2019-08-14 14:48:01 +0300

    GG-21224 new line after license header

--- modules/core/src/main/java/org/apache/ignite/internal/commandline/id_and_tag/ClusterIdAndTagSubcommands.java
+++ modules/core/src/main/java/org/apache/ignite/internal/commandline/id_and_tag/ClusterIdAndTagSubcommands.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.apache.ignite.internal.commandline.id_and_tag;
 
 import org.apache.ignite.internal.visor.id_and_tag.VisorIdAndTagOperation;
