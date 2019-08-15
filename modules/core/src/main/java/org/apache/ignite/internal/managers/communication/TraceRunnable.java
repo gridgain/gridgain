5a3baa688ea474b512ab7df23b1a32eb6cdf0006
Anton Kalashnikov <kaa.dev@yandex.ru>
2019-08-15 17:08:37 +0300

    GG-18714 Fixed code style

--- modules/core/src/main/java/org/apache/ignite/internal/managers/communication/TraceRunnable.java
+++ modules/core/src/main/java/org/apache/ignite/internal/managers/communication/TraceRunnable.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019 GridGain Systems, Inc. and Contributors.
+ *
+ * Licensed under the GridGain Community Edition License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.gridgain.com/products/software/community-edition/gridgain-community-edition-license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.ignite.internal.managers.communication;
 
 import org.apache.ignite.internal.processors.tracing.MTC;
@@ -19,7 +35,6 @@ public abstract class TraceRunnable implements Runnable {
     private final Span parent;
 
     /**
-     *
      * @param tracing Tracing processor.
      * @param name Name of new span.
      */
@@ -39,5 +54,5 @@ public abstract class TraceRunnable implements Runnable {
     /**
      * Main code to execution.
      */
-    abstract public void execute();
+    public abstract void execute();
 }
