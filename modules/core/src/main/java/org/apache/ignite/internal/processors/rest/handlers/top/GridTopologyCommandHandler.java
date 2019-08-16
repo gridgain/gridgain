fdc98505fa6e61cc9c619b32f32dbcb6b8a2da92
smoldachev <smoldachev@gridgain.com>
2019-08-16 12:47:22 +0300

    gg-22789: Move spring actuator dependency to ignite-web-console-server.

--- modules/core/src/main/java/org/apache/ignite/internal/processors/rest/handlers/top/GridTopologyCommandHandler.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/rest/handlers/top/GridTopologyCommandHandler.java
@@ -278,10 +278,11 @@ public class GridTopologyCommandHandler extends GridRestCommandHandlerAdapter {
             metricsBean.setTotalRejectedJobs(metrics.getTotalRejectedJobs());
             metricsBean.setTotalStartedThreadCount(metrics.getTotalStartedThreadCount());
             metricsBean.setTotalExecutedTasks(metrics.getTotalExecutedTasks());
-            metricsBean.setSentMessagesCount(metrics.getSentMessagesCount());
-            metricsBean.setSentBytesCount(metrics.getSentBytesCount());
-            metricsBean.setReceivedMessagesCount(metrics.getReceivedMessagesCount());
-            metricsBean.setReceivedBytesCount(metrics.getReceivedBytesCount());
+            //TODO: GMC dirty hach remove before merge.
+//            metricsBean.setSentMessagesCount(metrics.getSentMessagesCount());
+//            metricsBean.setSentBytesCount(metrics.getSentBytesCount());
+//            metricsBean.setReceivedMessagesCount(metrics.getReceivedMessagesCount());
+//            metricsBean.setReceivedBytesCount(metrics.getReceivedBytesCount());
             metricsBean.setUpTime(metrics.getUpTime());
 
             nodeBean.setMetrics(metricsBean);
