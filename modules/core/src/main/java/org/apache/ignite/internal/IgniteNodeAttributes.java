d958af3c29c5a34655c84a9581395805b7c72421
Sergey Antonov <antonovsergey93@gmail.com>
2019-08-25 19:37:20 +0300

    GG-23205 Rename USE_POOL_FOR_LAZY property to IGNITE_USE_POOL_FOR_LAZY_QUERIES

--- modules/core/src/main/java/org/apache/ignite/internal/IgniteNodeAttributes.java
+++ modules/core/src/main/java/org/apache/ignite/internal/IgniteNodeAttributes.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2019 GridGain Systems, Inc. and Contributors.
- * 
+ *
  * Licensed under the GridGain Community Edition License (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     https://www.gridgain.com/products/software/community-edition/gridgain-community-edition-license
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -210,6 +210,8 @@ public final class IgniteNodeAttributes {
     /** Supported features. */
     public static final String ATTR_IGNITE_FEATURES = ATTR_PREFIX + ".features";
 
+    /** */
+    public static final String ATTR_USE_POOL_FOR_LAZY_QUERIES = ATTR_PREFIX + ".query.lazy.usepool";
     /**
      * Enforces singleton.
      */
