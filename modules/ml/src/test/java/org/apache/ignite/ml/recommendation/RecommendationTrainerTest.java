4d03738fe7de710f96a0c7d52fead4f808d2407d
dmitrievanthony <dmitrievanthony@gmail.com>
2019-08-27 10:57:01 +0300

    GG-22877: Add recommendation model update test.

--- modules/ml/src/test/java/org/apache/ignite/ml/recommendation/RecommendationTrainerTest.java
+++ modules/ml/src/test/java/org/apache/ignite/ml/recommendation/RecommendationTrainerTest.java
@@ -26,6 +26,7 @@ import org.apache.ignite.ml.environment.LearningEnvironmentBuilder;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 /** Tests for {@link RecommendationTrainer}. */
 public class RecommendationTrainerTest {
@@ -62,6 +63,54 @@ public class RecommendationTrainerTest {
         int incorrect = 0;
         for (ObjectSubjectRatingTriplet<Integer, Integer> triplet : toList(ratings)) {
             double prediction = Math.round(mdl.predict(triplet));
+            if (Math.abs(prediction - triplet.getRating()) >= 1e-5) incorrect++;
+        }
+
+        assertEquals(0, incorrect);
+    }
+
+    /** */
+    @Test
+    public void testUpdate() {
+        int size = 100;
+        Random rnd = new Random(0L);
+        Double[][] ratings = new Double[size][size];
+        // Quadrant I contains "0", quadrant II contains "1", quadrant III contains "0", quadrant IV contains "1".
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (rnd.nextBoolean())
+                    ratings[i][j] = ((i > size / 2) ^ (j > size / 2)) ? 1.0 : 0.0;
+            }
+        }
+
+        int seq = 0;
+        Map<Integer, ObjectSubjectRatingTriplet<Integer, Integer>> data = new HashMap<>();
+        for (ObjectSubjectRatingTriplet<Integer, Integer> triplet : toList(ratings))
+            data.put(seq++, triplet);
+
+        RecommendationTrainer trainer = new RecommendationTrainer()
+            .withLearningRate(50.0)
+            .withBatchSize(10)
+            .withK(2)
+            .withMaxIterations(25)
+            .withLearningEnvironmentBuilder(LearningEnvironmentBuilder.defaultBuilder().withRNGSeed(1))
+            .withTrainerEnvironment(LearningEnvironmentBuilder.defaultBuilder().withRNGSeed(1).buildForTrainer());
+
+        RecommendationModel<Integer, Integer> mdl = trainer.fit(new LocalDatasetBuilder<>(data, 10));
+
+        int incorrect = 0;
+        for (ObjectSubjectRatingTriplet<Integer, Integer> triplet : toList(ratings)) {
+            double prediction = Math.round(mdl.predict(triplet));
+            if (Math.abs(prediction - triplet.getRating()) >= 1e-5) incorrect++;
+        }
+
+        assertNotEquals(0, incorrect);
+
+        RecommendationModel<Integer, Integer> updatedMdl = trainer.update(new LocalDatasetBuilder<>(data, 10), mdl);
+
+        incorrect = 0;
+        for (ObjectSubjectRatingTriplet<Integer, Integer> triplet : toList(ratings)) {
+            double prediction = Math.round(updatedMdl.predict(triplet));
             if (Math.abs(prediction - triplet.getRating()) >= 1e-5) incorrect++;
         }
 
