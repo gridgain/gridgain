398759ff42d4610cf4d42266ddee01bb06087af1
ktkalenko <ktkalenko@gridgain.com>
2019-08-28 13:24:25 +0300

    GG-20495 Rework GridCommandHandlerTest.
            (cherry picked from commit 932b66bd5f322826f42704278e178c7c3f7ba6f7)

--- modules/core/src/test/java/org/apache/ignite/testsuites/IgniteBasicWithPersistenceTestSuite.java
+++ modules/core/src/test/java/org/apache/ignite/testsuites/IgniteBasicWithPersistenceTestSuite.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2019 GridGain Systems, Inc. and Contributors.
- * 
+ *
  * Licensed under the GridGain Community Edition License (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     https://www.gridgain.com/products/software/community-edition/gridgain-community-edition-license
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -38,8 +38,9 @@ import org.apache.ignite.internal.processors.cache.persistence.SingleNodePersist
 import org.apache.ignite.internal.processors.service.ServiceDeploymentOnActivationTest;
 import org.apache.ignite.internal.processors.service.ServiceDeploymentOutsideBaselineTest;
 import org.apache.ignite.marshaller.GridMarshallerMappingConsistencyTest;
-import org.apache.ignite.util.GridCommandHandlerSslTest;
+import org.apache.ignite.util.GridCommandHandlerClusterByClassTest;
 import org.apache.ignite.util.GridCommandHandlerTest;
+import org.apache.ignite.util.GridCommandHandlerSslTest;
 import org.apache.ignite.util.GridInternalTaskUnusedWalSegmentsTest;
 import org.jetbrains.annotations.Nullable;
 import org.junit.runner.RunWith;
@@ -75,6 +76,7 @@ public class IgniteBasicWithPersistenceTestSuite extends TestSuite {
         suite.addTest(new JUnit4TestAdapter(CheckpointReadLockFailureTest.class));
 
         suite.addTest(new JUnit4TestAdapter(GridCommandHandlerTest.class));
+        suite.addTest(new JUnit4TestAdapter(GridCommandHandlerClusterByClassTest.class));
         suite.addTest(new JUnit4TestAdapter(GridCommandHandlerSslTest.class));
         suite.addTest(new JUnit4TestAdapter(GridInternalTaskUnusedWalSegmentsTest.class));
 
