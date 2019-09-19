eff18039b62c80ce48180efc2482eac62136cc0a
Pavel Tupitsyn <ptupitsyn@apache.org>
2019-09-19 19:07:47 +0300

    suppress inspections

--- modules/platforms/dotnet/Apache.Ignite.Core/Impl/Unmanaged/UnmanagedThread.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core/Impl/Unmanaged/UnmanagedThread.cs
@@ -18,6 +18,7 @@ namespace Apache.Ignite.Core.Impl.Unmanaged
 {
     using System;
     using System.Diagnostics;
+    using System.Diagnostics.CodeAnalysis;
     using System.Runtime.InteropServices;
 
     /// <summary>
@@ -148,12 +149,15 @@ namespace Apache.Ignite.Core.Impl.Unmanaged
             // ReSharper disable once InconsistentNaming
             public const int FLS_OUT_OF_INDEXES = -1;
 
+            [SuppressMessage("Microsoft.Design", "CA1060:MovePInvokesToNativeMethodsClass", Justification = "Reviewed.")]
             [DllImport("kernel32.dll", SetLastError = true)]
             public static extern int FlsAlloc(IntPtr destructorCallback);
 
+            [SuppressMessage("Microsoft.Design", "CA1060:MovePInvokesToNativeMethodsClass", Justification = "Reviewed.")]
             [DllImport("kernel32.dll", SetLastError = true)]
             public static extern bool FlsFree(int dwFlsIndex);
 
+            [SuppressMessage("Microsoft.Design", "CA1060:MovePInvokesToNativeMethodsClass", Justification = "Reviewed.")]
             [DllImport("kernel32.dll", SetLastError = true)]
             [return: MarshalAs(UnmanagedType.Bool)]
             public static extern bool FlsSetValue(int dwFlsIndex, IntPtr lpFlsData);
@@ -164,12 +168,15 @@ namespace Apache.Ignite.Core.Impl.Unmanaged
         /// </summary>
         private static class NativeMethodsLinux
         {
+            [SuppressMessage("Microsoft.Design", "CA1060:MovePInvokesToNativeMethodsClass", Justification = "Reviewed.")]
             [DllImport("libcoreclr.so")]
             public static extern int pthread_key_create(IntPtr key, IntPtr destructorCallback);
 
+            [SuppressMessage("Microsoft.Design", "CA1060:MovePInvokesToNativeMethodsClass", Justification = "Reviewed.")]
             [DllImport("libcoreclr.so")]
             public static extern int pthread_key_delete(int key);
 
+            [SuppressMessage("Microsoft.Design", "CA1060:MovePInvokesToNativeMethodsClass", Justification = "Reviewed.")]
             [DllImport("libcoreclr.so")]
             public static extern int pthread_setspecific(int key, IntPtr value);
 
@@ -190,12 +197,15 @@ namespace Apache.Ignite.Core.Impl.Unmanaged
         /// </summary>
         private static class NativeMethodsMacOs
         {
+            [SuppressMessage("Microsoft.Design", "CA1060:MovePInvokesToNativeMethodsClass", Justification = "Reviewed.")]
             [DllImport("libSystem.dylib")]
             public static extern int pthread_key_create(IntPtr key, IntPtr destructorCallback);
 
+            [SuppressMessage("Microsoft.Design", "CA1060:MovePInvokesToNativeMethodsClass", Justification = "Reviewed.")]
             [DllImport("libSystem.dylib")]
             public static extern int pthread_key_delete(int key);
 
+            [SuppressMessage("Microsoft.Design", "CA1060:MovePInvokesToNativeMethodsClass", Justification = "Reviewed.")]
             [DllImport("libSystem.dylib")]
             public static extern int pthread_setspecific(int key, IntPtr value);
         }
