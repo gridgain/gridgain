f83dbe4fa85e43dfad420ecf0d967762d8694118
Pavel Tupitsyn <ptupitsyn@apache.org>
2019-09-21 17:46:19 +0300

    Add disposable wrapper for system properties

---
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests/JavaSystemProperty.cs
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2019 GridGain Systems, Inc. and Contributors.
+ *
+ * Licensed under the GridGain Community Edition License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.gridgain.com/products/software/community-edition/gridgain-community-edition-license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Apache.Ignite.Core.Tests
+{
+    using System;
+    using Apache.Ignite.Core.Impl.Common;
+    using Apache.Ignite.Core.Impl.Unmanaged;
+
+    /// <summary>
+    /// Java system property utilities.
+    /// </summary>
+    public static class JavaSystemProperty
+    {
+        /// <summary>
+        /// Sets the environment variable and returns an object that resets the variable back when disposed.
+        /// </summary>
+        /// <param name="name">Name.</param>
+        /// <param name="value">Value.</param>
+        /// <returns>Object that resets environment variable to previous value when disposed.</returns>
+        public static IDisposable Set(string name, string value)
+        {
+            IgniteArgumentCheck.NotNullOrEmpty(name, "name");
+
+            var oldValue = UnmanagedUtils.SystemSetProperty(name, value);
+
+            return new DisposeAction(() => UnmanagedUtils.SystemSetProperty(name, oldValue));
+        }
+    }
+}
