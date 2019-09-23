1f1195666648f4455c9f5a47f7655044d3d66549
Aleksei Scherbakov <alexey.scherbakoff@gmail.com>
2019-09-23 19:57:19 +0300

    GG-24235 Fix assertion on update to renting partition.

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/transactions/TxPartitionCounterStateConsistencyTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/transactions/TxPartitionCounterStateConsistencyTest.java
@@ -31,6 +31,7 @@ import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.LongAdder;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import org.apache.ignite.Ignite;
@@ -241,6 +242,76 @@ public class TxPartitionCounterStateConsistencyTest extends TxPartitionCounterSt
                     doSleep(15_000);
 
                     startGrid(name);
+
+                    awaitPartitionMapExchange();
+                }
+                catch (Exception e) {
+                    fail(X.getFullStackTrace(e));
+                }
+            }
+        }, 1, "node-restarter");
+
+        doRandomUpdates(r, prim, primaryKeys, cache, stop).get();
+        fut.get();
+
+        assertPartitionsSame(idleVerify(prim, DEFAULT_CACHE_NAME));
+    }
+
+    /**
+     * Test primary-backup partitions consistency while restarting backup nodes under load with changing BLT.
+     */
+    @Test
+    public void testPartitionConsistencyWithBackupRestart_ChangeBLT() throws Exception {
+        backups = 2;
+
+        final int srvNodes = SERVER_NODES + 1; // Add one non-owner node to test to increase entropy.
+
+        Ignite prim = startGrids(srvNodes);
+
+        prim.cluster().active(true);
+
+        IgniteCache<Object, Object> cache = prim.cache(DEFAULT_CACHE_NAME);
+
+        List<Integer> primaryKeys = primaryKeys(cache, 10_000);
+
+        List<Ignite> backups = backupNodes(primaryKeys.get(0), DEFAULT_CACHE_NAME);
+
+        assertFalse(backups.contains(prim));
+
+        long stop = U.currentTimeMillis() + GridTestUtils.SF.applyLB(2 * 60_000, 30_000);
+
+        long seed = System.nanoTime();
+
+        log.info("Seed: " + seed);
+
+        Random r = new Random(seed);
+
+        assertTrue(prim == grid(0));
+
+        IgniteInternalFuture<?> fut = multithreadedAsync(() -> {
+            while (U.currentTimeMillis() < stop) {
+                doSleep(1_000);
+
+                Ignite restartNode = grid(1 + r.nextInt(backups.size()));
+
+                assertFalse(prim == restartNode);
+
+                String name = restartNode.name();
+
+                stopGrid(true, name);
+
+                try {
+                    waitForTopology(SERVER_NODES);
+
+                    resetBaselineTopology();
+
+                    awaitPartitionMapExchange();
+
+                    doSleep(5_000);
+
+                    startGrid(name);
+
+                    resetBaselineTopology();
 
                     awaitPartitionMapExchange();
                 }
