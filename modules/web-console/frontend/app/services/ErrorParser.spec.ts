f33dd87dde08d839c87f7b8d14dacb477c99fa51
vesisko@gridgain.com <vesisko@gridgain.com>
2019-09-24 15:14:44 +0300

    GG-23802 Added test.

--- modules/web-console/frontend/app/services/ErrorParser.spec.ts
+++ modules/web-console/frontend/app/services/ErrorParser.spec.ts
@@ -28,25 +28,25 @@ const TEST_ERROR = {
     message: `Failed to handle request: [req=EXE, taskName=test.TaskName, params=[], err=Final cause, trace=...]
     at test.Class1.function(Class1.java:1)
     ... 9 more 
-'Caused by: class test.Exception2: Final cause
+Caused by: class test.Exception2: Final cause
     at test.Class2.function(Class2.java:1)
     ... 14 more
-'Caused by: class test.Exception3: Duplicate cause
+Caused by: class test.Exception3: Duplicate cause
     at test.Class3.function(Class3.java:1)
     ... 14 more
-'Caused by: class test.Exception4: Duplicate cause
+Caused by: class test.Exception4: Duplicate cause
     at test.Class4.function(Class4.java:1)
     ... 29 more
-'Caused by: class test.Exception5: Root cause
+Caused by: class test.Exception5: Root cause
     at test.Class5.function(Class5.java:1)
     ... 40 more
-']`
+]`
 };
 
 const FULL_ERROR_MSG = '[Exception1] Failed to handle request: [req=EXE, taskName=test.TaskName, params=[], err=Final cause';
 const ERROR_MSG = 'Test: Final cause';
 
-suite.only('Error parser service', () => {
+suite('Error parser service', () => {
     test('Error parsing', () => {
         assert.equal(FULL_ERROR_MSG, parser.extractFullMessage(TEST_ERROR));
 
