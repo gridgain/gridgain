fb0791e0701feb2aaf5b7b8e8a0de69657d60ec8
Pavel Tupitsyn <ptupitsyn@apache.org>
2019-09-26 12:12:42 +0300

    Add test

---
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests.DotNetCore/ThinClient/Cache/CacheTestAsyncAwait.cs
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2019 GridGain Systems, Inc. and Contributors.
+ *
+ * Licensed under the GridGain Community Edition License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.gridgain.com/products/software/community-edition/gridgain-community-edition-license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Apache.Ignite.Core.Tests.DotNetCore.ThinClient.Cache
+{
+    using System.Threading.Tasks;
+    using Apache.Ignite.Core.Tests.Client;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Tests cache operations with async/await.
+    /// </summary>
+    public class CacheTestAsyncAwait : ClientTestBase
+    {
+        [Test]
+        public async Task TestAsyncContinuation()
+        {
+            var cache = GetCache<int>();
+            await cache.PutAsync(1, 1);
+
+            // This continues on the async result thread and can cause deadlock.
+            cache.PutAsync(2, 2).Wait();
+            Assert.AreEqual(2, cache.Get(2));
+        }
+    }
+}
