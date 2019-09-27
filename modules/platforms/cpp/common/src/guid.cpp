1920a656c59e263e3d81d79f6048c3cf28c1bb11
Igor Sapego <isapego@apache.org>
2019-09-27 18:14:59 +0300

    GG-24441: C++ compilation fix

--- modules/platforms/cpp/common/src/guid.cpp
+++ modules/platforms/cpp/common/src/guid.cpp
@@ -1,12 +1,12 @@
 /*
  * Copyright 2019 GridGain Systems, Inc. and Contributors.
- * 
+ *
  * Licensed under the GridGain Community Edition License (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     https://www.gridgain.com/products/software/community-edition/gridgain-community-edition-license
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -57,8 +57,41 @@ namespace ignite
         return static_cast<int32_t>(hilo >> 32) ^ static_cast<int32_t>(hilo);
     }
 
+    int64_t Guid::Compare(const Guid& other) const
+    {
+        if (most != other.most)
+            return most - other.most;
+
+        return least - other.least;
+    }
+
     bool operator==(const Guid& val1, const Guid& val2)
     {
-        return val1.least == val2.least && val1.most == val2.most;
+        return val1.Compare(val2) == 0;
+    }
+
+    bool operator!=(const Guid& val1, const Guid& val2)
+    {
+        return val1.Compare(val2) != 0;
+    }
+
+    bool operator<(const Guid& val1, const Guid& val2)
+    {
+        return val1.Compare(val2) < 0;
+    }
+
+    bool operator<=(const Guid& val1, const Guid& val2)
+    {
+        return val1.Compare(val2) <= 0;
+    }
+
+    bool operator>(const Guid& val1, const Guid& val2)
+    {
+        return val1.Compare(val2) > 0;
+    }
+
+    bool operator>=(const Guid& val1, const Guid& val2)
+    {
+        return val1.Compare(val2) >= 0;
     }
 }
