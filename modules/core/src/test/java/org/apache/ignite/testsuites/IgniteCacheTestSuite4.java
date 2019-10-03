ba360892565ebc6f00937266ec2b2836d1740692
ktkalenko <ktkalenko@gridgain.com>
2019-10-03 17:01:36 +0300

    GG-24286 Extend test coverage for GG-19117
    (cherry picked from commit e43af82924ff1685b488ea938de153f2fd742ec8)
    (cherry picked from commit 7ac5a4ecf07916428d21baf341edd01a5a0f9401)

--- modules/core/src/test/java/org/apache/ignite/testsuites/IgniteCacheTestSuite4.java
+++ modules/core/src/test/java/org/apache/ignite/testsuites/IgniteCacheTestSuite4.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2019 GridGain Systems, Inc. and Contributors.
- * 
+ *
  * Licensed under the GridGain Community Edition License (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     https://www.gridgain.com/products/software/community-edition/gridgain-community-edition-license
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -55,6 +55,7 @@ import org.apache.ignite.internal.processors.cache.GridCacheMultinodeUpdateAtomi
 import org.apache.ignite.internal.processors.cache.GridCacheMultinodeUpdateNearEnabledNoBackupsSelfTest;
 import org.apache.ignite.internal.processors.cache.GridCacheMultinodeUpdateNearEnabledSelfTest;
 import org.apache.ignite.internal.processors.cache.GridCacheMultinodeUpdateSelfTest;
+import org.apache.ignite.internal.processors.cache.GridCacheProcessorActiveTxTest;
 import org.apache.ignite.internal.processors.cache.GridCacheStoreManagerDeserializationTest;
 import org.apache.ignite.internal.processors.cache.GridCacheVersionMultinodeTest;
 import org.apache.ignite.internal.processors.cache.GridLocalCacheStoreManagerDeserializationTest;
@@ -361,6 +362,8 @@ public class IgniteCacheTestSuite4 {
         addTestIfNeeded(suite, CacheEventWithTxLabelTest.class, ignoredTests);
 
         addTestIfNeeded(suite, NearCacheNoMessagesTest.class, ignoredTests);
+
+        addTestIfNeeded(suite, GridCacheProcessorActiveTxTest.class, ignoredTests);
 
         return suite;
     }
