1b3545a89cbb6f0f6fc9fbc458edce8b253b8e9d
Pavel Tupitsyn <ptupitsyn@apache.org>
2019-10-08 00:47:10 +0300

    Cleanup

--- modules/platforms/dotnet/Apache.Ignite.Core/Impl/IgniteUtils.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core/Impl/IgniteUtils.cs
@@ -18,7 +18,6 @@ namespace Apache.Ignite.Core.Impl
 {
     using System;
     using System.Collections.Generic;
-    using System.IO;
     using System.Reflection;
     using System.Runtime.InteropServices;
     using System.Text;
