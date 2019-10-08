0d2f39f463255ae76abbc636aff4a0a9389f7671
Andrey Novikov <anovikov@gridgain.com>
2019-10-08 13:09:17 +0700

    GG-24325 Mute warning on Java 9+.

--- modules/web-console/bin/web-console.bat
+++ modules/web-console/bin/web-console.bat
@@ -117,6 +117,7 @@ if %MAJOR_JAVA_VER% == 8 (
 if %MAJOR_JAVA_VER% GEQ 9 if %MAJOR_JAVA_VER% LSS 11 (
     set JVM_OPTS= ^
     -XX:+AggressiveOpts ^
+    --add-opens=java.base/java.lang=ALL-UNNAMED ^
     --add-exports=java.base/jdk.internal.misc=ALL-UNNAMED ^
     --add-exports=java.base/sun.nio.ch=ALL-UNNAMED ^
     --add-exports=java.management/com.sun.jmx.mbeanserver=ALL-UNNAMED ^
@@ -129,6 +130,7 @@ if %MAJOR_JAVA_VER% GEQ 9 if %MAJOR_JAVA_VER% LSS 11 (
 
 if %MAJOR_JAVA_VER% GEQ 11 (
     set JVM_OPTS= ^
+    --add-opens=java.base/java.lang=ALL-UNNAMED ^
     --add-exports=java.base/jdk.internal.misc=ALL-UNNAMED ^
     --add-exports=java.base/sun.nio.ch=ALL-UNNAMED ^
     --add-exports=java.management/com.sun.jmx.mbeanserver=ALL-UNNAMED ^
