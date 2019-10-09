4e7e45d84167635f60e5d2396f7331c3424f8023
Andrei Aleksandrov <aealexsandrov@gmail.com>
2019-10-09 13:24:59 +0300

    IgniteClientReconnectEventHandlingTest was excluded because of GG-24771 + copyrights were fixed.
    (cherry picked from commit e606eee)

--- modules/core/src/test/java/org/apache/ignite/spi/discovery/IgniteClientReconnectEventHandlingTest.java
+++ modules/core/src/test/java/org/apache/ignite/spi/discovery/IgniteClientReconnectEventHandlingTest.java
@@ -1,12 +1,11 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ * Copyright 2019 GridGain Systems, Inc. and Contributors.
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the GridGain Community Edition License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.gridgain.com/products/software/community-edition/gridgain-community-edition-license
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -29,6 +28,7 @@ import org.apache.ignite.lang.IgniteFuture;
 import org.apache.ignite.lang.IgnitePredicate;
 import org.apache.ignite.spi.discovery.tcp.TcpDiscoverySpi;
 import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
@@ -41,6 +41,7 @@ import static org.apache.ignite.testframework.GridTestUtils.waitForCondition;
 /**
  * Checks that client will not process previous cluster events after reconnect.
  */
+@Ignore("https://ggsystems.atlassian.net/browse/GG-24771")
 public class IgniteClientReconnectEventHandlingTest extends GridCommonAbstractTest {
     /** */
     private final CountDownLatch latch = new CountDownLatch(1);
