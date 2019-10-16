72ced595df16984c9531328c537f5203059d6dde
Andrey Novikov <anovikov@gridgain.com>
2019-10-16 09:47:46 +0700

    GG-24827 [WC] Can't download Agent if WC was started from arbitrary directory. (#617)

--- modules/web-console/web-console-server/src/test/java/org/apache/ignite/console/services/AdminServiceTest.java
+++ modules/web-console/web-console-server/src/test/java/org/apache/ignite/console/services/AdminServiceTest.java
@@ -16,7 +16,7 @@
 
 package org.apache.ignite.console.services;
 
-
+import java.util.UUID;
 import org.apache.ignite.console.MockConfiguration;
 import org.apache.ignite.console.dto.Account;
 import org.apache.ignite.console.event.Event;
@@ -32,8 +32,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.UUID;
 
 import static org.apache.ignite.console.event.AccountEventType.ACCOUNT_CREATE_BY_ADMIN;
 import static org.apache.ignite.console.event.AccountEventType.ACCOUNT_DELETE;
