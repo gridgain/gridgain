9a1a5efd6cb82d6f2e40b5c2992485e043be2101
vesisko@gridgain.com <vesisko@gridgain.com>
2019-10-16 17:28:13 +0300

    GG-24882 First letter in first word of error cause converted to upper case.

--- modules/web-console/frontend/app/services/ErrorParser.service.ts
+++ modules/web-console/frontend/app/services/ErrorParser.service.ts
@@ -70,8 +70,7 @@ export default class {
                     const msgStart = err.message.indexOf(': ', causeIdx + CAUSE_STR.length) + 2;
                     const causeEndLine = err.message.indexOf('\n', msgStart);
                     const msgEnd = err.message.indexOf('[', msgStart);
-                    const cause = err.message.charAt(msgStart).toUpperCase() +
-                        err.message.substring(msgStart + 1, msgEnd >= 0 && msgEnd < causeEndLine ? msgEnd : causeEndLine);
+                    const cause = _.upperFirst(err.message.substring(msgStart, msgEnd >= 0 && msgEnd < causeEndLine ? msgEnd : causeEndLine));
 
                     if (causes && causes[0] !== cause)
                         causes.unshift(cause);
