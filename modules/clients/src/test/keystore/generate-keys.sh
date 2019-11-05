3429b727e9d73adea93f9fea849e590de237fdeb
Denis Mekhanikov <dmekhanikov@gmail.com>
2019-11-05 21:20:34 +0300

    GG-23137 Move CA generation script to a separate file.

--- modules/clients/src/test/keystore/generate-keys.sh
+++ modules/clients/src/test/keystore/generate-keys.sh
@@ -19,36 +19,6 @@ set -e
 
 pwd="123456"
 
-function createCa {
-    ca_name=$1
-
-    openssl req -new -newkey rsa:2048 -nodes -out ${ca_name}.csr -keyout ${ca_name}.key \
-        -subj "/emailAddress=${ca_name}@ignite.apache.org/CN=${ca_name}/OU=Dev/O=Ignite/L=SPb/ST=SPb/C=RU"
-
-    openssl x509 -trustout -signkey ${ca_name}.key -days 7305 -req -in ${ca_name}.csr -out ${ca_name}.pem
-
-    rm ${ca_name}.csr
-
-    touch ${ca_name}-index.txt
-    echo 01 > ${ca_name}-serial
-    echo "[ ca ]
-default_ca = ${ca_name}
-
-[ ${ca_name} ]
-dir=ca
-certificate = \$dir/${ca_name}.pem
-database = \$dir/${ca_name}-index.txt
-private_key = \$dir/${ca_name}.key
-new_certs_dir = \$dir/certs
-default_md = sha1
-policy = policy_match
-serial = \$dir/${ca_name}-serial
-default_days = 365
-
-[policy_match]
-commonName = supplied" > ${ca_name}.cnf
-}
-
 #
 # Create artifacts for specified name: key pair-> cert request -> ca-signed certificate.
 # Save private key and CA-signed certificate into key storages: PEM, JKS, PFX (PKCS12).
@@ -108,41 +78,10 @@ function createStore {
 	echo Update the keystore, ${artifact}.jks, by importing the full certificate chain for the ${artifact}.
 	keytool -import -alias ${artifact} -file ${artifact}.chain -keypass ${pwd} -noprompt -trustcacerts -keystore ${artifact}.jks -storepass ${pwd}
 
-	echo
-	echo Generate PKCS12 storage for the private key and certificate chain.
-	keytool -importkeystore \
-		-srcstoretype JKS -deststoretype PKCS12 \
-		-srckeystore ${artifact}.jks -destkeystore ${artifact}.pfx \
-		-srcstorepass ${pwd} -deststorepass ${pwd} \
-		-srcalias ${artifact} -destalias ${artifact} \
-		-srckeypass ${pwd} -destkeypass ${pwd} \
-		-noprompt
-
-	echo
-	echo Generate PEM storage for the private key and certificate chain.
-	openssl pkcs12 \
-		-in ${artifact}.pfx -out ${artifact}.pem \
-		-passin pass:${pwd} -passout pass:${pwd}
-
-	rm -f ${artifact}.chain ${artifact}.csr ${artifact}.pfx ${artifact}.pem
+	rm -f ${artifact}.chain ${artifact}.csr ${artifact}.pem
 }
 
 mkdir -p ca/certs
-
-cd ca
-
-createCa oneca
-createCa twoca
-createCa threeca
-
-cd ..
-
-keytool -import -noprompt -file ca/oneca.pem -alias oneca -keypass ${pwd} -storepass ${pwd} -keystore trust-one.jks
-keytool -import -noprompt -file ca/twoca.pem -alias twoca -keypass ${pwd} -storepass ${pwd} -keystore trust-two.jks
-keytool -import -noprompt -file ca/threeca.pem -alias threeca -keypass ${pwd} -storepass ${pwd} -keystore trust-three.jks
-
-keytool -import -noprompt -file ca/oneca.pem -alias oneca -keypass ${pwd} -storepass ${pwd} -keystore trust-both.jks
-keytool -import -noprompt -file ca/twoca.pem -alias twoca -keypass ${pwd} -storepass ${pwd} -keystore trust-both.jks
 
 createStore cluster oneca
 createStore thinClient twoca
@@ -154,5 +93,3 @@ createStore node01 oneca
 createStore node02 twoca
 createStore node03 twoca
 createStore node02old twoca true
-
-rm -rf ca
