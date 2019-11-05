58a4c9c802a7cf5fc7b9e6d752e52b4aaaed61de
Igor Sapego <isapego@apache.org>
2019-11-05 07:02:34 +0300

    GG-25386: Implemented .NET tests

--- modules/platforms/dotnet/Apache.Ignite.Core.Tests/Cache/Query/CacheDmlQueriesTestSchema.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests/Cache/Query/CacheDmlQueriesTestSchema.cs
@@ -20,6 +20,7 @@ namespace Apache.Ignite.Core.Tests.Cache.Query
     using System;
     using System.Collections.Generic;
     using Apache.Ignite.Core.Cache.Query;
+    using Apache.Ignite.Core.Common;
     using NUnit.Framework;
 
     /// <summary>
@@ -29,6 +30,13 @@ namespace Apache.Ignite.Core.Tests.Cache.Query
     {
         /// Table name
         private const string TBL_NAME = "T1";
+
+        private const string SCHEMA_NAME_1 = "SCHEMA_1";
+        private const string SCHEMA_NAME_2 = "SCHEMA_2";
+        private const string SCHEMA_NAME_3 = "ScHeMa3";
+        private const string SCHEMA_NAME_4 = "SCHEMA_4";
+
+        private static readonly string Q_SCHEMA_NAME_3 = '"' + SCHEMA_NAME_3 + '"';
 
         /// <summary>
         /// Sets up test fixture.
@@ -77,6 +85,101 @@ namespace Apache.Ignite.Core.Tests.Cache.Query
             int i = 0;
 
             ExecuteStmtsAndVerify(() => ((++i & 1) == 0));
+        }
+
+        /// <summary>
+        /// Create and drop non-existing schema.
+        /// </summary>
+        [Test]
+        public void TestCreateDropNonExistingSchema()
+        {
+            Assert.Throws<IgniteException>(() => 
+                Sql("CREATE TABLE UNKNOWN_SCHEMA." + TBL_NAME + "(id INT PRIMARY KEY, val INT)"));
+
+            Assert.Throws<IgniteException>(() => Sql("DROP TABLE UNKNOWN_SCHEMA." + TBL_NAME));
+        }
+
+        /// <summary>
+        /// Create tables in different schemas for same cache.
+        /// </summary>
+        [Test]
+        public void TestCreateTblsInDiffSchemasForSameCache()
+        {
+            const string testCache = "cache1";
+
+            Sql("CREATE TABLE " + SCHEMA_NAME_1 + '.' + TBL_NAME
+                + " (s1_key INT PRIMARY KEY, s1_val INT) WITH \"cache_name=" + testCache + "\"");
+
+            Assert.Throws<IgniteException>(
+                () => Sql("CREATE TABLE " + SCHEMA_NAME_2 + '.' + TBL_NAME
+                    + " (s1_key INT PRIMARY KEY, s2_val INT) WITH \"cache_name=" + testCache + "\"")
+            );
+
+            Sql("DROP TABLE " + SCHEMA_NAME_1 + '.' + TBL_NAME);
+        }
+
+        /// <summary>
+        /// Basic test with different schemas.
+        /// </summary>
+        [Test]
+        public void TestBasicOpsDiffSchemas()
+        {
+            Sql("CREATE TABLE " + SCHEMA_NAME_1 + '.' + TBL_NAME + " (s1_key INT PRIMARY KEY, s1_val INT)");
+            Sql("CREATE TABLE " + SCHEMA_NAME_2 + '.' + TBL_NAME + " (s2_key INT PRIMARY KEY, s2_val INT)");
+            Sql("CREATE TABLE " + Q_SCHEMA_NAME_3 + '.' + TBL_NAME + " (s3_key INT PRIMARY KEY, s3_val INT)");
+            Sql("CREATE TABLE " + SCHEMA_NAME_4 + '.' + TBL_NAME + " (s4_key INT PRIMARY KEY, s4_val INT)");
+
+            Sql("INSERT INTO " + SCHEMA_NAME_1 + '.' + TBL_NAME + " (s1_key, s1_val) VALUES (1, 2)");
+            Sql("INSERT INTO " + SCHEMA_NAME_2 + '.' + TBL_NAME + " (s2_key, s2_val) VALUES (1, 2)");
+            Sql("INSERT INTO " + Q_SCHEMA_NAME_3 + '.' + TBL_NAME + " (s3_key, s3_val) VALUES (1, 2)");
+            Sql("INSERT INTO " + SCHEMA_NAME_4 + '.' + TBL_NAME + " (s4_key, s4_val) VALUES (1, 2)");
+
+            Sql("UPDATE " + SCHEMA_NAME_1 + '.' + TBL_NAME + " SET s1_val = 5");
+            Sql("UPDATE " + SCHEMA_NAME_2 + '.' + TBL_NAME + " SET s2_val = 5");
+            Sql("UPDATE " + Q_SCHEMA_NAME_3 + '.' + TBL_NAME + " SET s3_val = 5");
+            Sql("UPDATE " + SCHEMA_NAME_4 + '.' + TBL_NAME + " SET s4_val = 5");
+
+            Sql("DELETE FROM " + SCHEMA_NAME_1 + '.' + TBL_NAME);
+            Sql("DELETE FROM " + SCHEMA_NAME_2 + '.' + TBL_NAME);
+            Sql("DELETE FROM " + Q_SCHEMA_NAME_3 + '.' + TBL_NAME);
+            Sql("DELETE FROM " + SCHEMA_NAME_4 + '.' + TBL_NAME);
+
+            Sql("CREATE INDEX t1_idx_1 ON " + SCHEMA_NAME_1 + '.' + TBL_NAME + "(s1_val)");
+            Sql("CREATE INDEX t1_idx_1 ON " + SCHEMA_NAME_2 + '.' + TBL_NAME + "(s2_val)");
+            Sql("CREATE INDEX t1_idx_1 ON " + Q_SCHEMA_NAME_3 + '.' + TBL_NAME + "(s3_val)");
+            Sql("CREATE INDEX t1_idx_1 ON " + SCHEMA_NAME_4 + '.' + TBL_NAME + "(s4_val)");
+
+            Sql("SELECT * FROM " + SCHEMA_NAME_1 + '.' + TBL_NAME);
+            Sql("SELECT * FROM " + SCHEMA_NAME_2 + '.' + TBL_NAME);
+            Sql("SELECT * FROM " + Q_SCHEMA_NAME_3 + '.' + TBL_NAME);
+            Sql("SELECT * FROM " + SCHEMA_NAME_4 + '.' + TBL_NAME);
+
+            Sql("SELECT * FROM " + SCHEMA_NAME_1 + '.' + TBL_NAME
+                + " JOIN " + SCHEMA_NAME_2 + '.' + TBL_NAME
+                + " JOIN " + Q_SCHEMA_NAME_3 + '.' + TBL_NAME
+                + " JOIN " + SCHEMA_NAME_4 + '.' + TBL_NAME);
+
+            VerifyTables();
+
+            Sql("DROP TABLE " + SCHEMA_NAME_1 + '.' + TBL_NAME);
+            Sql("DROP TABLE " + SCHEMA_NAME_2 + '.' + TBL_NAME);
+            Sql("DROP TABLE " + Q_SCHEMA_NAME_3 + '.' + TBL_NAME);
+            Sql("DROP TABLE " + SCHEMA_NAME_4 + '.' + TBL_NAME);
+        }
+
+        /// <summary>
+        /// Verify tables.
+        /// </summary>
+        private void VerifyTables()
+        {
+            Sql("SELECT SCHEMA_NAME, KEY_ALIAS FROM SYS.TABLES ORDER BY SCHEMA_NAME", res => {
+                res.Equals(new List<List<object>> {
+                    new List<object>{ SCHEMA_NAME_1, "S1_KEY" },
+                    new List<object>{ SCHEMA_NAME_2, "S2_KEY" },
+                    new List<object>{ SCHEMA_NAME_4, "S4_KEY" },
+                    new List<object>{ SCHEMA_NAME_3, "S3_KEY" }
+                });
+            });
         }
 
         /// <summary>
