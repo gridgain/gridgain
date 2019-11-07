a49151fe51f20e37df03f66c626e1fe1506c76a8
Denis Mekhanikov <dmekhanikov@gmail.com>
2019-11-07 19:36:06 +0300

    GG-23137 Add license header to CA configuration files.

--- modules/clients/src/test/keystore/generate-ca.sh
+++ modules/clients/src/test/keystore/generate-ca.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 #
 # Copyright 2019 GridGain Systems, Inc. and Contributors.
 #
@@ -42,7 +43,24 @@ function createCa {
     echo Create auxiliary files for ${ca_name}.
     touch ${ca_name}-index.txt
     echo 01 > ${ca_name}-serial
-    echo "[ ca ]
+    echo "
+#
+# Copyright 2019 GridGain Systems, Inc. and Contributors.
+#
+# Licensed under the GridGain Community Edition License (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.gridgain.com/products/software/community-edition/gridgain-community-edition-license
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+[ ca ]
 default_ca = ${ca_name}
 
 [ ${ca_name} ]
