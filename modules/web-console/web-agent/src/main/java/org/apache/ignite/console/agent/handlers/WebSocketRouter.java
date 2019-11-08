0e0c0372718f3786b2d90bccc1bf87b3efca0be3
Andrey Novikov <anovikov@gridgain.com>
2019-11-08 11:07:58 +0700

    GG-25620 Fixed thread leaks on reconnect.

--- modules/web-console/web-agent/src/main/java/org/apache/ignite/console/agent/handlers/WebSocketRouter.java
+++ modules/web-console/web-agent/src/main/java/org/apache/ignite/console/agent/handlers/WebSocketRouter.java
@@ -186,7 +186,6 @@ public class WebSocketRouter implements AutoCloseable {
         if (client != null) {
             try {
                 client.stop();
-                client.destroy();
             }
             catch (Exception ignored) {
                 // No-op.
@@ -224,13 +223,15 @@ public class WebSocketRouter implements AutoCloseable {
                 return;
 
             HttpClient httpClient = new HttpClient(createServerSslFactory(cfg));
+            httpClient.setName("http-client");
+            httpClient.addBean(httpClient.getExecutor());
 
             // TODO GG-18379 Investigate how to establish native websocket connection with proxy.
             configureProxy(httpClient, cfg.serverUri());
 
             client = new WebSocketClient(httpClient);
+            client.addBean(httpClient);
 
-            httpClient.start();
             client.start();
             client.connect(this, URI.create(cfg.serverUri()).resolve(AGENTS_PATH)).get(5L, TimeUnit.SECONDS);
 
