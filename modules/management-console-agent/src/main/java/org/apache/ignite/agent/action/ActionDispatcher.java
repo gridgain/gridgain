7ad15f6d9a384502a8e2811b5601e9607ec0d20a
Andrey Novikov <anovikov@gridgain.com>
2019-11-08 17:29:48 +0700

    GG-24671 Attempt to fix agent disconnect 4.

--- modules/management-console-agent/src/main/java/org/apache/ignite/agent/action/ActionDispatcher.java
+++ modules/management-console-agent/src/main/java/org/apache/ignite/agent/action/ActionDispatcher.java
@@ -127,6 +127,11 @@ public class ActionDispatcher implements AutoCloseable {
         catch (InvocationTargetException e) {
             return completeFutureWithException(e.getTargetException());
         }
+        catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+
+            return completeFutureWithException(e);
+        }
         catch (Exception e) {
             return completeFutureWithException(e);
         }
