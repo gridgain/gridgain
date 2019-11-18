689795c95ea1e1e8e659adb1c0ddc757cd6bd692
Pavel Tupitsyn <ptupitsyn@apache.org>
2019-11-18 18:28:57 +0300

    Implementing release verification script

---
+++ modules/platforms/dotnet/release/verify-nuget.ps1
@@ -0,0 +1,35 @@
+<#
+Copyright 2019 GridGain Systems, Inc. and Contributors.
+
+Licensed under the GridGain Community Edition License (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.gridgain.com/products/software/community-edition/gridgain-community-edition-license
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+#>
+
+ <#
+
+.SYNOPSIS
+Verifies release NuGet packages.
+
+.DESCRIPTION
+Creates a project with .NET Core CLI, installs all packages, runs simple code to start Ignite node, verifies exit code.
+
+Requirements:
+* .NET Core 2.0+
+* JDK 8+
+
+.PARAMETER packageDir
+Directory with nupkg files to verify, defaults to ..\nupkg.
+
+
+#>
+
+$
