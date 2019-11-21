ac443bc358503d800d21162817bfc57d3d8e285b
vesisko@gridgain.com <vesisko@gridgain.com>
2019-11-21 16:51:14 +0300

    GG-24887 Test valid loading of JDBC driver list.

--- modules/web-console/frontend/app/modules/agent/AgentManager.service.ts
+++ modules/web-console/frontend/app/modules/agent/AgentManager.service.ts
@@ -459,6 +459,8 @@ export default class AgentManager {
                 if (__dbg)
                     console.log('Received response: ', evt);
 
+                console.log(evt);
+
                 if (evt.eventType === 'error')
                     latch.reject(evt.payload);
                 else if (evt.eventType === 'disconnected')
