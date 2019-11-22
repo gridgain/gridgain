4469883a1869b1e558220781d2f0765a4d052081
Aleksei Scherbakov <alexey.scherbakoff@gmail.com>
2019-11-22 18:32:49 +0300

    GG-25687 revert.

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/distributed/dht/atomic/GridDhtAtomicCache.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/distributed/dht/atomic/GridDhtAtomicCache.java
@@ -2603,25 +2603,12 @@ public class GridDhtAtomicCache<K, V> extends GridDhtCacheAdapter<K, V> {
         if (req0 != null) {
             locBuf.add(new T4<>(nearNode0, req0, dhtUpdRes0, value));
 
-            if (locBuf.size() < 32)
+            if (locBuf.size() < 10)
                 return;
 
             if (!ctx.time().removeTimeoutObject(thread.flusher))
                 return;
         }
-        else {
-            if (locBuf.size() > thread.last && locBuf.size() != 32) {
-                thread.last = locBuf.size();
-
-                thread.flusher = new Flusher(stripe);
-
-                ctx.time().addTimeoutObject(thread.flusher);
-
-                return;
-            }
-        }
-
-        thread.last = 0;
 
         // Generate version for per-stripe updates.
         GridCacheVersion ver = ctx.versions().next(topology().readyTopologyVersion());
@@ -4027,7 +4014,7 @@ public class GridDhtAtomicCache<K, V> extends GridDhtCacheAdapter<K, V> {
         Flusher(int stripe) {
             this.stripe = stripe;
 
-            endTime = System.currentTimeMillis() + 5;
+            endTime = System.currentTimeMillis() + 15;
 
             id = IgniteUuid.randomUuid();
         }
