0e0c1bbd9fefced00ab69393be238c17d804893b
Pavel Tupitsyn <ptupitsyn@apache.org>
2019-11-24 20:50:52 +0300

    Adding ClientProtocolVersionTests

---
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests/Client/ClientProtocolVersionTests.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Copyright 2019 GridGain Systems, Inc. and Contributors.
+ *
+ * Licensed under the GridGain Community Edition License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.gridgain.com/products/software/community-edition/gridgain-community-edition-license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Apache.Ignite.Core.Tests.Client
+{
+    using Apache.Ignite.Core.Impl.Client;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Tests for <see cref="ClientProtocolVersion"/>.
+    /// </summary>
+    public class ClientProtocolVersionTests
+    {
+        /// <summary>
+        /// Tests comparison for equality.
+        /// </summary>
+        [Test]
+        public void TestEqualityComparison()
+        {
+            Assert.AreEqual(
+                new ClientProtocolVersion(1, 2, 3), 
+                new ClientProtocolVersion(1, 2, 3));
+        }
+        
+    }
+}
