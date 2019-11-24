6430e7829aa6c8760718ed36d0f531e435ec685c
Pavel Tupitsyn <ptupitsyn@apache.org>
2019-11-24 20:31:23 +0300

    Add MinVersionAttribute class

---
+++ modules/platforms/dotnet/Apache.Ignite.Core/Impl/Client/MinVersionAttribute.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * Copyright 2019 GridGain Systems, Inc. and Contributors.
+ *
+ * Licensed under the GridGain Community Edition License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.gridgain.com/products/software/community-edition/gridgain-community-edition-license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Apache.Ignite.Core.Impl.Client
+{
+    /// <summary>
+    /// Version attribute for <see cref="ClientOp"/>.
+    /// </summary>
+    internal class MinVersionAttribute
+    {
+        /** */
+        private ClientProtocolVersion _version;
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="MinVersionAttribute"/> class.
+        /// </summary>
+        public MinVersionAttribute(short major, short minor, short maintenance)
+        {
+            _version = new ClientProtocolVersion(major, minor, maintenance);
+        }
+    }
+}
