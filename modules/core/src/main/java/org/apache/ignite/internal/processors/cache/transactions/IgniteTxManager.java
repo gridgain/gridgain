1e5ea625eac2ab76de6f2335ce5ce0aec6286966
Aleksei Scherbakov <alexey.scherbakoff@gmail.com>
2019-12-06 18:34:11 +0300

    GG-26246 Fix codestyle and license.

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/transactions/IgniteTxManager.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/transactions/IgniteTxManager.java
@@ -20,7 +20,6 @@ import java.io.Externalizable;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.List;
