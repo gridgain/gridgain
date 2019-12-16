9d7d141b6458c5000740a8639b9430c52a1c444c
smoldachev <smoldachev@gridgain.com>
2019-12-16 17:04:14 +0300

    GG-26416: Review fixes.

--- modules/management-console-agent/src/test/java/org/apache/ignite/agent/dto/topology/TopologySnapshotTest.java
+++ modules/management-console-agent/src/test/java/org/apache/ignite/agent/dto/topology/TopologySnapshotTest.java
@@ -119,8 +119,10 @@ public class TopologySnapshotTest {
 
             if (consistentNodeId_2.equals(node.getConsistentId())) {
                 assertTrue(node.isOnline());
-                assertEquals(2, node.getOrder());
                 assertFalse(node.isBaselineNode());
+                assertEquals(2, node.getOrder());
+                assertFalse(node.isClient());
+                assertEquals(clusterNodeId_2, node.getNodeId());
             }
         }
     }
@@ -146,6 +148,8 @@ public class TopologySnapshotTest {
 
         clusterNode_1.setAttributes(Collections.emptyMap());
 
+        clusterNode_1.order(1);
+
         UUID crdId = UUID.fromString("c-c-c-c-c");
 
         TopologySnapshot top = TopologySnapshot.topology(
@@ -163,6 +167,7 @@ public class TopologySnapshotTest {
             if (consistentNodeId_1.equals(node.getConsistentId())) {
                 assertTrue(node.isOnline());
                 assertFalse(node.isBaselineNode());
+                assertEquals(1, node.getOrder());
                 assertFalse(node.isClient());
                 assertEquals(clusterNodeId_1, node.getNodeId());
             }
