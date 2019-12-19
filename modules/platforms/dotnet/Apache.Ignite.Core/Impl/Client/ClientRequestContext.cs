782694b4b38e65e1c8b7b285267b92119380cf68
Pavel <ptupitsyn@apache.org>
2019-12-19 19:43:00 +0300

    Implement ClientRequestContext

---
+++ modules/platforms/dotnet/Apache.Ignite.Core/Impl/Client/ClientRequestContext.cs
@@ -0,0 +1,92 @@
+﻿/*
+ * Copyright 2019 GridGain Systems, Inc. and Contributors.
+ *
+ * Licensed under the GridGain Community Edition License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.gridgain.com/products/software/community-edition/gridgain-community-edition-license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Apache.Ignite.Core.Impl.Client
+{
+    using System.Diagnostics;
+    using Apache.Ignite.Core.Binary;
+    using Apache.Ignite.Core.Impl.Binary;
+    using Apache.Ignite.Core.Impl.Binary.IO;
+
+    /// <summary>
+    /// Request context.
+    /// </summary>
+    internal class ClientRequestContext
+    {
+        /** */
+        private readonly ClientSocket _socket;
+        
+        /** */
+        private readonly IBinaryStream _stream;
+        
+        /** */
+        private readonly Marshaller _marshaller;
+
+        /** */
+        private BinaryWriter _writer = null;
+            
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="ClientRequestContext"/> class.
+        /// </summary>
+        /// <param name="socket">The actual socket to be used for this request.</param>
+        /// <param name="stream"></param>
+        /// <param name="marshaller"></param>
+        public ClientRequestContext(ClientSocket socket, IBinaryStream stream, Marshaller marshaller)
+        {
+            Debug.Assert(socket != null);
+            Debug.Assert(stream != null);
+            Debug.Assert(marshaller != null);
+            
+            _socket = socket;
+            _stream = stream;
+            _marshaller = marshaller;
+        }
+
+        /// <summary>
+        /// Protocol version to be used for this request.
+        /// (Takes partition awareness, failover and reconnect into account).
+        /// </summary>
+        public ClientProtocolVersion ProtocolVersion
+        {
+            get { return _socket.ServerVersion; }
+        }
+
+        /// <summary>
+        /// Stream.
+        /// </summary>
+        public IBinaryStream Stream
+        {
+            get { return _stream; }
+        }
+        
+        /// <summary>
+        /// Writer.
+        /// </summary>
+        public IBinaryWriter Writer
+        {
+            get { return _writer ?? (_writer = _marshaller.StartMarshal(_stream)); }
+        }
+
+        /// <summary>
+        /// Finishes marshal session for this request (if any).
+        /// </summary>
+        public void FinishMarshal()
+        {
+            _marshaller.FinishMarshal(_writer);
+        }
+    }
+}
