39f4ff705c709fdf5fbf6ae5ea9a9b9521ec30dd
Vasiliy Sisko <vsisko@gridgain.com>
2019-12-23 10:05:47 +0300

    GG-19450 Added metrics for number of clusters, caches and models for "Admin panel" screen. (#759)

--- modules/web-console/web-console-server/src/main/java/org/apache/ignite/console/services/AdminService.java
+++ modules/web-console/web-console-server/src/main/java/org/apache/ignite/console/services/AdminService.java
@@ -26,7 +26,9 @@ import org.apache.ignite.console.event.EventPublisher;
 import org.apache.ignite.console.json.JsonArray;
 import org.apache.ignite.console.json.JsonObject;
 import org.apache.ignite.console.repositories.AnnouncementRepository;
+import org.apache.ignite.console.repositories.ConfigurationsRepository;
 import org.apache.ignite.console.tx.TransactionManager;
+import org.apache.ignite.console.web.model.ConfigurationKey;
 import org.apache.ignite.console.web.model.SignUpRequest;
 import org.apache.ignite.console.web.socket.TransitionService;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
@@ -37,6 +39,8 @@ import org.springframework.stereotype.Service;
 
 import static org.apache.ignite.console.event.AccountEventType.ACCOUNT_CREATE_BY_ADMIN;
 import static org.apache.ignite.console.event.AccountEventType.ACCOUNT_DELETE;
+import static org.apache.ignite.console.repositories.ConfigurationsRepository.CACHES_CNT;
+import static org.apache.ignite.console.repositories.ConfigurationsRepository.MODELS_CNT;
 import static org.springframework.session.FindByIndexNameSessionRepository.PRINCIPAL_NAME_INDEX_NAME;
 
 /**
@@ -71,6 +75,9 @@ public class AdminService {
     /** */
     private final FindByIndexNameSessionRepository<ExpiringSession> sesRepo;
 
+    /** */
+    private final ConfigurationsRepository cfgsRepo;
+
     /**
      * @param txMgr Transactions manager.
      * @param accountsSrv Service to work with accounts.
@@ -81,6 +88,7 @@ public class AdminService {
      * @param annRepo Repository to work with announcement.
      * @param transitionSrvc Transition service.
      * @param sesRepo Sessions repository.
+     * @param cfgsRepo Configurations repository.
      */
     public AdminService(
         TransactionManager txMgr,
@@ -91,7 +99,8 @@ public class AdminService {
         EventPublisher evtPublisher,
         AnnouncementRepository annRepo,
         TransitionService transitionSrvc,
-        FindByIndexNameSessionRepository<ExpiringSession> sesRepo
+        FindByIndexNameSessionRepository<ExpiringSession> sesRepo,
+        ConfigurationsRepository cfgsRepo
     ) {
         this.txMgr = txMgr;
         this.accountsSrv = accountsSrv;
@@ -102,6 +111,7 @@ public class AdminService {
         this.annRepo = annRepo;
         this.transitionSrvc = transitionSrvc;
         this.sesRepo = sesRepo;
+        this.cfgsRepo = cfgsRepo;
     }
 
     /**
@@ -123,6 +133,18 @@ public class AdminService {
             long lastLogin = ses.values().stream().map(ExpiringSession::getCreationTime).max(Long::compareTo).orElse(-1L);
             long lastActivity = ses.values().stream().map(ExpiringSession::getLastAccessedTime).max(Long::compareTo).orElse(-1L);
 
+            JsonArray clusters = cfgsRepo.loadClusters(new ConfigurationKey(account.getId(), false));
+
+            int caches = 0;
+            int models = 0;
+
+            for (Object item : clusters) {
+                JsonObject json = (JsonObject)item;
+
+                caches += json.getInteger(CACHES_CNT, 0);
+                models += json.getInteger(MODELS_CNT, 0);
+            }
+
             res.add(new JsonObject()
                 .add("id", account.getId())
                 .add("firstName", account.getFirstName())
@@ -135,9 +157,9 @@ public class AdminService {
                 .add("lastActivity", lastActivity)
                 .add("activated", account.isEnabled())
                 .add("counters", new JsonObject()
-                    .add("clusters", 0)
-                    .add("caches", 0)
-                    .add("models", 0))
+                    .add("clusters", clusters.size())
+                    .add("caches", caches)
+                    .add("models", models))
                 .add("activitiesDetail", activitiesSrv.activitiesForPeriod(account.getId(), startDate, endDate))
             );
         });
