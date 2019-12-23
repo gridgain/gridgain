59631b6487858c8e5a5f6999aa93570ad716f485
Pavel Tupitsyn <ptupitsyn@apache.org>
2019-12-23 10:51:59 +0300

    GG-26775 .NET: Fix Mono issues
    * Fix P/Invoke in UnmanagedThread
    * Fix GetDefaultAppDomain
    * Fix build.ps1 to work on Linux under PowerShell Core
    
    This enables full development cycle on Linux with Mono, including NuGet package creation
    
    (cherry picked from commit 3e9a991631e8717cba45b0b714ad98ff3a7ed912)

--- modules/platforms/dotnet/Apache.Ignite.Core.Tests/TestRunner.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests/TestRunner.cs
@@ -23,7 +23,6 @@ namespace Apache.Ignite.Core.Tests
     using System.Reflection;
     using Apache.Ignite.Core.Tests.Binary.Serializable;
     using Apache.Ignite.Core.Tests.Cache;
-    using Apache.Ignite.Core.Tests.Cache.Query.Linq;
     using Apache.Ignite.Core.Tests.Client.Cache;
     using Apache.Ignite.Core.Tests.Compute;
     using Apache.Ignite.Core.Tests.Memory;
@@ -73,10 +72,10 @@ namespace Apache.Ignite.Core.Tests
             var basicTests = new[]
             {
                 typeof(ComputeApiTest),
-                typeof(CacheLinqTest),
                 typeof(SqlDmlTest),
                 typeof(LinqTest),
-                typeof(PersistenceTest)
+                typeof(PersistenceTest),
+                typeof(CacheTest)
             };
 
             Environment.ExitCode = TestAll(basicTests, true);
