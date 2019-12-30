09a0708f9b8c376529044da8a860148376ac0283
Andrey V. Mashenkov <andrey.mashenkov@gmail.com>
2019-12-30 18:17:51 +0300

    GG-26659: Fix typo in method name.
    (cherry picked from commit 37a0f4455e8116b9723181b1a944efdd1af5c68f)

--- modules/core/src/main/java/org/apache/ignite/internal/jdbc/thin/ConnectionPropertiesImpl.java
+++ modules/core/src/main/java/org/apache/ignite/internal/jdbc/thin/ConnectionPropertiesImpl.java
@@ -618,7 +618,7 @@ public class ConnectionPropertiesImpl implements ConnectionProperties, Serializa
     }
 
     /** {@inheritDoc} */
-    @Override public void setQueryMaxMemory(@Nullable Integer timeout) throws SQLException {
+    @Override public void setQueryTimeout(@Nullable Integer timeout) throws SQLException {
         qryTimeout.setValue(timeout);
     }
 
