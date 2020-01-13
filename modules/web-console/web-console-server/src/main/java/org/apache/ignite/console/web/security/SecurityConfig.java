a91eb4c2602b5f863acb94579b797cdba508ff55
Andrey Novikov <anovikov@gridgain.com>
2020-01-13 13:38:34 +0700

    GG-26546 Optimize imports.

--- modules/web-console/web-console-server/src/main/java/org/apache/ignite/console/web/security/SecurityConfig.java
+++ modules/web-console/web-console-server/src/main/java/org/apache/ignite/console/web/security/SecurityConfig.java
@@ -31,7 +31,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.authentication.DefaultAuthenticationEventPublisher;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
