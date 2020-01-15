d21a62b424808956be68d0f3909a2c0da09f7c52
Vasiliy Sisko <vsisko@gridgain.com>
2020-01-15 14:35:38 +0300

    GG-27017 Fixed availability of "Add user" action. (#848)

--- modules/web-console/e2e/testcafe/fixtures/admin/assume-identity.js
+++ modules/web-console/e2e/testcafe/fixtures/admin/assume-identity.js
@@ -20,8 +20,40 @@ import * as admin from '../../page-models/pageAdminListOfRegisteredUsers';
 import {pageProfile as profile} from '../../page-models/pageProfile';
 import * as notifications from '../../components/permanentNotifications';
 import {userMenu} from '../../components/userMenu';
+import { Selector } from 'testcafe';
+import { successNotification } from '../../components/notifications';
+import {confirmation} from '../../components/confirmation'
 
 const regularUser = createRegularUser();
+
+const _checkListOfActions = async(t, actions, excluded) => {
+    const actionSelector = Selector('.dropdown-menu a');
+
+    for (let i = 0; i < actions.length; i++) {
+        const actionLbl = actions[i];
+
+        await t.expect(actionSelector.withText(actionLbl).exists).notEql(excluded);
+    }
+
+    return false;
+};
+
+const actions = {
+    alwaysAvailable: ['Add user'],
+    curUserSelectedOnly: ['Activity details'],
+    otherUserActions: ['Become this user', 'Revoke admin', 'Remove user']
+};
+
+const _checkActionSet = async(t, excludeOtherActions = false, excludeCurOnly = false) => {
+    await _checkListOfActions(t, actions.alwaysAvailable);
+    await _checkListOfActions(t, actions.curUserSelectedOnly, excludeCurOnly);
+    await _checkListOfActions(t, actions.otherUserActions, excludeOtherActions);
+};
+
+const CUR_USER = 'John Doe';
+const OTHER_USER = 'User Name';
+
+const LONG_TIMEOUT_OPTION = { timeout: 15000 };
 
 fixture('Assumed identity')
     .beforeEach(async(t) => {
@@ -38,7 +70,7 @@ fixture('Assumed identity')
 
 test('Become user', async(t) => {
     await t.navigateTo(resolveUrl('/settings/admin'));
-    await t.click(admin.userNameCell.withText('User Name'));
+    await t.click(admin.userNameCell.withText(OTHER_USER));
     await admin.usersTable.performAction('Become this user');
     await t
         .hover(userMenu.button)
@@ -52,7 +84,7 @@ test('Become user', async(t) => {
         .expect(profile.email.control.value).eql(admin.TEST_USER.email)
         .expect(profile.country.control.value).eql(admin.TEST_USER.country)
         .expect(profile.company.control.value).eql(admin.TEST_USER.company)
-        .expect(notifications.assumedUserFullName.innerText).eql('User Name')
+        .expect(notifications.assumedUserFullName.innerText).eql(OTHER_USER)
         .typeText(profile.firstName.control, '1')
         .click(profile.saveChangesButton)
         .expect(notifications.assumedIdentityNotification.visible).ok()
@@ -60,4 +92,90 @@ test('Become user', async(t) => {
         .hover(userMenu.button)
         // See https://ggsystems.atlassian.net/browse/GG-24068
         .expect(userMenu.menuItem.withText('Admin panel').count).eql(1);
+});
+
+test("Action availability", async(t) => {
+    const _changeUserSelection = async(userName) => {
+        await t.click(admin.userNameCell.withText(userName))
+            .hover(admin.usersTable.actionsButton);
+    };
+
+    await t.navigateTo(resolveUrl('/settings/admin'))
+        .hover(admin.usersTable.actionsButton);
+
+    await _checkActionSet(t, true, true);
+
+    await admin.usersTable.performAction('Add user');
+
+    const createUserDialog = Selector('.modal-dialog');
+
+    await t.expect(createUserDialog.find('.modal-header').withText('Create User').exists).ok()
+        .click(createUserDialog.find('button').withText('Cancel'))
+        .hover(admin.usersTable.actionsButton);
+
+    await _checkActionSet(t, true, true);
+
+    await _changeUserSelection(CUR_USER);
+
+    await _checkActionSet(t, true, false);
+
+    await _changeUserSelection(CUR_USER);
+
+    await _checkActionSet(t, true, true);
+
+    await _changeUserSelection(OTHER_USER);
+
+    await _checkActionSet(t, false, false);
+
+    await _changeUserSelection(OTHER_USER);
+
+    await _checkActionSet(t, true, true);
+
+    await _changeUserSelection(OTHER_USER);
+
+    await admin.usersTable.performAction('Revoke admin');
+
+    await t.expect(successNotification.withText(`Admin rights was successfully revoked for user: "${OTHER_USER}"`).exists).ok();
+
+    await t.eval(() => window.location.reload());
+    await t.expect(admin.userNameCell.withText(OTHER_USER).find('i.icon-user').exists).ok(LONG_TIMEOUT_OPTION);
+
+    await _changeUserSelection(OTHER_USER);
+    await admin.usersTable.performAction('Grant admin');
+
+    await t.expect(successNotification.withText(`Admin rights was successfully granted for user: "${OTHER_USER}"`).exists).ok();
+
+    await t.eval(() => window.location.reload());
+    await t.expect(admin.userNameCell.withText(OTHER_USER).find('i.icon-admin').exists).ok(LONG_TIMEOUT_OPTION);
+});
+
+test("Remove user", async(t) => {
+    await t.navigateTo(resolveUrl('/settings/admin'))
+        .click(admin.userNameCell.withText(OTHER_USER));
+
+    await admin.usersTable.performAction('Remove user');
+
+    await t.expect(confirmation.body.textContent).contains(`Are you sure you want to remove user: "${OTHER_USER}"?`);
+
+    await confirmation.cancel();
+
+    await t.expect(admin.userNameCell.withText(OTHER_USER).exists).ok();
+
+    await admin.usersTable.performAction('Remove user');
+
+    await t.expect(confirmation.body.textContent).contains(`Are you sure you want to remove user: "${OTHER_USER}"?`);
+
+    await confirmation.confirm();
+
+    await t.expect(successNotification.withText(`User has been removed: "${OTHER_USER}"`).exists).ok();
+
+    await t.expect(admin.userNameCell.withText(OTHER_USER).exists).notOk();
+
+    await t.hover(admin.usersTable.actionsButton);
+
+    await _checkActionSet(t, true, true);
+
+    await t.eval(() => window.location.reload());
+
+    await t.expect(admin.userNameCell.withText(OTHER_USER).exists).notOk(LONG_TIMEOUT_OPTION);
 });
