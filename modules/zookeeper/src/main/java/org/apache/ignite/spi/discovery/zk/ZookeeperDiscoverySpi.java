9165d91484a9f892e0e9d8e6bd68cc97e4174c93
Sergey Chugunov <schugunov@gridgain.com>
2020-01-24 14:35:44 +0300

    GG-27019 Additional tests and improvements for Distributed Metastorage
    Signed-off by: Ivan Bessonov

--- modules/zookeeper/src/main/java/org/apache/ignite/spi/discovery/zk/ZookeeperDiscoverySpi.java
+++ modules/zookeeper/src/main/java/org/apache/ignite/spi/discovery/zk/ZookeeperDiscoverySpi.java
@@ -37,6 +37,7 @@ import org.apache.ignite.internal.util.typedef.internal.A;
 import org.apache.ignite.internal.util.typedef.internal.S;
 import org.apache.ignite.internal.util.typedef.internal.U;
 import org.apache.ignite.lang.IgniteBiTuple;
+import org.apache.ignite.lang.IgnitePredicate;
 import org.apache.ignite.lang.IgniteProductVersion;
 import org.apache.ignite.resources.LoggerResource;
 import org.apache.ignite.spi.IgniteSpiAdapter;
@@ -337,11 +338,11 @@ public class ZookeeperDiscoverySpi extends IgniteSpiAdapter implements IgniteDis
     }
 
     /** {@inheritDoc} */
-    @Override public boolean allNodesSupport(IgniteFeatures feature) {
+    @Override public boolean allNodesSupport(IgniteFeatures feature, IgnitePredicate<ClusterNode> nodesPred) {
         if (impl == null)
             return false;
 
-        return impl.allNodesSupport(feature);
+        return impl.allNodesSupport(feature, nodesPred);
     }
 
     /** {@inheritDoc} */
