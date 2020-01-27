6142172d1f751a075dbf8a638cc0d76893887290
Pavel Tupitsyn <ptupitsyn@apache.org>
2020-01-27 13:08:27 +0300

    Add PutNearBenchmark

---
+++ modules/platforms/dotnet/Apache.Ignite.Benchmarks/Interop/PutNearBenchmark.cs
@@ -0,0 +1,41 @@
+﻿namespace Apache.Ignite.Benchmarks.Interop
+{
+    using System.Collections.Generic;
+    using Apache.Ignite.Core.Cache;
+
+    /// <summary>
+    /// Cache put benchmark.
+    /// </summary>
+    internal class PutNearBenchmark : PlatformBenchmarkBase
+    {
+        /** Cache name. */
+        private const string CacheName = "cacheNear";
+
+        /** Native cache wrapper. */
+        private ICache<object, object> _cache;
+
+        /** <inheritDoc /> */
+        protected override void OnStarted()
+        {
+            base.OnStarted();
+
+            _cache = Node.GetCache<object, object>(CacheName);
+        }
+
+        /** <inheritDoc /> */
+        protected override void GetDescriptors(ICollection<BenchmarkOperationDescriptor> descs)
+        {
+            descs.Add(BenchmarkOperationDescriptor.Create("Put", Put, 1));
+        }
+        
+        /// <summary>
+        /// Cache put.
+        /// </summary>
+        private void Put(BenchmarkState state)
+        {
+            int idx = BenchmarkUtils.GetRandomInt(Dataset);
+
+            _cache.Put(idx, Emps[idx]);
+        }
+    }
+}
