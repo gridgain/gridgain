442e4bd967f007614928b066d2363e378e809914
Pavel Tupitsyn <ptupitsyn@apache.org>
2020-01-28 22:49:15 +0300

    Add test placeholders

---
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests/Cache/Near/CacheNearNodeLeaveTest.cs
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2019 GridGain Systems, Inc. and Contributors.
+ *
+ * Licensed under the GridGain Community Edition License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.gridgain.com/products/software/community-edition/gridgain-community-edition-license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Apache.Ignite.Core.Tests.Cache.Near
+{
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Tests Near Cache behavior when primary node leaves.
+    /// </summary>
+    public class CacheNearNodeLeaveTest
+    {
+        /// <summary>
+        /// Tests that near cache is cleared when primary node for a given key leaves and there are no backups.
+        /// </summary>
+        [Test]
+        public void TestPrimaryNodeLeaveNoBackupClearsNearCache()
+        {
+            
+        }
+        
+        /// <summary>
+        /// Tests that near cache is not cleared when primary node for a given key leaves but there are backups.
+        /// </summary>
+        [Test]
+        public void TestPrimaryNodeLeaveWithBackupKeepsNearCache()
+        {
+            
+        }
+    }
+}
