2b87ff7871d896d700bbb36258c2a007ef54901a
Pavel Tupitsyn <ptupitsyn@apache.org>
2020-01-30 17:55:32 +0300

    Adding topology change tests

--- modules/platforms/dotnet/Apache.Ignite.Core.Tests/Cache/Near/CacheNearTopologyChangeTest.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests/Cache/Near/CacheNearTopologyChangeTest.cs
@@ -102,6 +102,7 @@ namespace Apache.Ignite.Core.Tests.Cache.Near
             Assert.AreEqual(-1, _cache[1][Key3].Bar);
             
             // New node enters and becomes primary for the key.
+            // GridCacheNearEntry does not yet exist on old primary node, so updates may be missing.
             InitGrid(2);
 
             Assert.AreEqual(-1, _cache[2][Key3].Bar);
