b929284142ebe9dc593f019add0673af902422f5
Igor Belyakov <igor.belyakov.s@gmail.com>
2020-02-07 12:11:45 +0300

    GG-17432 Excessive use of memory in continuous queries (#359)
    * GG-17432 clearing entries after processing in continuous queries implemented
    
    * GG-17432 CQ buffers cleanup after getting acknowledge from client implemented
    
    * GG-17432 processed buffer check implemented
    
    * GG-17432 test for CQ buffer clean up implemented
    
    * GG-17432 cleanup entries for backup partitions implemented
    
    * GG-17432 unit test added for case with multiple nodes and backups
    
    * GG-17432 code review fixes
    
    * GG-17432 code review fixes. "cleanupEntries" method was optimized. "testMultipleNodesWithBackupsWithoutClient" test added.
    
    * GG-17432 code style fixed
    
    * GG-17432 tests for the CQ with transformer added
    
    * GG-17432 continuous query buffer cleanup tests renamed
    
    Co-authored-by: Igor Belyakov <igor.belyakov.s@gmail.com>
    (cherry picked from commit bdf60aeeabfae6cb2b7bcc10a6dead4654615803)

---
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/query/continuous/ContinuousQueryWithTransformerBufferCleanupTest.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2019 GridGain Systems, Inc. and Contributors.
+ *
+ * Licensed under the GridGain Community Edition License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.gridgain.com/products/software/community-edition/gridgain-community-edition-license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.ignite.internal.processors.cache.query.continuous;
+
+import javax.cache.configuration.Factory;
+import javax.cache.configuration.FactoryBuilder;
+import javax.cache.event.CacheEntryEvent;
+import org.apache.ignite.cache.query.AbstractContinuousQuery;
+import org.apache.ignite.cache.query.ContinuousQueryWithTransformer;
+import org.apache.ignite.lang.IgniteClosure;
+
+/**
+ * Test for continuous query with transformer buffer cleanup.
+ */
+public class ContinuousQueryWithTransformerBufferCleanupTest extends ContinuousQueryBufferCleanupAbstractTest {
+    /** {@inheritDoc} */
+    @Override
+    protected AbstractContinuousQuery<Integer, String> getContinuousQuery() {
+        ContinuousQueryWithTransformer<Integer, String, String> qry = new ContinuousQueryWithTransformer<>();
+
+        Factory factory = FactoryBuilder.factoryOf(
+            (IgniteClosure<CacheEntryEvent, String>)event -> ((String)event.getValue())
+        );
+
+        qry.setRemoteTransformerFactory(factory);
+
+        qry.setLocalListener((evts) -> evts.forEach(e -> System.out.println("val=" + e)));
+
+        return qry;
+    }
+}
