310482fae8a59c3f647e8676511f886ef0033dda
Alexey Scherbakov <alexey.scherbakoff@gmail.com>
2020-02-09 15:35:39 +0300

    GG-17349 Fixed issue with missed state on partition loss + ignore policy.

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/distributed/dht/preloader/GridDhtPreloader.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/distributed/dht/preloader/GridDhtPreloader.java
@@ -221,6 +221,8 @@ public class GridDhtPreloader extends GridCachePreloaderAdapter {
 
         CachePartitionFullCountersMap countersMap = grp.topology().fullUpdateCounters();
 
+        boolean changed = false;
+
         for (int p = 0; p < partitions; p++) {
             if (ctx.exchange().hasPendingServerExchange()) {
                 if (log.isDebugEnabled())
@@ -302,7 +304,7 @@ public class GridDhtPreloader extends GridCachePreloaderAdapter {
                     List<ClusterNode> picked = remoteOwners(p, topVer);
 
                     if (picked.isEmpty()) {
-                        top.own(part);
+                        changed |= top.own(part);
 
                         if (grp.eventRecordable(EVT_CACHE_REBALANCE_PART_DATA_LOST)) {
                             grp.addRebalanceEvent(p,
@@ -340,6 +342,9 @@ public class GridDhtPreloader extends GridCachePreloaderAdapter {
                 "Unexpected rebalance on rebalanced cluster " +
                     "[top=" + topVer + ", grp=" + grp.groupId() + ", assignments=" + assignments + "]";
         }
+
+        if (changed)
+            ctx.exchange().scheduleResendPartitions();
 
         return assignments;
     }
