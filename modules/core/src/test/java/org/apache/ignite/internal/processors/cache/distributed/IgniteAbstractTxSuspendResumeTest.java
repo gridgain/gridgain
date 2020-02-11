374af0b0cbc0217010d97ec838d66fe4ab255cb8
Alexey Goncharuk <alexey.goncharuk@gmail.com>
2020-02-11 11:37:08 +0300

    GG-23556 Add support for suspend/resume of PESSIMISTIC transactions

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/distributed/IgniteAbstractTxSuspendResumeTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/distributed/IgniteAbstractTxSuspendResumeTest.java
@@ -451,7 +451,7 @@ public abstract class IgniteAbstractTxSuspendResumeTest extends GridCommonAbstra
                     // Here we check that we can start any transactional operation in the same thread after a suspended
                     // transaction is timed-out.
                     assertFalse(cache.containsKey(1));
-                    cache.put(2, 2);
+                    cache.put(4, 2);
 
                     tx.close();
 
