baf6015f2e7ba189797742598d591d4e2792d57b
Pavel Tupitsyn <ptupitsyn@apache.org>
2020-02-19 23:20:20 +0300

    Add EnablePlatformNearCache convenience config method

--- modules/platforms/dotnet/Apache.Ignite.Core/Cache/Configuration/NearCacheConfiguration.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core/Cache/Configuration/NearCacheConfiguration.cs
@@ -44,6 +44,8 @@ namespace Apache.Ignite.Core.Cache.Configuration
         /// <summary>
         /// Initializes a new instance of the <see cref="NearCacheConfiguration"/> class.
         /// </summary>
+        /// <param name="enablePlatformNearCache">When true, sets <see cref="PlatformNearCacheConfiguration"/>
+        /// to a default instance.</param>
         public NearCacheConfiguration(bool enablePlatformNearCache) : this()
         {
             if (enablePlatformNearCache)
@@ -102,5 +104,24 @@ namespace Apache.Ignite.Core.Cache.Configuration
         /// TODO
         /// </summary>
         public PlatformNearCacheConfiguration PlatformNearCacheConfiguration { get; set; }
+
+        /// <summary>
+        /// Convenience method to set <see cref="PlatformNearCacheConfiguration"/> with specified key and value types.
+        /// </summary>
+        /// <param name="keepBinary">Whether to enable binary mode for the platform near cache.</param>
+        /// <typeparam name="TK">Key type for near cache map.</typeparam>
+        /// <typeparam name="TV">Value type for near cache map.</typeparam>
+        /// <returns>This instance for chaining.</returns>
+        public NearCacheConfiguration EnablePlatformNearCache<TK, TV>(bool keepBinary = false)
+        {
+            PlatformNearCacheConfiguration = new PlatformNearCacheConfiguration
+            {
+                KeepBinary = keepBinary,
+                KeyTypeName = typeof(TK).AssemblyQualifiedName,
+                ValueTypeName = typeof(TV).AssemblyQualifiedName
+            };
+            
+            return this;
+        }
     }
 }
