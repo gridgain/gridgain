6c67681d04db24e586717e770b2f28cebea6a0f1
tledkov <tledkov@gridgain.com>
2020-02-21 15:59:44 +0300

    GG-27582: fix imports

--- modules/indexing/src/main/java/org/apache/ignite/internal/processors/query/h2/H2Connection.java
+++ modules/indexing/src/main/java/org/apache/ignite/internal/processors/query/h2/H2Connection.java
@@ -20,7 +20,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import org.apache.ignite.IgniteCheckedException;
 import org.apache.ignite.IgniteLogger;
 import org.apache.ignite.internal.processors.cache.query.IgniteQueryErrorCode;
 import org.apache.ignite.internal.processors.query.IgniteSQLException;
