8b34b42742325c3554771a0344cab3e33d12b41f
Pavel Tupitsyn <ptupitsyn@apache.org>
2020-02-24 14:40:22 +0300

    Notes on failing test

--- modules/platforms/dotnet/Apache.Ignite.Core.Tests/Cache/Near/CacheNearTopologyChangeTest.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests/Cache/Near/CacheNearTopologyChangeTest.cs
@@ -128,6 +128,8 @@ namespace Apache.Ignite.Core.Tests.Cache.Near
             Assert.IsFalse(_cache[1].TryLocalPeek(Key3, out foo, CachePeekMode.NativeNear));
             
             // Check value on the new node: it should be already in near cache, because key is primary.
+            // TODO: This fails because GridCacheMapEntry.initialValue provides older topology version,
+            // and IsValid check fails. But the entry is actually valid. Investigate.
             Assert.AreEqual(-1, _cache[2].LocalPeek(Key3, CachePeekMode.NativeNear).Bar);
             Assert.AreEqual(-1, _cache[2][Key3].Bar);
             Assert.AreSame(_cache[2][Key3], _cache[2][Key3]);
