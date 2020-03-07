3bdcc4ceb7bf0bdc09db62317357bdb5efe676f6
Pavel Tupitsyn <ptupitsyn@apache.org>
2020-03-07 20:55:07 +0300

    Implementing thread-local optimization for primary key Put - update bench

--- modules/platforms/dotnet/Apache.Ignite.Benchmarks/BenchmarkRunner.cs
+++ modules/platforms/dotnet/Apache.Ignite.Benchmarks/BenchmarkRunner.cs
@@ -43,7 +43,7 @@ namespace Apache.Ignite.Benchmarks
 
             args = new[] {
                 //typeof(GetAllBenchmark).FullName,
-                typeof(GetBenchmark).FullName,
+                typeof(PutNearBenchmark).FullName,
                 //typeof(ThinClientGetAllBenchmark).FullName,
                 //typeof(ThinClientGetAllBinaryBenchmark).FullName,
                 "-ConfigPath", GetConfigPath(),
