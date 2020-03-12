5256fecd37f5170d171aab82f202fa56dca891d0
Nikolay <nizhikov@apache.org>
2020-03-12 11:48:17 +0300

    GG-29121 [IGNITE-12745] Compute job view implemented. (#7495)

--- modules/core/src/main/java/org/apache/ignite/internal/processors/task/GridTaskProcessor.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/task/GridTaskProcessor.java
@@ -173,8 +173,8 @@ public class GridTaskProcessor extends GridProcessorAdapter implements IgniteCha
 
         ctx.systemView().registerView(TASKS_VIEW, TASKS_VIEW_DESC,
             new ComputeTaskViewWalker(),
-            tasks.values(),
-            ComputeTaskView::new);
+            tasks.entrySet(),
+            e -> new ComputeTaskView(e.getKey(), e.getValue()));
 
         isPersistenceEnabled = !ctx.clientNode() && isPersistenceEnabled(ctx.config());
     }
