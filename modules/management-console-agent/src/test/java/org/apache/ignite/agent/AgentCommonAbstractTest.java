ad210df3afe168675f38d0e8986e292e066c6c80
Mirza Aliev <alievmirza@gmail.com>
2020-03-19 12:31:57 +0300

    checkstyle issues

--- modules/management-console-agent/src/test/java/org/apache/ignite/agent/AgentCommonAbstractTest.java
+++ modules/management-console-agent/src/test/java/org/apache/ignite/agent/AgentCommonAbstractTest.java
@@ -56,8 +56,6 @@ import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static java.util.stream.Collectors.toList;
 import static org.apache.ignite.agent.StompDestinationsUtils.buildActionRequestTopic;
-import static org.apache.ignite.internal.SupportFeaturesUtils.IGNITE_BASELINE_AUTO_ADJUST_FEATURE;
-import static org.apache.ignite.internal.SupportFeaturesUtils.IGNITE_BASELINE_FOR_IN_MEMORY_CACHES_FEATURE;
 import static org.apache.ignite.internal.SupportFeaturesUtils.IGNITE_CLUSTER_ID_AND_TAG_FEATURE;
 import static org.apache.ignite.internal.SupportFeaturesUtils.IGNITE_DISTRIBUTED_META_STORAGE_FEATURE;
 import static org.awaitility.Awaitility.with;
