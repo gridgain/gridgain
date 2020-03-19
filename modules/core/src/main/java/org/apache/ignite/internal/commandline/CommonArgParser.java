e0ba6d769f88f47a2f8faf3ce6940558c377885b
ktkalenko <ktkalenko@gridgain.com>
2020-03-19 23:40:46 +0300

    GG-28216 cherry-pick GG-23528 and add test

--- modules/core/src/main/java/org/apache/ignite/internal/commandline/CommonArgParser.java
+++ modules/core/src/main/java/org/apache/ignite/internal/commandline/CommonArgParser.java
@@ -60,6 +60,9 @@ public class CommonArgParser {
     /** */
     static final String CMD_PING_TIMEOUT = "--ping-timeout";
 
+    /** Option is used to print error trace stack. */
+    public static final String CMD_PRINT_ERR_STACK_TRACE = "--print-err-stack-trace";
+
     // SSL configuration section
 
     /** */
@@ -103,6 +106,7 @@ public class CommonArgParser {
         AUX_COMMANDS.add(CMD_USER);
 
         AUX_COMMANDS.add(CMD_AUTO_CONFIRMATION);
+        AUX_COMMANDS.add(CMD_PRINT_ERR_STACK_TRACE);
 
         AUX_COMMANDS.add(CMD_PING_INTERVAL);
         AUX_COMMANDS.add(CMD_PING_TIMEOUT);
@@ -154,6 +158,7 @@ public class CommonArgParser {
         list.add(optional(CMD_PASSWORD, "PASSWORD"));
         list.add(optional(CMD_PING_INTERVAL, "PING_INTERVAL"));
         list.add(optional(CMD_PING_TIMEOUT, "PING_TIMEOUT"));
+        list.add(optional(CMD_PRINT_ERR_STACK_TRACE));
         list.add(optional(CMD_SSL_PROTOCOL, "SSL_PROTOCOL[, SSL_PROTOCOL_2, ..., SSL_PROTOCOL_N]"));
         list.add(optional(CMD_SSL_CIPHER_SUITES, "SSL_CIPHER_1[, SSL_CIPHER_2, ..., SSL_CIPHER_N]"));
         list.add(optional(CMD_SSL_KEY_ALGORITHM, "SSL_KEY_ALGORITHM"));
@@ -322,6 +327,9 @@ public class CommonArgParser {
                     case CMD_AUTO_CONFIRMATION:
                         autoConfirmation = true;
 
+                        break;
+
+                    case CMD_PRINT_ERR_STACK_TRACE:
                         break;
 
                     default:
