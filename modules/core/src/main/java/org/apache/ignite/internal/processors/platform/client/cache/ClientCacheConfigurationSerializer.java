apache/ignite

GG-20879 .NET: specify expiry policy when creating cache using thin client
Added ability to create dynamic caches for thin clients with expiry policies. 

- Added expiry policy property  to thin client cache configuration
- Added withExpiryPolicy method to the client cache interface

modules/core/src/main/java/org/apache/ignite/internal/processors/platform/client/cache/ClientCacheConfigurationSerializer.java
@@ -30,7 +30,9 @@ import org.apache.ignite.internal.binary.BinaryRawWriterEx;
 import java.util.ArrayList;
 import java.util.Collection;
 import org.apache.ignite.internal.processors.odbc.ClientListenerProtocolVersion;
+import org.apache.ignite.internal.processors.platform.utils.PlatformConfigurationUtils;
 
+import static org.apache.ignite.internal.processors.platform.client.ClientConnectionContext.VER_1_5_0;
 import static org.apache.ignite.internal.processors.platform.utils.PlatformConfigurationUtils.readQueryEntity;
 import static org.apache.ignite.internal.processors.platform.utils.PlatformConfigurationUtils.writeEnumInt;
 import static org.apache.ignite.internal.processors.platform.utils.PlatformConfigurationUtils.writeQueryEntity;
@@ -129,6 +131,9 @@ public class ClientCacheConfigurationSerializer {
     /** */
     private static final short STATISTICS_ENABLED = 406;
 
+    /** */
+    private static final short EXPIRY_POLICY = 407;
+
 
     /**
      * Writes the cache configuration.
@@ -196,6 +201,9 @@ public class ClientCacheConfigurationSerializer {
         } else
             writer.writeInt(0);
 
+        if (ver.compareTo(VER_1_5_0) >= 0)
+            PlatformConfigurationUtils.writeExpiryPolicyFactory(writer, cfg.getExpiryPolicyFactory());
+
         // Write length (so that part of the config can be skipped).
         writer.writeInt(pos, writer.out().position() - pos - 4);
     }
@@ -240,6 +248,10 @@ public class ClientCacheConfigurationSerializer {
 
                 case EAGER_TTL:
                     cfg.setEagerTtl(reader.readBoolean());
+                    break;
+
+                case EXPIRY_POLICY:
+                    cfg.setExpiryPolicyFactory(PlatformConfigurationUtils.readExpiryPolicyFactory(reader));
                     break;
 
                 case STATISTICS_ENABLED:

gridgain/gridgain

Added ability to create dynamic caches for thin clients with expiry policies.
(cherry picked from commit b2b12eb31df3d240de8a657eec57cd6cbedb94fe)

modules/core/src/main/java/org/apache/ignite/internal/processors/platform/client/cache/ClientCacheConfigurationSerializer.java
@@ -30,7 +30,9 @@ import org.apache.ignite.internal.binary.BinaryRawWriterEx;
 import java.util.ArrayList;
 import java.util.Collection;
 import org.apache.ignite.internal.processors.odbc.ClientListenerProtocolVersion;
+import org.apache.ignite.internal.processors.platform.utils.PlatformConfigurationUtils;
 
+import static org.apache.ignite.internal.processors.platform.client.ClientConnectionContext.VER_1_6_0;
 import static org.apache.ignite.internal.processors.platform.utils.PlatformConfigurationUtils.readQueryEntity;
 import static org.apache.ignite.internal.processors.platform.utils.PlatformConfigurationUtils.writeEnumInt;
 import static org.apache.ignite.internal.processors.platform.utils.PlatformConfigurationUtils.writeQueryEntity;
@@ -129,6 +131,9 @@ public class ClientCacheConfigurationSerializer {
     /** */
     private static final short STATISTICS_ENABLED = 406;
 
+    /** */
+    private static final short EXPIRY_POLICY = 407;
+
 
     /**
      * Writes the cache configuration.
@@ -196,6 +201,9 @@ public class ClientCacheConfigurationSerializer {
         } else
             writer.writeInt(0);
 
+        if (ver.compareTo(VER_1_6_0) >= 0)
+            PlatformConfigurationUtils.writeExpiryPolicyFactory(writer, cfg.getExpiryPolicyFactory());
+
         // Write length (so that part of the config can be skipped).
         writer.writeInt(pos, writer.out().position() - pos - 4);
     }
@@ -240,6 +248,10 @@ public class ClientCacheConfigurationSerializer {
 
                 case EAGER_TTL:
                     cfg.setEagerTtl(reader.readBoolean());
+                    break;
+
+                case EXPIRY_POLICY:
+                    cfg.setExpiryPolicyFactory(PlatformConfigurationUtils.readExpiryPolicyFactory(reader));
                     break;
 
                 case STATISTICS_ENABLED: