b8e537cca989996e9da0406789328c35f62431f8
Alexey Goncharuk <alexey.goncharuk@gmail.com>
2020-03-24 13:43:29 +0300

    GG-28219 Deprecate IgniteCache#lock method family

--- modules/core/src/main/java/org/apache/ignite/IgniteCache.java
+++ modules/core/src/main/java/org/apache/ignite/IgniteCache.java
@@ -325,7 +325,10 @@ public interface IgniteCache<K, V> extends javax.cache.Cache<K, V>, IgniteAsyncS
      * @return New lock instance associated with passed key.
      * @see Lock#lock()
      * @see Lock#tryLock(long, TimeUnit)
+     * @deprecated It is recommended to use {@link Ignite#reentrantLock(String, boolean, boolean, boolean)} instead.
+     *      This method will be removed in  future releases.
      */
+    @Deprecated
     public Lock lock(K key);
 
     /**
@@ -338,7 +341,10 @@ public interface IgniteCache<K, V> extends javax.cache.Cache<K, V>, IgniteAsyncS
      * @return New lock instance associated with passed key.
      * @see Lock#lock()
      * @see Lock#tryLock(long, TimeUnit)
+     * @deprecated It is recommended to use {@link Ignite#reentrantLock(String, boolean, boolean, boolean)} instead.
+     *      This method will be removed in  future releases.
      */
+    @Deprecated
     public Lock lockAll(Collection<? extends K> keys);
 
     /**
@@ -351,7 +357,10 @@ public interface IgniteCache<K, V> extends javax.cache.Cache<K, V>, IgniteAsyncS
      * @param byCurrThread If {@code true} method will check that current thread owns a lock on this key, other vise
      *     will check that any thread on any node owns a lock on this key.
      * @return {@code True} if lock is owned by some node.
+     * @deprecated It is recommended to use the lock instance obtained via
+     *      {@link Ignite#reentrantLock(String, boolean, boolean, boolean)} method.
      */
+    @Deprecated
     public boolean isLocalLocked(K key, boolean byCurrThread);
 
     /**
