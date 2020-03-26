6ff9c6adbf772c1917b85beda6f0ec7d6f52b603
korlov42 <korlov@gridgain.com>
2020-03-26 10:39:05 +0300

    GG-28142 Enable OOM protection for 8.7

--- modules/indexing/src/test/java/org/apache/ignite/internal/metric/SqlStatisticsMemoryQuotaTest.java
+++ modules/indexing/src/test/java/org/apache/ignite/internal/metric/SqlStatisticsMemoryQuotaTest.java
@@ -19,10 +19,10 @@ package org.apache.ignite.internal.metric;
 import java.util.concurrent.TimeUnit;
 import org.apache.ignite.IgniteCache;
 import org.apache.ignite.cache.query.SqlFieldsQuery;
-import org.apache.ignite.configuration.IgniteConfiguration;
 import org.apache.ignite.internal.IgniteInternalFuture;
 import org.apache.ignite.internal.processors.metric.MetricRegistry;
 import org.apache.ignite.internal.processors.query.h2.H2MemoryTracker;
+import org.apache.ignite.internal.util.IgniteUtils;
 import org.apache.ignite.spi.metric.LongMetric;
 import org.apache.ignite.spi.metric.Metric;
 import org.junit.After;
@@ -67,12 +67,6 @@ public class SqlStatisticsMemoryQuotaTest extends SqlStatisticsAbstractTest {
         SqlStatisticsAbstractTest.SuspendQuerySqlFunctions.refresh();
     }
 
-    /** {@inheritDoc} */
-    @Override protected IgniteConfiguration getConfiguration(String igniteInstanceName) throws Exception {
-        return super.getConfiguration(igniteInstanceName)
-            .setSqlGlobalMemoryQuota(String.valueOf(Runtime.getRuntime().maxMemory() / 2));
-    }
-
     /**
      * Check values of all sql memory metrics right after grid starts and no queries are executed.
      *
@@ -85,7 +79,7 @@ public class SqlStatisticsMemoryQuotaTest extends SqlStatisticsAbstractTest {
         assertEquals(0, longMetricValue(0, "requests"));
         assertEquals(0, longMetricValue(1, "requests"));
 
-        long dfltSqlGlobQuota = Runtime.getRuntime().maxMemory() / 2;
+        long dfltSqlGlobQuota = IgniteUtils.parseBytes("60%");
 
         assertTrue(almostEquals(dfltSqlGlobQuota, longMetricValue(0, "maxMem"), (long)(dfltSqlGlobQuota * 0.05)));
         assertTrue(almostEquals(dfltSqlGlobQuota, longMetricValue(1, "maxMem"), (long)(dfltSqlGlobQuota * 0.05)));
