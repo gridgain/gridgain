7d38bf0fb545f37d6a641d1947a88d3ef6530ffe
Pavel Tupitsyn <ptupitsyn@apache.org>
2020-04-02 21:54:56 +0300

    cleanup

--- modules/platforms/dotnet/Apache.Ignite.Core/Impl/Binary/Structure/BinaryStructureJumpTable.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core/Impl/Binary/Structure/BinaryStructureJumpTable.cs
@@ -78,10 +78,7 @@ namespace Apache.Ignite.Core.Impl.Binary.Structure
             for (var i = 0; i < _names.Length; i++)
             {
                 if (fieldName.Equals(_names[i]))
-                {
-                    // TODO: Not covered with tests.
                     return _pathIdxs[i];
-                }
             }
 
             // No path found for the field.
