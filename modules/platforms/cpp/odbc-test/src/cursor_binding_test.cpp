c86808d6e3259610c1619a5eeac6b262db19c558
Igor Sapego <isapego@apache.org>
2020-04-07 17:34:51 -0700

    GG-27288: Improved test

--- modules/platforms/cpp/odbc-test/src/cursor_binding_test.cpp
+++ modules/platforms/cpp/odbc-test/src/cursor_binding_test.cpp
@@ -91,18 +91,39 @@ BOOST_FIXTURE_TEST_SUITE(CursorBindingTestSuite, CursorBindingTestSuiteFixture)
         {                                                                                                           \
             BOOST_CHECK(RowStatus[idx] == SQL_ROW_SUCCESS || RowStatus[idx] == SQL_ROW_SUCCESS_WITH_INFO);          \
                                                                                                                     \
+            BOOST_CHECK(i8FieldsInd[idx] != SQL_NULL_DATA);                                                         \
+            BOOST_CHECK(i16FieldsInd[idx] != SQL_NULL_DATA);                                                        \
             BOOST_CHECK(i32FieldsInd[idx] != SQL_NULL_DATA);                                                        \
             BOOST_CHECK(strFieldsLen[idx] != SQL_NULL_DATA);                                                        \
+            BOOST_CHECK(floatFields[idx] != SQL_NULL_DATA);                                                         \
             BOOST_CHECK(doubleFieldsInd[idx] != SQL_NULL_DATA);                                                     \
+            BOOST_CHECK(boolFieldsInd[idx] != SQL_NULL_DATA);                                                       \
+            BOOST_CHECK(dateFieldsInd[idx] != SQL_NULL_DATA);                                                       \
+            BOOST_CHECK(timeFieldsInd[idx] != SQL_NULL_DATA);                                                       \
+            BOOST_CHECK(timestampFieldsInd[idx] != SQL_NULL_DATA);                                                  \
+            BOOST_CHECK(i8ArrayFieldsLen[idx] != SQL_NULL_DATA);                                                    \
                                                                                                                     \
+            int8_t i8Field = static_cast<int8_t>(i8Fields[idx]);                                                    \
+            int16_t i16Field = static_cast<int16_t>(i16Fields[idx]);                                                \
             int32_t i32Field = static_cast<int32_t>(i32Fields[idx]);                                                \
-            double doubleField = static_cast<double>(doubleFields[idx]);                                            \
             std::string strField(reinterpret_cast<char*>(&strFields[idx][0]),                                       \
                 static_cast<size_t>(strFieldsLen[idx]));                                                            \
+            float floatField = static_cast<float>(floatFields[idx]);                                                \
+            double doubleField = static_cast<double>(doubleFields[idx]);                                            \
+            bool boolField = boolFields[idx] != 0;                                                                  \
                                                                                                                     \
+            CheckTestI8Value(testIdx, i8Field);                                                                     \
+            CheckTestI16Value(testIdx, i16Field);                                                                   \
             CheckTestI32Value(testIdx, i32Field);                                                                   \
-            CheckTestDoubleValue(testIdx, doubleField);                                                             \
             CheckTestStringValue(testIdx, strField);                                                                \
+            CheckTestFloatValue(testIdx, floatField);                                                               \
+            CheckTestDoubleValue(testIdx, doubleField);                                                             \
+            CheckTestBoolValue(testIdx, boolField);                                                                 \
+            CheckTestDateValue(testIdx, dateFields[idx]);                                                           \
+            CheckTestTimeValue(testIdx, timeFields[idx]);                                                           \
+            CheckTestTimestampValue(testIdx, timestampFields[idx]);                                                 \
+            CheckTestI8ArrayValue(testIdx, reinterpret_cast<int8_t*>(i8ArrayFields[idx]),                           \
+                static_cast<SQLLEN>(i8ArrayFieldsLen[idx]));                                                        \
         }                                                                                                           \
     } while (false)
 
@@ -110,28 +131,20 @@ BOOST_AUTO_TEST_CASE(TestCursorBindingColumnWise)
 {
     enum { ROWS_COUNT = 15 };
     enum { ROW_ARRAY_SIZE = 10 };
-    enum { STRING_SIZE = 1024 };
+    enum { BUFFER_SIZE = 1024 };
 
     Connect("DRIVER={Apache Ignite};ADDRESS=127.0.0.1:11110;SCHEMA=cache;PAGE_SIZE=8");
 
     // Preloading data.
+
     InsertTestBatch(0, ROWS_COUNT, ROWS_COUNT);
 
-    SQLINTEGER i32Fields[ROW_ARRAY_SIZE] = {0};
-    SQLLEN i32FieldsInd[ROW_ARRAY_SIZE];
-
-    SQLCHAR strFields[ROW_ARRAY_SIZE][STRING_SIZE];
-    SQLLEN strFieldsLen[ROW_ARRAY_SIZE];
-
-    SQLDOUBLE doubleFields[ROW_ARRAY_SIZE];
-    SQLLEN doubleFieldsInd[ROW_ARRAY_SIZE];
+    // Setting attributes.
 
     SQLUSMALLINT RowStatus[ROW_ARRAY_SIZE];
     SQLUINTEGER NumRowsFetched;
 
     SQLRETURN ret;
-
-    // Setting attributes.
 
     ret = SQLSetStmtAttr(stmt, SQL_ATTR_ROW_BIND_TYPE, SQL_BIND_BY_COLUMN, 0);
     ODBC_THROW_ON_ERROR(ret, SQL_HANDLE_STMT, stmt);
@@ -147,17 +160,79 @@ BOOST_AUTO_TEST_CASE(TestCursorBindingColumnWise)
 
     // Binding collumns.
 
-    ret = SQLBindCol(stmt, 1, SQL_C_LONG, i32Fields, 0, i32FieldsInd);
+    SQLSCHAR i8Fields[ROW_ARRAY_SIZE] = {0};
+    SQLLEN i8FieldsInd[ROW_ARRAY_SIZE];
+
+    SQLSMALLINT i16Fields[ROW_ARRAY_SIZE] = {0};
+    SQLLEN i16FieldsInd[ROW_ARRAY_SIZE];
+
+    SQLINTEGER i32Fields[ROW_ARRAY_SIZE] = {0};
+    SQLLEN i32FieldsInd[ROW_ARRAY_SIZE];
+
+    SQLCHAR strFields[ROW_ARRAY_SIZE][BUFFER_SIZE];
+    SQLLEN strFieldsLen[ROW_ARRAY_SIZE];
+
+    SQLREAL floatFields[ROW_ARRAY_SIZE];
+    SQLLEN floatFieldsInd[ROW_ARRAY_SIZE];
+
+    SQLDOUBLE doubleFields[ROW_ARRAY_SIZE];
+    SQLLEN doubleFieldsInd[ROW_ARRAY_SIZE];
+
+    SQLCHAR boolFields[ROW_ARRAY_SIZE];
+    SQLLEN boolFieldsInd[ROW_ARRAY_SIZE];
+
+    SQL_DATE_STRUCT dateFields[ROW_ARRAY_SIZE];
+    SQLLEN dateFieldsInd[ROW_ARRAY_SIZE];
+
+    SQL_TIME_STRUCT timeFields[ROW_ARRAY_SIZE];
+    SQLLEN timeFieldsInd[ROW_ARRAY_SIZE];
+
+    SQL_TIMESTAMP_STRUCT timestampFields[ROW_ARRAY_SIZE];
+    SQLLEN timestampFieldsInd[ROW_ARRAY_SIZE];
+
+    SQLCHAR i8ArrayFields[ROW_ARRAY_SIZE][BUFFER_SIZE];
+    SQLLEN i8ArrayFieldsLen[ROW_ARRAY_SIZE];
+
+    ret = SQLBindCol(stmt, 1, SQL_C_STINYINT, i8Fields, 0, i8FieldsInd);
     ODBC_THROW_ON_ERROR(ret, SQL_HANDLE_STMT, stmt);
 
-    ret = SQLBindCol(stmt, 2, SQL_C_CHAR, strFields, STRING_SIZE, strFieldsLen);
+    ret = SQLBindCol(stmt, 2, SQL_C_SSHORT, i16Fields, 0, i16FieldsInd);
     ODBC_THROW_ON_ERROR(ret, SQL_HANDLE_STMT, stmt);
 
-    ret = SQLBindCol(stmt, 3, SQL_C_DOUBLE, doubleFields, 0, doubleFieldsInd);
+    ret = SQLBindCol(stmt, 3, SQL_C_LONG, i32Fields, 0, i32FieldsInd);
     ODBC_THROW_ON_ERROR(ret, SQL_HANDLE_STMT, stmt);
+
+    ret = SQLBindCol(stmt, 4, SQL_C_CHAR, strFields, BUFFER_SIZE, strFieldsLen);
+    ODBC_THROW_ON_ERROR(ret, SQL_HANDLE_STMT, stmt);
+
+    ret = SQLBindCol(stmt, 5, SQL_C_FLOAT, floatFields, 0, floatFieldsInd);
+    ODBC_THROW_ON_ERROR(ret, SQL_HANDLE_STMT, stmt);
+
+    ret = SQLBindCol(stmt, 6, SQL_C_DOUBLE, doubleFields, 0, doubleFieldsInd);
+    ODBC_THROW_ON_ERROR(ret, SQL_HANDLE_STMT, stmt);
+
+    ret = SQLBindCol(stmt, 7, SQL_C_BIT, boolFields, 0, boolFieldsInd);
+    ODBC_THROW_ON_ERROR(ret, SQL_HANDLE_STMT, stmt);
+
+    ret = SQLBindCol(stmt, 8, SQL_C_DATE, dateFields, 0, dateFieldsInd);
+    ODBC_THROW_ON_ERROR(ret, SQL_HANDLE_STMT, stmt);
+
+    ret = SQLBindCol(stmt, 9, SQL_C_TIME, timeFields, 0, timeFieldsInd);
+    ODBC_THROW_ON_ERROR(ret, SQL_HANDLE_STMT, stmt);
+
+    ret = SQLBindCol(stmt, 10, SQL_C_TIMESTAMP, timestampFields, 0, timestampFieldsInd);
+    ODBC_THROW_ON_ERROR(ret, SQL_HANDLE_STMT, stmt);
+
+    ret = SQLBindCol(stmt, 11, SQL_C_BINARY, i8ArrayFields, BUFFER_SIZE, i8ArrayFieldsLen);
+    ODBC_THROW_ON_ERROR(ret, SQL_HANDLE_STMT, stmt);
+
+    SQLCHAR sql[] = "SELECT "
+            "i8Field, i16Field, i32Field, strField, floatField, doubleField, "
+            "boolField, dateField, timeField, timestampField, i8ArrayField "
+            "FROM TestType "
+            "ORDER BY _key";
 
     // Execute a statement to retrieve rows from the Orders table.
-    SQLCHAR sql[] = "SELECT i32Field, strField, doubleField FROM TestType ORDER BY _key";
     ret = SQLExecDirect(stmt, sql, SQL_NTS);
     ODBC_THROW_ON_ERROR(ret, SQL_HANDLE_STMT, stmt);
 
