120d0d99e23e773ecd6fd5f445ec6bab92ad8780
Sergey Antonov <antonovsergey93@gmail.com>
2020-04-10 13:43:01 +0300

    GG-23133 Utility test added.

--- modules/indexing/src/test/java/org/apache/ignite/util/GridCommandHandlerCheckIndexesInlineSizeTest.java
+++ modules/indexing/src/test/java/org/apache/ignite/util/GridCommandHandlerCheckIndexesInlineSizeTest.java
@@ -17,21 +17,29 @@
 package org.apache.ignite.util;
 
 import java.util.List;
+import java.util.UUID;
 import org.apache.ignite.cache.query.SqlFieldsQuery;
+import org.apache.ignite.configuration.IgniteConfiguration;
 import org.apache.ignite.internal.IgniteEx;
 import org.apache.ignite.testframework.junits.WithSystemProperty;
 import org.junit.Test;
 
+import static java.lang.String.format;
 import static org.apache.ignite.IgniteSystemProperties.IGNITE_MAX_INDEX_PAYLOAD_SIZE;
 import static org.apache.ignite.internal.commandline.CommandHandler.EXIT_CODE_OK;
+import static org.apache.ignite.testframework.GridTestUtils.assertContains;
 
 /**
- *
+ * Checks utility output if check_index_inline_sizes command found problems.
  */
 @WithSystemProperty(key = IGNITE_MAX_INDEX_PAYLOAD_SIZE, value = "1")
 public class GridCommandHandlerCheckIndexesInlineSizeTest extends GridCommandHandlerAbstractTest {
     /** */
     private static final String STR = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    /** */
+    private static final String INDEX_PROBLEM_FMT =
+        "Full index name: PUBLIC#TEST_TABLE#%s nodes: [%s] inline size: 1, nodes: [%s] inline size: 2";
 
     /** Nodes count. */
     private static final int NODES_CNT = 2;
@@ -41,6 +49,19 @@ public class GridCommandHandlerCheckIndexesInlineSizeTest extends GridCommandHan
 
     /** Max payload inline index size. */
     private int payloadSize;
+
+    /** */
+    private static final UUID remoteNodeId = UUID.randomUUID();
+
+    /** {@inheritDoc} */
+    @Override protected IgniteConfiguration getConfiguration(String igniteInstanceName) throws Exception {
+        IgniteConfiguration cfg = super.getConfiguration(igniteInstanceName);
+
+        if (isRemoteJvm(igniteInstanceName))
+            cfg.setNodeId(remoteNodeId);
+
+        return cfg;
+    }
 
     /** {@inheritDoc} */
     @Override protected void beforeTestsStarted() throws Exception {
@@ -95,13 +116,26 @@ public class GridCommandHandlerCheckIndexesInlineSizeTest extends GridCommandHan
         return super.startGrid(idx);
     }
 
+    /**
+     * See class description.
+     */
     @Test
-    public void test() throws Exception {
+    public void test() {
         injectTestSystemOut();
 
-        assertEquals(EXIT_CODE_OK, execute("--verbose","--cache", "check_index_inline_sizes"));
+        assertEquals(EXIT_CODE_OK, execute("--cache", "check_index_inline_sizes"));
 
-        log.error("GG-23133: \n" + testOut.toString());
+        UUID locNodeId = grid(0).localNode().id();
+
+        String utilityOutput = testOut.toString();
+
+        assertContains(log, utilityOutput, "3 indexes have different effective inline size on nodes. It can lead to performance degradation in SQL queries.");
+        assertContains(log, utilityOutput, "Indexes:");
+        assertContains(log, utilityOutput, format(INDEX_PROBLEM_FMT, "L_IDX", locNodeId, remoteNodeId));
+        assertContains(log, utilityOutput, format(INDEX_PROBLEM_FMT, "S1_IDX", locNodeId, remoteNodeId));
+        assertContains(log, utilityOutput, format(INDEX_PROBLEM_FMT, "I_IDX", locNodeId, remoteNodeId));
+        assertContains(log, utilityOutput, "  Check that value of property IGNITE_MAX_INDEX_PAYLOAD_SIZE are the same on all nodes.");
+        assertContains(log, utilityOutput, "  Recreate indexes with different inline size.");
     }
 
     /** */
