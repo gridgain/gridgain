777c88d9273327fe010e05ef8b1d12ebd35df9d9
rkondakov <kondakov87@mail.ru>
2020-04-16 21:31:29 +0300

    Revert "GG-27289: SQLRowCount implemented for SELECT queries (#1037)"
    This reverts commit e3d64632

--- modules/platforms/cpp/odbc/src/cursor.cpp
+++ modules/platforms/cpp/odbc/src/cursor.cpp
@@ -20,11 +20,8 @@ namespace ignite
 {
     namespace odbc
     {
-        Cursor::Cursor(int64_t queryId) :
-            queryId(queryId),
-            currentPage(),
-            currentPagePos(0),
-            currentRow()
+        Cursor::Cursor(int64_t queryId) : queryId(queryId), currentPage(),
+            currentPagePos(0), currentRow()
         {
             // No-op.
         }
