20f4e261c2397c1077ff2e176bf988fb792adb65
Vladislav Pyatkov <vldpyatkov@gmail.com>
2020-04-24 09:28:45 +0300

    Got rid of logic of pending server exchange.

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/distributed/dht/preloader/GridDhtPreloader.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/distributed/dht/preloader/GridDhtPreloader.java
@@ -221,15 +221,15 @@ public class GridDhtPreloader extends GridCachePreloaderAdapter {
         boolean changed = false;
 
         for (int p = 0; p < partitions; p++) {
-            if (ctx.exchange().hasPendingServerExchange()) {
-                if (log.isDebugEnabled())
-                    log.debug("Skipping assignments creation, exchange worker has pending assignments: " +
-                        exchId);
-
-                assignments.cancelled(true);
-
-                return assignments;
-            }
+//            if (ctx.exchange().hasPendingServerExchange()) {
+//                if (log.isDebugEnabled())
+//                    log.debug("Skipping assignments creation, exchange worker has pending assignments: " +
+//                        exchId);
+//
+//                assignments.cancelled(true);
+//
+//                return assignments;
+//            }
 
             // If partition belongs to local node.
             if (aff.get(p).contains(ctx.localNode())) {
