3be870b03994f7d662c71e61bafed33813ac9463
Vladislav Pyatkov <vldpyatkov@gmail.com>
2020-05-03 09:31:56 +0300

    Removed unused import.

--- modules/core/src/main/java/org/apache/ignite/internal/processors/affinity/GridAffinityAssignmentCache.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/affinity/GridAffinityAssignmentCache.java
@@ -17,7 +17,6 @@
 package org.apache.ignite.internal.processors.affinity;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
