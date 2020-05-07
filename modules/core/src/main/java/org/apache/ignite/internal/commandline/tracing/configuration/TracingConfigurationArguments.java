817a1c579b2f1cd4573b7badf72107682d80c98c
sanpwc <lapin1702@gmail.com>
2020-05-07 14:25:22 +0300

    GG-29163 Introducing TracingConfigurationArguments.
    Signed-off-by: alapin <lapin1702@gmail.com>

---
+++ modules/core/src/main/java/org/apache/ignite/internal/commandline/tracing/configuration/TracingConfigurationArguments.java
@@ -0,0 +1,165 @@
+/*
+ * Copyright 2020 GridGain Systems, Inc. and Contributors.
+ *
+ * Licensed under the GridGain Community Edition License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.gridgain.com/products/software/community-edition/gridgain-community-edition-license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.ignite.internal.commandline.tracing.configuration;
+
+import java.util.Collections;
+import java.util.Set;
+import org.apache.ignite.internal.processors.tracing.Scope;
+import org.apache.ignite.internal.processors.tracing.Span;
+import org.apache.ignite.internal.visor.tracing.configuration.VisorTracingConfigurationItem;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * This class contains all possible arguments after parsing tracing-configuration command input.
+ */
+ public final class TracingConfigurationArguments extends VisorTracingConfigurationItem {
+    /** Command. */
+    private TracingConfigurationSubcommand cmd;
+
+    /**
+     * Constructor.
+     *
+     * @param cmd Command
+     * @param scope Specifies the {@link Scope} of a trace's root span to which some specific tracing configuration will be applied.
+     * @param lb Specifies the label of a traced operation. It's an optional attribute.
+     * @param samplingRate Number between 0 and 1 that more or less reflects the probability of sampling specific trace.
+     *  0 and 1 have special meaning here, 0 means never 1 means always. Default value is 0 (never).
+     * @param supportedScopes Set of {@link Scope} that defines which sub-traces will be included in given trace.
+     *  In other words, if child's span scope is equals to parent's scope
+     *  or it belongs to the parent's span supported scopes, then given child span will be attached to the current trace,
+     *  otherwise it'll be skipped.
+     *  See {@link Span#isChainable(org.apache.ignite.internal.processors.tracing.Scope)} for more details.
+     */
+    public TracingConfigurationArguments(
+        TracingConfigurationSubcommand cmd,
+        Scope scope,
+        String lb,
+        double samplingRate,
+        Set<Scope> supportedScopes)
+    {
+        super(
+            scope,
+            lb,
+            samplingRate,
+            supportedScopes);
+
+        this.cmd = cmd;
+    }
+
+    /**
+     * @return Command.
+     */
+    public TracingConfigurationSubcommand command() {
+        return cmd;
+    }
+
+    /**
+     * Builder of {@link TracingConfigurationArguments}.
+     */
+    @SuppressWarnings("PackageVisibleInnerClass") static class Builder {
+        /** Counterpart of {@code TracingConfigurationArguments}'s command. */
+        private TracingConfigurationSubcommand cmd;
+
+        /** Counterpart of {@code TracingConfigurationArguments}'s scope. */
+        private Scope scope;
+
+        /** Counterpart of {@code TracingConfigurationArguments}'s label. */
+        private String lb;
+
+        /** Counterpart of {@code TracingConfigurationArguments}'s samplingRate. */
+        private double samplingRate;
+
+        /** Counterpart of {@code TracingConfigurationArguments}'s supportedScopes. */
+        private Set<Scope> supportedScopes;
+
+        /**
+         * Constructor.
+         *
+         * @param cmd {@code TracingConfigurationSubcommand} command.
+         */
+        public Builder(TracingConfigurationSubcommand cmd) {
+            this.cmd = cmd;
+        }
+
+        /**
+         * Builder method that allows to set scope.
+         * @param scope {@link Scope} of a trace's root span to which some specific tracing configuration will be applied.
+         * @return Builder.
+         */
+        public @NotNull Builder withScope(Scope scope) {
+            this.scope = scope;
+
+            return this;
+        }
+
+        /**
+         * Builder method that allows to set sampling rate.
+         *
+         * @param samplingRate Number between 0 and 1 that more or less reflects the probability of sampling specific trace.
+         * 0 and 1 have special meaning here, 0 means never 1 means always. Default value is 0 (never).
+         * @return Builder.
+         */
+        public @NotNull Builder withSamplingRate(double samplingRate) {
+            this.samplingRate = samplingRate;
+
+            return this;
+        }
+
+        /**
+         * Builder method that allows to set optional label attribute.
+         *
+         * @param lb Label of traced operation. It's an optional attribute.
+         * @return Current {@code TracingConfigurationCoordinates} instance.
+         */
+        public @NotNull Builder withLabel(@Nullable String lb) {
+            this.lb = lb;
+
+            return this;
+        }
+
+        /**
+         * Builder method that allows to set supported scopes.
+         *
+         * @param supportedScopes Set of {@link Scope} that defines which sub-traces will be included in given trace.
+         * In other words, if child's span scope is equals to parent's scope
+         * or it belongs to the parent's span supported scopes, then given child span will be attached to the current trace,
+         * otherwise it'll be skipped.
+         * See {@link Span#isChainable(org.apache.ignite.internal.processors.tracing.Scope)} for more details.
+         * @return {@code TracingConfigurationParameters} instance.
+         */
+        public @NotNull Builder withSupportedScopes(Set<Scope> supportedScopes) {
+            this.supportedScopes = supportedScopes == null ? Collections.emptySet() : supportedScopes;
+
+            return this;
+        }
+
+        /**
+         * Builder's build method that produces {@link TracingConfigurationArguments}.
+         *
+         * @return {@code TracingConfigurationArguments} instance.
+         */
+        public TracingConfigurationArguments build() {
+            return new TracingConfigurationArguments(
+                cmd,
+                scope,
+                lb,
+                samplingRate,
+                supportedScopes);
+        }
+    }
+}
