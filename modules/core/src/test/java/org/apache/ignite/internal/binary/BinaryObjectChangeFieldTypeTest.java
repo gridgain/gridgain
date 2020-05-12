0bb241179e943f9636bc0d8bada0e6547e3826f5
VladimirGoncharov-GG <vgoncharov@gridgain.com>
2020-05-12 15:23:43 +0300

    [gg-19651] Fix test

--- modules/core/src/test/java/org/apache/ignite/internal/binary/BinaryObjectChangeFieldTypeTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/binary/BinaryObjectChangeFieldTypeTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2020 GridGain Systems, Inc. and Contributors.
+ *
+ * Licensed under the GridGain Community Edition License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.gridgain.com/products/software/community-edition/gridgain-community-edition-license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.ignite.internal.binary;
 
 import java.util.concurrent.Callable;
@@ -17,7 +33,6 @@ import org.apache.ignite.testframework.GridTestUtils;
 import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
 import org.apache.ignite.transactions.Transaction;
 import org.apache.ignite.transactions.TransactionConcurrency;
-import org.apache.ignite.transactions.TransactionHeuristicException;
 import org.apache.ignite.transactions.TransactionIsolation;
 import org.junit.Test;
 
@@ -73,31 +88,31 @@ public class BinaryObjectChangeFieldTypeTest extends GridCommonAbstractTest {
     }
 
     /**
-     * In progress
-     * @throws Exception exc
+     * Check commit changed BinaryObject to non-backup node
      */
     @Test
-    public void testTxNoBackups() throws Exception {
+    public void testTxNoBackups() {
 
-        CacheConfiguration<Integer, Foo> cc = new CacheConfiguration<Integer, Foo>(CACHE_NAME)
+        CacheConfiguration<Integer, TestClass> cc = new CacheConfiguration<Integer, TestClass>(CACHE_NAME)
             .setAtomicityMode(CacheAtomicityMode.TRANSACTIONAL)
             .setCacheMode(CacheMode.PARTITIONED)
-            .setIndexedTypes(Integer.class, Foo.class);
+            .setIndexedTypes(Integer.class, TestClass.class);
 
-        IgniteCache<Integer, Foo> fooCache = ignite.getOrCreateCache(cc);
+        IgniteCache<Integer, TestClass> fooCache = ignite.getOrCreateCache(cc);
 
         IgniteCache<Integer, BinaryObject> binCache = fooCache.withKeepBinary();
 
         try (Transaction tx = transactions.txStart()) {
-            fooCache.put(1, new Foo("dummy", 123));
+            fooCache.put(1, new TestClass("dummy", 123));
+            tx.commit();
         }
 
         assertEquals(123, fooCache.get(1).intField);
 
         try (Transaction tx = transactions.txStart()) {
-            fooCache.put(1, new Foo("dummy", 5));
+            fooCache.put(1, new TestClass("dummy", 5));
 
-            BinaryObject bo = binary.builder(Foo.class.getName())
+            BinaryObject bo = binary.builder(TestClass.class.getName())
                 .build()
                 .toBuilder()
                 .setField("intField", "String")
@@ -106,36 +121,32 @@ public class BinaryObjectChangeFieldTypeTest extends GridCommonAbstractTest {
             binCache.put(1001, bo);
 
             tx.commit();
+            fail("exception expected!!!");
         }
-        catch (TransactionHeuristicException e) {
-            log().warning("expected exception", e);
-
+        catch (Exception e) {
+            assertTrue(e.getMessage().startsWith("Wrong value has been set"));
         }
-
-        // actually genereates NPE
-        assertEquals(123, fooCache.get(1).intField);
     }
 
     /**
-     * In progress
-     * @throws Exception exc
+     * Check commit changed BinaryObject to replicated node
      */
     @Test
-    public void testTxReplicated() throws Exception {
+    public void testTxReplicated(){
 
-        CacheConfiguration<Integer, Foo> cc = new CacheConfiguration<Integer, Foo>(CACHE_NAME)
+        CacheConfiguration<Integer, TestClass> cc = new CacheConfiguration<Integer, TestClass>(CACHE_NAME)
             .setCacheMode(CacheMode.REPLICATED)
             .setAtomicityMode(CacheAtomicityMode.TRANSACTIONAL)
-            .setIndexedTypes(Integer.class, Foo.class);
+            .setIndexedTypes(Integer.class, TestClass.class);
 
-        IgniteCache<Integer, Foo> fooCache = ignite.getOrCreateCache(cc);
+        IgniteCache<Integer, TestClass> fooCache = ignite.getOrCreateCache(cc);
 
         IgniteCache<Integer, BinaryObject> binCache = fooCache.withKeepBinary();
 
         try (Transaction tx = transactions.txStart(TransactionConcurrency.PESSIMISTIC, TransactionIsolation.REPEATABLE_READ)) {
-            fooCache.put(1, new Foo("1", 1));
+            fooCache.put(1, new TestClass("1", 1));
 
-            fooCache.put(2, new Foo("2", 2));
+            fooCache.put(2, new TestClass("2", 2));
 
             tx.commit();
         }
@@ -145,9 +156,9 @@ public class BinaryObjectChangeFieldTypeTest extends GridCommonAbstractTest {
         assertEquals(2, fooCache.get(2).intField);
 
         try (Transaction tx = transactions.txStart(TransactionConcurrency.PESSIMISTIC, TransactionIsolation.REPEATABLE_READ)) {
-            fooCache.put(1, new Foo("11", 11));
+            fooCache.put(1, new TestClass("11", 11));
 
-            BinaryObject bo = binary.builder(Foo.class.getName())
+            BinaryObject bo = binary.builder(TestClass.class.getName())
                 .build()
                 .toBuilder()
                 .setField("intField", "String")
@@ -155,20 +166,16 @@ public class BinaryObjectChangeFieldTypeTest extends GridCommonAbstractTest {
 
             binCache.put(1001, bo);
 
-            fooCache.put(2, new Foo("22", 22));
+            fooCache.put(2, new TestClass("22", 22));
 
             assertEquals(22, fooCache.get(2).intField);
 
             tx.commit();
             fail("exception expected!!!");
         }
-        catch (TransactionHeuristicException e) {
-            log().warning("log expected exception", e);
+        catch (Exception e) {
+            assertTrue(e.getMessage().startsWith("Wrong value has been set"));
         }
-
-        assertEquals(2, fooCache.get(2).intField);
-
-        assertEquals(1, fooCache.get(1).intField);
     }
 
     /**
@@ -176,15 +183,15 @@ public class BinaryObjectChangeFieldTypeTest extends GridCommonAbstractTest {
      */
     @Test
     public void testBuild1() {
-        CacheConfiguration<Integer, Foo> cc = new CacheConfiguration<Integer, Foo>(CACHE_NAME);
+        CacheConfiguration<Integer, TestClass> cc = new CacheConfiguration<Integer, TestClass>(CACHE_NAME);
 
-        IgniteCache<Integer, Foo> clientCache = ignite.getOrCreateCache(cc);
+        IgniteCache<Integer, TestClass> clientCache = ignite.getOrCreateCache(cc);
 
-        clientCache.put(1, new Foo("11", 11));
+        clientCache.put(1, new TestClass("11", 11));
 
         GridTestUtils.assertThrows(log, new Callable<BinaryObject>() {
             @Override public BinaryObject call() throws Exception {
-                return binary.builder(Foo.class.getName())
+                return binary.builder(TestClass.class.getName())
                     .removeField("intField")
                     .build()
                     .toBuilder()
@@ -200,11 +207,11 @@ public class BinaryObjectChangeFieldTypeTest extends GridCommonAbstractTest {
      */
     @Test
     public void testBuild2() {
-        CacheConfiguration<Integer, Foo> cc = new CacheConfiguration<>(CACHE_NAME);
+        CacheConfiguration<Integer, TestClass> cc = new CacheConfiguration<>(CACHE_NAME);
 
-        IgniteCache<Integer, Foo> fooCache = ignite.getOrCreateCache(cc);
+        IgniteCache<Integer, TestClass> fooCache = ignite.getOrCreateCache(cc);
 
-        fooCache.put(1, new Foo("11", 11));
+        fooCache.put(1, new TestClass("11", 11));
 
         GridTestUtils.assertThrows(log, new Callable<BinaryObject>() {
             @Override public BinaryObject call() throws Exception {
@@ -224,7 +231,7 @@ public class BinaryObjectChangeFieldTypeTest extends GridCommonAbstractTest {
     /**
      *
      */
-    static class Foo {
+    static class TestClass {
 
         /**
          *
@@ -242,7 +249,7 @@ public class BinaryObjectChangeFieldTypeTest extends GridCommonAbstractTest {
          * @param strField String field.
          * @param intField Int field.
          */
-        public Foo(String strField, int intField) {
+        public TestClass(String strField, int intField) {
             this.intField = intField;
             this.strField = strField;
         }
