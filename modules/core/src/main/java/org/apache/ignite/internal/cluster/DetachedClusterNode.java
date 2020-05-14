5eb4ec840cf9f7c0e7fb2e99ceb558dcf4d4a6eb
ibessonov <bessonov.ip@gmail.com>
2020-05-14 16:29:42 +0300

    GG-29268 Store detached nodes in snapshot metadata.

--- modules/core/src/main/java/org/apache/ignite/internal/cluster/DetachedClusterNode.java
+++ modules/core/src/main/java/org/apache/ignite/internal/cluster/DetachedClusterNode.java
@@ -16,34 +16,51 @@
 
 package org.apache.ignite.internal.cluster;
 
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
 import java.util.Collection;
 import java.util.Map;
 import java.util.UUID;
 import org.apache.ignite.cluster.ClusterMetrics;
 import org.apache.ignite.cluster.ClusterNode;
+import org.apache.ignite.internal.IgniteNodeAttributes;
 import org.apache.ignite.internal.util.tostring.GridToStringExclude;
 import org.apache.ignite.internal.util.tostring.GridToStringInclude;
 import org.apache.ignite.internal.util.typedef.internal.S;
+import org.apache.ignite.internal.util.typedef.internal.U;
 import org.apache.ignite.lang.IgniteProductVersion;
 import org.jetbrains.annotations.Nullable;
+
+import static org.apache.ignite.internal.IgniteNodeAttributes.ATTR_DAEMON;
 
 /**
  * Representation of cluster node that either isn't currently present in cluster, or semantically detached.
  * For example nodes returned from {@code BaselineTopology.currentBaseline()} are always considered as
  * semantically detached, even if they are currently present in cluster.
  */
-public class DetachedClusterNode implements ClusterNode {
+public class DetachedClusterNode implements ClusterNode, Externalizable {
+    /** Serial version uid. */
+    private static final long serialVersionUID = 0L;
+
     /** */
     @GridToStringExclude
-    private final UUID uuid = UUID.randomUUID();
+    private UUID uuid = UUID.randomUUID();
 
     /** Consistent ID. */
     @GridToStringInclude
-    private final Object consistentId;
+    private Object consistentId;
 
     /** Node attributes. */
     @GridToStringInclude
-    private final Map<String, Object> attributes;
+    private Map<String, Object> attributes;
+
+    /**
+     * Empty constructor required for {@link Externalizable}.
+     */
+    public DetachedClusterNode() {
+    }
 
     /**
      * @param consistentId Consistent ID.
@@ -106,12 +123,27 @@ public class DetachedClusterNode implements ClusterNode {
 
     /** {@inheritDoc} */
     @Override public boolean isDaemon() {
-        return false;
+        return "true".equalsIgnoreCase(attribute(ATTR_DAEMON));
     }
 
     /** {@inheritDoc} */
     @Override public boolean isClient() {
-        return false;
+        return Boolean.TRUE.equals(attribute(IgniteNodeAttributes.ATTR_CLIENT_MODE));
+    }
+
+    /** {@inheritDoc} */
+    @Override public void writeExternal(ObjectOutput out) throws IOException {
+        out.writeObject(consistentId);
+
+        U.writeMap(out, attributes);
+    }
+
+    /** {@inheritDoc} */
+    @Override public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+        uuid = UUID.randomUUID();
+
+        consistentId = in.readObject();
+        attributes = U.sealMap(U.<String, Object>readMap(in));
     }
 
     /** {@inheritDoc} */
