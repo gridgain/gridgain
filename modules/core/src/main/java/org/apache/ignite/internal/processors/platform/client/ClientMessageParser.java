819785f679d9da30af8fa7e9daf5b380c7a0a37b
Pavel Tupitsyn <ptupitsyn@apache.org>
2020-05-26 12:04:37 +0300

    GG-29024 [IGNITE-12835] Thin client: Add compute support

--- modules/core/src/main/java/org/apache/ignite/internal/processors/platform/client/ClientMessageParser.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/platform/client/ClientMessageParser.java
@@ -66,14 +66,15 @@ import org.apache.ignite.internal.processors.platform.client.cache.ClientCacheRe
 import org.apache.ignite.internal.processors.platform.client.cache.ClientCacheScanQueryRequest;
 import org.apache.ignite.internal.processors.platform.client.cache.ClientCacheSqlFieldsQueryRequest;
 import org.apache.ignite.internal.processors.platform.client.cache.ClientCacheSqlQueryRequest;
+import org.apache.ignite.internal.processors.platform.client.compute.ClientExecuteTaskRequest;
+import org.apache.ignite.internal.processors.platform.client.tx.ClientTxEndRequest;
+import org.apache.ignite.internal.processors.platform.client.tx.ClientTxStartRequest;
 import org.apache.ignite.internal.processors.platform.client.cluster.ClientClusterChangeStateRequest;
 import org.apache.ignite.internal.processors.platform.client.cluster.ClientClusterIsActiveRequest;
 import org.apache.ignite.internal.processors.platform.client.cluster.ClientClusterWalChangeStateRequest;
 import org.apache.ignite.internal.processors.platform.client.cluster.ClientClusterWalGetStateRequest;
 import org.apache.ignite.internal.processors.platform.client.cluster.ClientClusterGroupGetNodeIdsRequest;
 import org.apache.ignite.internal.processors.platform.client.cluster.ClientClusterGroupGetNodesDetailsRequest;
-import org.apache.ignite.internal.processors.platform.client.tx.ClientTxEndRequest;
-import org.apache.ignite.internal.processors.platform.client.tx.ClientTxStartRequest;
 
 /**
  * Thin client message parser.
@@ -236,6 +237,13 @@ public class ClientMessageParser implements ClientListenerMessageParser {
 
     /** */
     private static final short OP_CLUSTER_GROUP_GET_NODE_INFO = 5101;
+
+    /* Compute operations. */
+    /** */
+    private static final short OP_COMPUTE_TASK_EXECUTE = 6000;
+
+    /** */
+    public static final short OP_COMPUTE_TASK_FINISHED = 6001;
 
     /* Custom queries working through processors registry. */
     /** */
@@ -436,6 +444,9 @@ public class ClientMessageParser implements ClientListenerMessageParser {
             case OP_CLUSTER_GROUP_GET_NODE_INFO:
                 return new ClientClusterGroupGetNodesDetailsRequest(reader);
 
+            case OP_COMPUTE_TASK_EXECUTE:
+                return new ClientExecuteTaskRequest(reader);
+
             case OP_CUSTOM_QUERY:
                 return new ClientCustomQueryRequest(reader);
         }
@@ -452,7 +463,9 @@ public class ClientMessageParser implements ClientListenerMessageParser {
 
         BinaryRawWriterEx writer = marsh.writer(outStream);
 
-        ((ClientResponse)resp).encode(ctx, writer);
+        assert resp instanceof ClientOutgoingMessage : "Unexpected response type: " + resp.getClass();
+
+        ((ClientOutgoingMessage)resp).encode(ctx, writer);
 
         return outStream.arrayCopy();
     }
