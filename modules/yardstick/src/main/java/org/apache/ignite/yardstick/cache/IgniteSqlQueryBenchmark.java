6d192c30cb9304a8c1b1393d2f4cf826fda42a05
korlov42 <korlov@gridgain.com>
2020-05-26 13:49:33 +0300

    refactored test

--- modules/yardstick/src/main/java/org/apache/ignite/yardstick/cache/IgniteSqlQueryBenchmark.java
+++ modules/yardstick/src/main/java/org/apache/ignite/yardstick/cache/IgniteSqlQueryBenchmark.java
@@ -16,13 +16,11 @@
 
 package org.apache.ignite.yardstick.cache;
 
-import java.util.Collection;
+import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ThreadLocalRandom;
-import javax.cache.Cache;
 import org.apache.ignite.IgniteCache;
 import org.apache.ignite.IgniteDataStreamer;
-import org.apache.ignite.cache.query.SqlQuery;
+import org.apache.ignite.cache.query.SqlFieldsQuery;
 import org.apache.ignite.yardstick.cache.model.Person;
 import org.yardstickframework.BenchmarkConfiguration;
 
@@ -32,6 +30,15 @@ import static org.yardstickframework.BenchmarkUtils.println;
  * Ignite benchmark that performs query operations.
  */
 public class IgniteSqlQueryBenchmark extends IgniteCacheAbstractBenchmark<Integer, Object> {
+    /** Last salary. */
+    private static final String LAST_SALARY = "LAST_SALARY";
+
+    /** Salary step. */
+    private static final int SALARY_STEP = 1000;
+
+    /** */
+    private static final int SALARY_RANGE = 10 * SALARY_STEP;
+
     /** {@inheritDoc} */
     @Override public void setUp(BenchmarkConfiguration cfg) throws Exception {
         super.setUp(cfg);
@@ -46,7 +53,7 @@ public class IgniteSqlQueryBenchmark extends IgniteCacheAbstractBenchmark<Intege
                 if (i % 100 == 0 && Thread.currentThread().isInterrupted())
                     break;
 
-                dataLdr.addData(i, new Person(i, "firstName" + i, "lastName" + i, i * 1000));
+                dataLdr.addData(i, new Person(i, "firstName" + i, "lastName" + i, i * SALARY_STEP));
 
                 if (i % 100000 == 0)
                     println(cfg, "Populated persons: " + i);
@@ -56,19 +63,26 @@ public class IgniteSqlQueryBenchmark extends IgniteCacheAbstractBenchmark<Intege
 
     /** {@inheritDoc} */
     @Override public boolean test(Map<Object, Object> ctx) throws Exception {
-        double salary = ThreadLocalRandom.current().nextDouble() * args.range() * 1000;
+        double minSalary = (double)ctx.getOrDefault(LAST_SALARY, 0.0);
 
-        double maxSalary = salary + 1000;
+        double maxSalary = minSalary + SALARY_RANGE;
 
-        Collection<Cache.Entry<Integer, Object>> entries = executeQuery(salary, maxSalary);
+        List<List<?>> rows = executeQuery(minSalary, maxSalary);
 
-        for (Cache.Entry<Integer, Object> entry : entries) {
-            Person p = (Person)entry.getValue();
+        for (List<?> row : rows) {
+            double salary = (Double)row.get(0);
 
-            if (p.getSalary() < salary || p.getSalary() > maxSalary)
-                throw new Exception("Invalid person retrieved [min=" + salary + ", max=" + maxSalary +
-                        ", person=" + p + ']');
+            if (salary < minSalary || salary > maxSalary)
+                throw new Exception("Invalid person retrieved [min=" + minSalary + ", max=" + maxSalary +
+                        ", salary=" + salary + ']');
         }
+
+        double newMinSalary =  maxSalary - SALARY_RANGE * 1.0 / 2;
+
+        if (newMinSalary + SALARY_RANGE > args.range() * SALARY_STEP)
+            newMinSalary = 0.0;
+
+        ctx.put(LAST_SALARY, newMinSalary);
 
         return true;
     }
@@ -79,10 +93,10 @@ public class IgniteSqlQueryBenchmark extends IgniteCacheAbstractBenchmark<Intege
      * @return Query result.
      * @throws Exception If failed.
      */
-    private Collection<Cache.Entry<Integer, Object>> executeQuery(double minSalary, double maxSalary) throws Exception {
+    private List<List<?>> executeQuery(double minSalary, double maxSalary) {
         IgniteCache<Integer, Object> cache = cacheForOperation(true);
 
-        SqlQuery qry = new SqlQuery(Person.class, "salary >= ? and salary <= ?");
+        SqlFieldsQuery qry = new SqlFieldsQuery("select salary, id, firstName, lastName from Person where salary >= ? and salary <= ?");
 
         qry.setArgs(minSalary, maxSalary);
 
