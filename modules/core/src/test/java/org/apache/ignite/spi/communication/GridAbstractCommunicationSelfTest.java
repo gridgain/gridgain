4fcf069e70aa4f6d6cdcc3ac7bd9913a08284fa8
mstepachev <maksim.stepachev@gmail.com>
2020-05-30 23:41:07 +0300

    GG-28511 TcpCommunicationSpi split to classes for comfort testing

--- modules/core/src/test/java/org/apache/ignite/spi/communication/GridAbstractCommunicationSelfTest.java
+++ modules/core/src/test/java/org/apache/ignite/spi/communication/GridAbstractCommunicationSelfTest.java
@@ -333,7 +333,6 @@ public abstract class GridAbstractCommunicationSelfTest<T extends CommunicationS
 
             spi.setListener(new MessageListener(rsrcs.getNodeId()));
 
-            node.setAttributes(spi.getNodeAttributes());
             node.setAttribute(ATTR_MACS, F.concat(U.allLocalMACs(), ", "));
 
             node.order(i + 1);
@@ -341,6 +340,8 @@ public abstract class GridAbstractCommunicationSelfTest<T extends CommunicationS
             nodes.add(node);
 
             spi.spiStart(getTestIgniteInstanceName() + (i + 1));
+
+            node.setAttributes(spi.getNodeAttributes());
 
             spis.put(rsrcs.getNodeId(), spi);
 
