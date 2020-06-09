024e70bf4a4d41e4f169fd7e92fd79f166c86f5f
Pavel Tupitsyn <ptupitsyn@apache.org>
2020-06-09 13:34:52 +0300

    GG-28374 [IGNITE-7105] .NET: Add IgniteLock
    * Add `IIgnite.GetOrCreateLock` (no `reentrant` because .NET locks are reentrant by default, this is expected)
    * `IIgniteLock` methods are named according to the standard `Monitor` class and existing Ignite `ICacheLock`

--- modules/platforms/dotnet/Apache.Ignite.Core.Tests/TestBase.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests/TestBase.cs
@@ -21,8 +21,19 @@ namespace Apache.Ignite.Core.Tests
     /// <summary>
     /// Code configuration test base.
     /// </summary>
-    public class TestBase
+    public abstract class TestBase
     {
+        /** */
+        private readonly int _gridCount;
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="TestBase"/> class.
+        /// </summary>
+        protected TestBase(int gridCount = 1)
+        {
+            _gridCount = gridCount;
+        }
+
         /// <summary>
         /// Sets up the fixture.
         /// </summary>
@@ -30,6 +41,16 @@ namespace Apache.Ignite.Core.Tests
         public void TestFixtureSetUp()
         {
             Ignition.Start(GetConfig());
+
+            for (var i = 1; i < _gridCount; i++)
+            {
+                var cfg = new IgniteConfiguration(GetConfig())
+                {
+                    IgniteInstanceName = i.ToString()
+                };
+
+                Ignition.Start(cfg);
+            }
         }
 
         /// <summary>
@@ -55,6 +76,14 @@ namespace Apache.Ignite.Core.Tests
         protected IIgnite Ignite
         {
             get { return Ignition.GetIgnite(); }
+        }
+
+        /// <summary>
+        /// Gets the second Ignite instance, if present.
+        /// </summary>
+        protected IIgnite Ignite2
+        {
+            get { return Ignition.GetIgnite("1"); }
         }
     }
 }
