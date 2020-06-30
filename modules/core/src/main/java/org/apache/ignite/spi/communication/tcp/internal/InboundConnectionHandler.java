18bde70a7e2dae4dc601fcc3049661be84e7a6cd
ibessonov <bessonov.ip@gmail.com>
2020-06-30 14:08:17 +0300

    GG-29690 Deadlock in TCP worker fixed when Communication via Discovery is enabled. EnvironmentType setting replaced with a "forceClientToServerConnections" setting of TcpCommunicationSpi. (#1268)

--- modules/core/src/main/java/org/apache/ignite/spi/communication/tcp/internal/InboundConnectionHandler.java
+++ modules/core/src/main/java/org/apache/ignite/spi/communication/tcp/internal/InboundConnectionHandler.java
@@ -634,8 +634,13 @@ public class InboundConnectionHandler extends GridNioServerListenerAdapter<Messa
                     boolean reserved = recoveryDesc.tryReserve(msg0.connectCount(),
                         new ConnectClosure(ses, recoveryDesc, rmtNode, connKey, msg0, !hasShmemClient, fut));
 
+                    GridTcpNioCommunicationClient client = null;
+
                     if (reserved)
-                        connected(recoveryDesc, ses, rmtNode, msg0.received(), true, !hasShmemClient);
+                        client = connected(recoveryDesc, ses, rmtNode, msg0.received(), true, !hasShmemClient);
+
+                    if (oldFut instanceof ConnectionRequestFuture && !oldFut.isDone())
+                        oldFut.onDone(client);
                 }
             }
         }
