231782e1564ab61b369a48a046e5596afc29f810
ibessonov <bessonov.ip@gmail.com>
2020-07-03 09:43:41 +0300

    GG-29900 Addresses duplication fixed.

--- modules/core/src/main/java/org/apache/ignite/internal/util/IgniteUtils.java
+++ modules/core/src/main/java/org/apache/ignite/internal/util/IgniteUtils.java
@@ -9478,7 +9478,7 @@ public abstract class IgniteUtils {
         for (String addr : addrs) {
             String hostName = hostNamesIt.hasNext() ? hostNamesIt.next() : null;
 
-            if (!F.isEmpty(hostName)) {
+            if (!F.isEmpty(hostName) && !addrs.contains(hostName)) {
                 InetSocketAddress inetSockAddr = resolve
                     ? new InetSocketAddress(hostName, port)
                     : InetSocketAddress.createUnresolved(hostName, port);
