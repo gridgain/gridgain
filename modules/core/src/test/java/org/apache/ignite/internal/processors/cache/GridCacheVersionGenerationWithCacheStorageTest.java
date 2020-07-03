c73257118985dc36b8415527dd8d3f68a247407e
mstepachev <maksim.stepachev@gmail.com>
2020-07-03 12:20:41 +0300

    GG-29905 Fixes

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/GridCacheVersionGenerationWithCacheStorageTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/GridCacheVersionGenerationWithCacheStorageTest.java
@@ -210,7 +210,7 @@ public class GridCacheVersionGenerationWithCacheStorageTest extends GridCommonAb
                     latch.await();
                 }
                 catch (InterruptedException e) {
-                    e.printStackTrace();
+                    throw new RuntimeException(e);
                 }
             }
         });
