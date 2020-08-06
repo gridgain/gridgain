e02394dc0f2c9ce532ecf23aa2f319e1ff6877e7
Pavel Vinokurov <vinokurov.pasha@gmail.com>
2020-08-06 07:14:21 +0300

    dev

--- modules/yardstick/src/main/java/org/apache/ignite/yardstick/compute/IgniteNoopBenchmark.java
+++ modules/yardstick/src/main/java/org/apache/ignite/yardstick/compute/IgniteNoopBenchmark.java
@@ -5,8 +5,6 @@ import org.apache.ignite.yardstick.IgniteAbstractBenchmark;
 
 public class IgniteNoopBenchmark extends IgniteAbstractBenchmark {
     @Override public boolean test(Map<Object, Object> ctx) throws Exception {
-        Thread.sleep(100);
-
         return true;
     }
 }
