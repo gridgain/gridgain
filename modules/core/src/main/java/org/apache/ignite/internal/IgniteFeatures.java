d4d40733c0d7e9b1bcb30e12b3dfd8a651f81ecf
Nikita Tolstunov <ntolstunov@gmail.com>
2020-08-10 20:01:17 +0300

    GG-30129 Support PITR with remote snapshot with SFTP - add PITR test

--- modules/core/src/main/java/org/apache/ignite/internal/IgniteFeatures.java
+++ modules/core/src/main/java/org/apache/ignite/internal/IgniteFeatures.java
@@ -166,7 +166,10 @@ public enum IgniteFeatures {
     INDEXES_MANIPULATIONS_FROM_CONTROL_SCRIPT(42),
 
     /** Snapshots without PME. */
-    EXCHANGELESS_SNAPSHOT(43);
+    EXCHANGELESS_SNAPSHOT(43),
+
+    /** Snapshots uploading via SFTP. */
+    SNAPSHOT_SFTP_UPLOAD(44);
 
     /**
      * Unique feature identifier.
