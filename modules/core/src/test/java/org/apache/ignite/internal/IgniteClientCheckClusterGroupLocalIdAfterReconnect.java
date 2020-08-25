587f09f8a517d7194bbaa689bbe79a6d2a3e8182
Andrei Aleksandrov <aealexsandrov@gmail.com>
2020-08-25 14:15:49 +0300

    GG-30588 fixes after review

--- modules/core/src/test/java/org/apache/ignite/internal/IgniteClientCheckClusterGroupLocalIdAfterReconnect.java
+++ modules/core/src/test/java/org/apache/ignite/internal/IgniteClientCheckClusterGroupLocalIdAfterReconnect.java
@@ -20,8 +20,6 @@ import java.io.Externalizable;
 import java.io.IOException;
 import java.io.ObjectInput;
 import java.io.ObjectOutput;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
@@ -34,9 +32,6 @@ import org.apache.ignite.events.EventType;
 import org.apache.ignite.lang.IgniteBiPredicate;
 import org.apache.ignite.lang.IgniteFuture;
 import org.apache.ignite.lang.IgnitePredicate;
-import org.apache.ignite.spi.communication.tcp.TcpCommunicationSpi;
-import org.apache.ignite.spi.discovery.tcp.TcpDiscoverySpi;
-import org.apache.ignite.spi.discovery.tcp.ipfinder.vm.TcpDiscoveryVmIpFinder;
 import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
 import org.junit.Test;
 
@@ -44,11 +39,11 @@ import org.junit.Test;
  * Tests that localId for cluster groups will be changed after reconnect.
  */
 public class IgniteClientCheckClusterGroupLocalIdAfterReconnect extends GridCommonAbstractTest {
-    /** Ignite start up mode*/
-    private boolean serverMode;
+    /** Latch timeout*/
+    private final int LATCH_TIMEOUT = 10_000;
 
     /** Object for messaging*/
-    static class External implements Externalizable {
+    private static class External implements Externalizable {
         /** */
         private External() {}
 
@@ -63,60 +58,30 @@ public class IgniteClientCheckClusterGroupLocalIdAfterReconnect extends GridComm
     @Override protected IgniteConfiguration getConfiguration(String igniteInstanceName) throws Exception {
         IgniteConfiguration cfg = super.getConfiguration(igniteInstanceName);
 
-        TcpDiscoverySpi discoverySpi = new TcpDiscoverySpi();
-
-        TcpCommunicationSpi communicationSpi = new TcpCommunicationSpi();
-
-        communicationSpi.setSocketWriteTimeout(20000);
-
-        TcpDiscoveryVmIpFinder ipFinder = new TcpDiscoveryVmIpFinder();
-
-        List<String> ips = new ArrayList<>();
-
-        ips.add("127.0.0.1");
-
-        ipFinder.setAddresses(ips);
-
-        discoverySpi.setIpFinder(ipFinder);
-
-        cfg.setDiscoverySpi(discoverySpi);
-
-        cfg.setCommunicationSpi(communicationSpi);
-
-        cfg.setClientMode(!serverMode);
+        cfg.setFailureDetectionTimeout(5000);
+        cfg.setClientFailureDetectionTimeout(5000);
 
         return cfg;
     }
 
     /** {@inheritDoc} */
-    @Override protected long getTestTimeout() {
-        return 600_000;
-    }
-
-    /** {@inheritDoc} */
     @Override protected void afterTest() throws Exception {
+        super.afterTest();
+
         stopAllGrids();
     }
 
     /**
-     * Test check that local id in cluster group was change and client
+     * Test checks that local id in cluster group was change and client
      * will be able to send the message to itself after reconnect.
      */
     @Test
     public void testClusterGroupLocalIdAfterClientReconnect() throws Exception {
-        serverMode = true;
-
         Ignite server = startGrid(0);
 
-        serverMode = false;
-
-        Ignite client = startGrid(1);
-
-        UUID serverId = server.cluster().node().id();
+        Ignite client = startClientGrid(1);
 
         UUID clientId = client.cluster().node().id();
-
-        assertFalse("Server and client nodes have identical ids", serverId.equals(clientId));
 
         ClusterGroup cg1 = client.cluster().forLocal();
 
@@ -125,8 +90,10 @@ public class IgniteClientCheckClusterGroupLocalIdAfterReconnect extends GridComm
         // check sending messages is possible while connected
         IgniteMessaging messaging = client.message(client.cluster().forLocal());
 
+        CountDownLatch topicSignal = new CountDownLatch(1);
+
         messaging.localListen("topic", (IgniteBiPredicate<UUID, Object>) (uuid, n) -> {
-            System.out.println(n);
+            topicSignal.countDown();
 
             return true;
         });
@@ -143,11 +110,9 @@ public class IgniteClientCheckClusterGroupLocalIdAfterReconnect extends GridComm
 
         server.close();
 
-        assertTrue("client did not disconnect", discSignal.await(20, TimeUnit.SECONDS));
+        assertTrue("client did not disconnect", discSignal.await(LATCH_TIMEOUT, TimeUnit.SECONDS));
 
-        serverMode = true;
-
-        server = startGrid(0);
+        startGrid(0);
 
         // wait for client reconnect
         IgniteFuture future = client.cluster().clientReconnectFuture();
@@ -162,15 +127,13 @@ public class IgniteClientCheckClusterGroupLocalIdAfterReconnect extends GridComm
 
         assertNotNull("Local client ID wasn't changed for local ClusterGroup.", cg2.node(newClientId));
 
-        Thread.sleep(5000);
+        awaitPartitionMapExchange();
 
         // check sending messages is possible after reconnecting
         messaging = client.message(client.cluster().forLocal());
 
         messaging.send("topic", new External());
 
-        client.close();
-
-        server.close();
+        assertTrue("Message wasn't received", topicSignal.await(LATCH_TIMEOUT, TimeUnit.SECONDS));
     }
 }
