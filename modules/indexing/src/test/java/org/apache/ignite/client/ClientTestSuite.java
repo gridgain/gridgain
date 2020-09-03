3d1b585bb2af2284e14e8410b4de9516e19e4804
Pavel Tupitsyn <ptupitsyn@apache.org>
2020-09-03 13:58:35 +0300

    IGNITE-12407 Java thin client: Cluster API support - Fixes #7753.
    Signed-off-by: Aleksey Plekhanov <plehanov.alex@gmail.com>
    
    # Conflicts:
    #	modules/core/src/main/java/org/apache/ignite/internal/client/thin/ProtocolBitmaskFeature.java
    #	modules/core/src/main/java/org/apache/ignite/internal/processors/platform/client/ClientBitmaskFeature.java
    #	modules/core/src/main/java/org/apache/ignite/internal/processors/platform/client/ClientMessageParser.java
    #	modules/indexing/src/test/java/org/apache/ignite/client/ClientTestSuite.java

--- modules/indexing/src/test/java/org/apache/ignite/client/ClientTestSuite.java
+++ modules/indexing/src/test/java/org/apache/ignite/client/ClientTestSuite.java
@@ -16,6 +16,7 @@
 
 package org.apache.ignite.client;
 
+import org.apache.ignite.internal.client.thin.ClusterApiTest;
 import org.apache.ignite.internal.client.thin.*;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -40,6 +41,7 @@ import org.junit.runners.Suite;
     ConnectToStartingNodeTest.class,
     AsyncChannelTest.class,
     ComputeTaskTest.class,
+    ClusterApiTest.class,
     ThinClientTxMissingBackupsFailover.class,
     ThinClientAffinityAwarenessConnectionTest.class,
     ThinClientAffinityAwarenessStableTopologyTest.class,
