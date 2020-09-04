031c6aeade6d053e276270401ba361296fbb79ec
sergeyuttsel <uttsel@gmail.com>
2020-09-04 14:57:53 +0300

    GG-29908 WIP

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/IgniteTopologyValidatorGridSplitCacheTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/IgniteTopologyValidatorGridSplitCacheTest.java
@@ -63,7 +63,7 @@ public class IgniteTopologyValidatorGridSplitCacheTest extends IgniteCacheTopolo
     private static final String ACTIVATOR_NODE_ATTR = "split.resolved";
 
     /** */
-    private static final int GRID_CNT = 8;//GridTestUtils.SF.applyLB(28, 16);
+    private static final int GRID_CNT = 12;//GridTestUtils.SF.applyLB(28, 16);
 
     /** */
     private static final int CACHES_CNT = GridTestUtils.SF.applyLB(50, 20);
