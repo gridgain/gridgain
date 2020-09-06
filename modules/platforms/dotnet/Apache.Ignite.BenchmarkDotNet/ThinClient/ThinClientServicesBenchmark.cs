b455c7d07a4356e2d0fb9549ec075333f5dbb5d2
Pavel Tupitsyn <ptupitsyn@apache.org>
2020-09-06 22:06:01 +0300

    Add a note about DIGNITE_EVENT_DRIVEN_SERVICE_PROCESSOR_ENABLED

--- modules/platforms/dotnet/Apache.Ignite.BenchmarkDotNet/ThinClient/ThinClientServicesBenchmark.cs
+++ modules/platforms/dotnet/Apache.Ignite.BenchmarkDotNet/ThinClient/ThinClientServicesBenchmark.cs
@@ -25,7 +25,10 @@ namespace Apache.Ignite.BenchmarkDotNet.ThinClient
     /// <summary>
     /// Thin client services benchmark.
     /// <para />
-    /// .NET Core 3.1.7
+    /// NOTE: IGNITE_EVENT_DRIVEN_SERVICE_PROCESSOR_ENABLED settings affects the performance dramatically,
+    /// it is enabled by default in Ignite, and disabled by default in GridGain.
+    /// <para />
+    /// .NET Core 3.1.7, IGNITE_EVENT_DRIVEN_SERVICE_PROCESSOR_ENABLED=true
     /// |         Method |      Mean |     Error |    StdDev |    Median |  Gen 0 | Gen 1 | Gen 2 | Allocated |
     /// |--------------- |----------:|----------:|----------:|----------:|-------:|------:|------:|----------:|
     /// |  IntMethodThin |  32.10 us |  0.640 us |  1.496 us |  31.86 us | 0.3052 |     - |     - |   1.85 KB |
