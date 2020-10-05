e6fbac0fc16efd880bc46ffa4d33098116819f72
mstepachev <maksim.stepachev@gmail.com>
2020-10-05 15:02:08 +0300

    master merge

--- modules/core/src/main/java/org/apache/ignite/spi/encryption/keystore/KeystoreEncryptionSpi.java
+++ modules/core/src/main/java/org/apache/ignite/spi/encryption/keystore/KeystoreEncryptionSpi.java
@@ -16,14 +16,16 @@
 
 package org.apache.ignite.spi.encryption.keystore;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Serializable;
+import java.net.URL;
 import java.nio.ByteBuffer;
 import java.security.GeneralSecurityException;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
-import java.security.Key;
 import java.security.Key;
 import java.security.KeyStore;
 import java.security.MessageDigest;
@@ -440,6 +442,25 @@ public class KeystoreEncryptionSpi extends IgniteSpiAdapter implements Encryptio
         assert !started() : "Spi already started";
 
         this.keySize = keySize;
+    }
+
+    /**
+     * {@code keyStorePath} could be absolute path or path to classpath resource.
+     *
+     * @return File for {@code keyStorePath}.
+     */
+    private InputStream keyStoreFile() throws IOException {
+        File abs = new File(keyStorePath);
+
+        if (abs.exists())
+            return new FileInputStream(abs);
+
+        URL clsPthRes = KeystoreEncryptionSpi.class.getClassLoader().getResource(keyStorePath);
+
+        if (clsPthRes != null)
+            return clsPthRes.openStream();
+
+        return null;
     }
 
     /**
