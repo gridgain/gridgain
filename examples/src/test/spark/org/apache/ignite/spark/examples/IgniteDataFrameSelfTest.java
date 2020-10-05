e497f5753d93b043ab0fe67c0e9132e58077559c
Alexander Belyak <abelyak@gridgain.com>
2020-10-05 13:49:39 +0700

    GG-18652 revert unnecessary changes

--- examples/src/test/spark/org/apache/ignite/spark/examples/IgniteDataFrameSelfTest.java
+++ examples/src/test/spark/org/apache/ignite/spark/examples/IgniteDataFrameSelfTest.java
@@ -16,6 +16,9 @@
 
 package org.apache.ignite.spark.examples;
 
+import org.apache.ignite.examples.spark.IgniteCatalogExample;
+import org.apache.ignite.examples.spark.IgniteDataFrameExample;
+import org.apache.ignite.examples.spark.IgniteDataFrameWriteExample;
 import org.apache.ignite.testframework.junits.common.GridAbstractExamplesTest;
 import org.junit.Test;
 
@@ -24,4 +27,27 @@ import org.junit.Test;
 public class IgniteDataFrameSelfTest extends GridAbstractExamplesTest {
     static final String[] EMPTY_ARGS = new String[0];
 
+    /**
+     * @throws Exception If failed.
+     */
+    @Test
+    public void testCatalogExample() throws Exception {
+        IgniteCatalogExample.main(EMPTY_ARGS);
+    }
+
+    /**
+     * @throws Exception If failed.
+     */
+    @Test
+    public void testDataFrameExample() throws Exception {
+        IgniteDataFrameExample.main(EMPTY_ARGS);
+    }
+
+    /**
+     * @throws Exception If failed.
+     */
+    @Test
+    public void testDataFrameWriteExample() throws Exception {
+        IgniteDataFrameWriteExample.main(EMPTY_ARGS);
+    }
 }
