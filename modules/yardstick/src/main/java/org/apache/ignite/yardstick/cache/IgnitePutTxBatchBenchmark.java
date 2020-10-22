958cb5959c17dd40c55a1f62966c9fa311766aa3
Ivan Artukhov <iartukhov@gridgain.com>
2020-10-22 11:19:46 +0300

    QA-3020 Add IgnitePutTxBatchBenchmark

--- modules/yardstick/src/main/java/org/apache/ignite/yardstick/cache/IgnitePutTxBatchBenchmark.java
+++ modules/yardstick/src/main/java/org/apache/ignite/yardstick/cache/IgnitePutTxBatchBenchmark.java
@@ -16,56 +16,36 @@
 
 package org.apache.ignite.yardstick.cache;
 
-import java.util.Map;
-import java.util.concurrent.Callable;
 import org.apache.ignite.IgniteCache;
-import org.apache.ignite.IgniteSystemProperties;
-import org.apache.ignite.IgniteTransactions;
-import org.apache.ignite.yardstick.IgniteBenchmarkUtils;
 import org.apache.ignite.yardstick.cache.model.SampleValue;
 import org.yardstickframework.BenchmarkConfiguration;
 
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.concurrent.Callable;
+
 /**
- * Ignite benchmark that performs transactional put operations.
+ * Ignite benchmark that performs transactional put operations with several operations per transaction.
  */
-public class IgnitePutTxBenchmark extends IgniteCacheAbstractBenchmark<Integer, Object> {
-    /** */
-    protected IgniteTransactions txs;
+public class IgnitePutTxBatchBenchmark extends IgnitePutTxBenchmark {
 
-    /** */
-    protected Callable<Void> clo;
-
-    /** {@inheritDoc} */
     @Override public void setUp(BenchmarkConfiguration cfg) throws Exception {
         super.setUp(cfg);
 
-        if (!IgniteSystemProperties.getBoolean("SKIP_MAP_CHECK"))
-            ignite().compute().broadcast(new WaitMapExchangeFinishCallable());
+        Set<Integer> keys = new TreeSet<>();
 
-        txs = ignite().transactions();
+        while (keys.size() < args.batch())
+            keys.add(nextRandom(args.range()));
 
         clo = new Callable<Void>() {
             @Override public Void call() throws Exception {
                 IgniteCache<Integer, Object> cache = cacheForOperation();
 
-                int key = nextRandom(args.range());
-
-                cache.put(key, new SampleValue(key));
+                for (Integer key : keys)
+                    cache.put(key, new SampleValue(key));
 
                 return null;
             }
         };
-    }
-
-    /** {@inheritDoc} */
-    @Override public boolean test(Map<Object, Object> ctx) throws Exception {
-        IgniteBenchmarkUtils.doInTransaction(txs, args.txConcurrency(), args.txIsolation(), clo);
-
-        return true;
-    }
-
-    /** {@inheritDoc} */
-    @Override protected IgniteCache<Integer, Object> cache() {
-        return ignite().cache("tx");
     }
 }
