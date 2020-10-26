511bf76a29f90a984f3d2ba4e14c1033ad0ffb81
Pavel Tupitsyn <ptupitsyn@apache.org>
2020-10-26 12:48:39 +0300

    Fix merge

--- modules/core/src/main/java/org/apache/ignite/internal/processors/query/GridQueryProcessor.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/query/GridQueryProcessor.java
@@ -1210,7 +1210,7 @@ public class GridQueryProcessor extends GridProcessorAdapter {
         if (cacheObjProc instanceof CacheObjectBinaryProcessorImpl) {
             ((CacheObjectBinaryProcessorImpl)cacheObjProc)
                 .binaryContext()
-                .registerClass(cls, false, false, true);
+                .registerClass(cls, true, false, true);
         }
     }
 
