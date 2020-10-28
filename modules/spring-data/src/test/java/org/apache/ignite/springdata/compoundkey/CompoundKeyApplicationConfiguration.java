0c818e9341f8be855c0c11aeb56e6ee0579622dc
Ilya Kazakov <kazakov.ilya@gmail.com>
2020-10-28 09:27:34 +0800

    GG-31196 fix style

--- modules/spring-data/src/test/java/org/apache/ignite/springdata/compoundkey/CompoundKeyApplicationConfiguration.java
+++ modules/spring-data/src/test/java/org/apache/ignite/springdata/compoundkey/CompoundKeyApplicationConfiguration.java
@@ -19,13 +19,8 @@ package org.apache.ignite.springdata.compoundkey;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.Ignition;
 import org.apache.ignite.springdata.repository.config.EnableIgniteRepositories;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
 
 /**
  * Spring application configuration
@@ -39,44 +34,5 @@ public class CompoundKeyApplicationConfiguration {
     @Bean
     public Ignite igniteInstance() {
         return Ignition.start("/home/kazakov/Documents/apache/ignite/examples/config/example-ignite.xml");
-    }
-
-    public static void main(String[] args) throws Exception {
-        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
-        ctx.register(CompoundKeyApplicationConfiguration.class);
-        ctx.refresh();
-
-        Ignite ignite =  ctx.getBean(Ignite.class);
-
-        System.out.println(ignite.cacheNames());
-
-        ignite.destroyCache("City");
-
-        System.out.println(ignite.cacheNames());
-
-        try (Connection conn = DriverManager.getConnection("jdbc:ignite:thin://127.0.0.1/")) {
-            Statement st = conn.createStatement();
-
-            st.execute("DROP TABLE IF EXISTS City");
-            st.execute("CREATE TABLE City (ID INT, Name VARCHAR, CountryCode CHAR(3), District VARCHAR, Population INT, PRIMARY KEY (ID, CountryCode)) WITH \"template=partitioned, backups=1, affinityKey=CountryCode, CACHE_NAME=City, KEY_TYPE=org.apache.ignite.springdata.compoundkey.CityKey, VALUE_TYPE=org.apache.ignite.springdata.compoundkey.City\"");
-            st.execute("SET STREAMING ON;");
-            st.execute("INSERT INTO City(ID, Name, CountryCode, District, Population) VALUES (1,'Kabul','AFG','Kabol',1780000)");
-            st.execute("INSERT INTO City(ID, Name, CountryCode, District, Population) VALUES (2,'Qandahar','AFG','Qandahar',237500)");
-            st.execute("INSERT INTO City(ID, Name, CountryCode, District, Population) VALUES (3,'Herat','AFG','Herat',186800)");
-            st.execute("INSERT INTO City(ID, Name, CountryCode, District, Population) VALUES (4,'Mazar-e-Sharif','AFG','Balkh',127800)");
-            st.execute("INSERT INTO City(ID, Name, CountryCode, District, Population) VALUES (5,'Amsterdam','NLD','Noord-Holland',731200)");
-        }
-
-        System.out.println(ignite.cacheNames());
-
-
-
-        CityRepository repo = ctx.getBean(CityRepository.class);
-        System.out.println(repo.findByCountryCode("AFG"));
-
-
-        //CityRepository rep = ctx.getBean(CityRepository.class);
-        //System.out.println(rep.findById(1));
-
     }
 }
