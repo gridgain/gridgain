e61b1fbca13b2aba0630a62c6d331200b045ffe9
Pavel Tupitsyn <ptupitsyn@apache.org>
2020-11-05 21:38:47 +0300

    GG-31141 [IGNITE-13160] .NET: Register binary metadata during cache start
    * Register binary metadata for QueryEntity key and value .NET types on cache start (same way as we do for Java types - IGNITE-5795, 3bb03444246f863096063d084393676a84d2bc0e)
    * Move `registerMetadataForRegisteredCaches` call to `onCacheKernalStart` so that `PlatformProcessor` is available
    
    This builds on the fix from IGNITE-5795 by adding a platform callback to get metadata for .NET types. It has the same limitations as the original fix: does not work when classes are not present on the server node, in particular, for thin clients. IGNITE-13607 filed to deal with that separately.

--- modules/core/src/main/java/org/apache/ignite/internal/binary/builder/BinaryObjectBuilderImpl.java
+++ modules/core/src/main/java/org/apache/ignite/internal/binary/builder/BinaryObjectBuilderImpl.java
@@ -45,6 +45,8 @@ import org.apache.ignite.lang.IgniteBiTuple;
 import org.apache.ignite.thread.IgniteThread;
 import org.jetbrains.annotations.Nullable;
 
+import static org.apache.ignite.internal.MarshallerPlatformIds.JAVA_ID;
+
 /**
  *
  */
@@ -359,7 +361,7 @@ public class BinaryObjectBuilderImpl implements BinaryObjectBuilder {
                 if (affFieldName0 == null)
                     affFieldName0 = ctx.affinityKeyFieldName(typeId);
 
-                ctx.registerUserClassName(typeId, typeName, writer.failIfUnregistered(), false);
+                ctx.registerUserClassName(typeId, typeName, writer.failIfUnregistered(), false, JAVA_ID);
 
                 ctx.updateMetadata(typeId, new BinaryMetadata(typeId, typeName, fieldsMeta, affFieldName0,
                     Collections.singleton(curSchema), false, null), writer.failIfUnregistered());
