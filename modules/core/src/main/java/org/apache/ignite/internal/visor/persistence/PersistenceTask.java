7cb827541ca496df2f1440c1548832e33af6702c
Sergey Chugunov <sergey.chugunov@gmail.com>
2020-11-17 16:38:04 +0300

    GG-31545 Compilation error fixed

--- modules/core/src/main/java/org/apache/ignite/internal/visor/persistence/PersistenceTask.java
+++ modules/core/src/main/java/org/apache/ignite/internal/visor/persistence/PersistenceTask.java
@@ -250,15 +250,17 @@ public class PersistenceTask extends VisorOneNodeTask<PersistenceTaskArg, Persis
             if (!failedToCleanCaches.isEmpty())
                 res.failedCaches(failedToCleanCaches);
 
-            List<MaintenanceAction> actions = mntcReg.actionsForMaintenanceTask(CORRUPTED_DATA_FILES_MNTC_TASK_NAME);
+            List<MaintenanceAction<?>> actions = mntcReg.actionsForMaintenanceTask(CORRUPTED_DATA_FILES_MNTC_TASK_NAME);
 
-            Optional<MaintenanceAction> checkActionOpt = actions.stream().filter(a -> a.name().equals(CheckCorruptedCacheStoresCleanAction.ACTION_NAME))
+            Optional<MaintenanceAction<?>> checkActionOpt = actions
+                .stream()
+                .filter(a -> a.name().equals(CheckCorruptedCacheStoresCleanAction.ACTION_NAME))
                 .findFirst();
 
             if (checkActionOpt.isPresent()) {
-                MaintenanceAction<Boolean> action = checkActionOpt.get();
+                MaintenanceAction<?> action = checkActionOpt.get();
 
-                Boolean mntcTaskCompleted = action.execute();
+                Boolean mntcTaskCompleted = (Boolean) action.execute();
 
                 res.maintenanceTaskCompleted(mntcTaskCompleted);
 
@@ -297,10 +299,10 @@ public class PersistenceTask extends VisorOneNodeTask<PersistenceTaskArg, Persis
         private PersistenceTaskResult cleanCorrupted(MaintenanceRegistry mntcReg) {
             PersistenceTaskResult res = new PersistenceTaskResult(true);
 
-            List<MaintenanceAction> actions = mntcReg
+            List<MaintenanceAction<?>> actions = mntcReg
                 .actionsForMaintenanceTask(CORRUPTED_DATA_FILES_MNTC_TASK_NAME);
 
-            Optional<MaintenanceAction> cleanCorruptedActionOpt = actions
+            Optional<MaintenanceAction<?>> cleanCorruptedActionOpt = actions
                 .stream()
                 .filter(a -> a.name().equals(CleanCacheStoresMaintenanceAction.ACTION_NAME))
                 .findFirst();
