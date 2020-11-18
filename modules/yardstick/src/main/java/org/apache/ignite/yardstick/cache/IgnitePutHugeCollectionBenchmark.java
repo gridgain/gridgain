53496acebee4b913c84e763f4c6402e43a94ef7d
Ivan Artukhov <iartukhov@gridgain.com>
2020-11-18 11:23:43 +0300

    QA-3069 Add IgnitePutHugeCollectionBenchmark

---
+++ modules/yardstick/src/main/java/org/apache/ignite/yardstick/cache/IgnitePutHugeCollectionBenchmark.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2019 GridGain Systems, Inc. and Contributors.
+ *
+ * Licensed under the GridGain Community Edition License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.gridgain.com/products/software/community-edition/gridgain-community-edition-license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.ignite.yardstick.cache;
+
+import org.apache.ignite.IgniteCache;
+import org.apache.ignite.yardstick.cache.model.AllTypesHugeCollection;
+
+import java.util.Map;
+
+/**
+ * Ignite benchmark that performs put operations for entity with large fields.
+ */
+public class IgnitePutHugeCollectionBenchmark extends IgniteCacheAbstractBenchmark<Integer, Object> {
+    /** {@inheritDoc} */
+    @Override public boolean test(Map<Object, Object> ctx) throws Exception {
+        IgniteCache<Integer, Object> cache = cacheForOperation();
+
+        int key = nextRandom(args.range());
+
+        cache.put(key, new AllTypesHugeCollection((long) key));
+
+        return true;
+    }
+
+    /** {@inheritDoc} */
+    @Override protected IgniteCache<Integer, Object> cache() {
+        return ignite().cache("atomic");
+    }
+}
