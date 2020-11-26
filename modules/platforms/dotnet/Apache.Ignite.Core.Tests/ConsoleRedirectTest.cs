c52b7ffab22ad7a430f8418752603a35a9562149
antkr <antkr.dev@gmail.com>
2020-11-26 17:00:58 +0300

    GG-31179 test fix.

--- modules/platforms/dotnet/Apache.Ignite.Core.Tests/ConsoleRedirectTest.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests/ConsoleRedirectTest.cs
@@ -82,8 +82,10 @@ namespace Apache.Ignite.Core.Tests
                 Logger = null
             };
             
-            using (Ignition.Start(cfg))
+            using (var _grid = Ignition.Start(cfg))
             {
+                Assert.IsTrue(_grid.WaitTopology(1));
+
                 Assert.AreEqual(1, Regex.Matches(_outSb.ToString(), "ver=1, locNode=[a-fA-F0-9]{8,8}, servers=1, clients=0,").Count);
             }
         }
