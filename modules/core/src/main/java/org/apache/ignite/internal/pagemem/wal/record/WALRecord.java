c4b80dcabdeea51dd377f1adedc2db395a0756a3
Alexey Scherbakov <alexey.scherbakoff@gmail.com>
2020-11-30 15:40:15 +0300

    GG-24620 Removed unused record type.

--- modules/core/src/main/java/org/apache/ignite/internal/pagemem/wal/record/WALRecord.java
+++ modules/core/src/main/java/org/apache/ignite/internal/pagemem/wal/record/WALRecord.java
@@ -249,10 +249,7 @@ public abstract class WALRecord {
         PARTITION_META_PAGE_DELTA_RECORD_V3(67, PHYSICAL),
 
         /** Index meta page delta record includes encryption status data. */
-        INDEX_META_PAGE_DELTA_RECORD(68, PHYSICAL),
-
-        /** Partition meta page containing tombstone presence flag. */
-        PARTITION_META_PAGE_UPDATE_COUNTERS_V3(69, PHYSICAL);
+        INDEX_META_PAGE_DELTA_RECORD(68, PHYSICAL);
 
         /** Index for serialization. Should be consistent throughout all versions. */
         private final int idx;
