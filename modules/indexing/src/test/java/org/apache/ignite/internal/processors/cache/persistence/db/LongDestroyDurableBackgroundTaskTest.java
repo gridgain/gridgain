d05f615b0eb588740ea32d736ab2cc33f3fd3f80
denis-chudov <moonglloom@gmail.com>
2020-12-17 20:17:10 +0300

    GG-31933 GG-31939 Remove FailureHandler call from DurableBackgroundTasksProcessor#asyncDurableBackgroundTaskExecute, clear irrelevant tasks
    (cherry picked from commit c658e86b5db53dd5d662a15b2c500fd3d9fb2378)

--- modules/indexing/src/test/java/org/apache/ignite/internal/processors/cache/persistence/db/LongDestroyDurableBackgroundTaskTest.java
+++ modules/indexing/src/test/java/org/apache/ignite/internal/processors/cache/persistence/db/LongDestroyDurableBackgroundTaskTest.java
@@ -642,6 +642,36 @@ public class LongDestroyDurableBackgroundTaskTest extends GridCommonAbstractTest
     }
 
     /**
+     * Check that index deletion tasks are executed successfully when corresponding cache group is deleted.
+     *
+     * @throws Exception If failed.
+     */
+    @Test
+    public void testIrrelevantTasksAreCleared() throws Exception {
+        taskLifecycleListener.reset();
+
+        IgniteEx ignite = prepareAndPopulateCluster(1, false, false);
+
+        IgniteCache<Integer, Integer> cache = ignite.cache(DEFAULT_CACHE_NAME);
+
+        // Drop table faster than index deletion task completes.
+        query(cache, "drop table t");
+
+        awaitPartitionMapExchange();
+
+        stopAllGrids();
+
+        assertTrue(pendingDelLatch.getCount() > 0);
+
+        startGrid(RESTARTED_NODE_NUM);
+
+        awaitPartitionMapExchange();
+
+        // Task should complete without errors despite the cache has been deleted.
+        awaitLatch(pendingDelLatch, "Test timed out: failed to await for durable background task completion.");
+    }
+
+    /**
      * Tests that index is correctly deleted when corresponding SQL table is deleted.
      *
      * @throws Exception If failed.
