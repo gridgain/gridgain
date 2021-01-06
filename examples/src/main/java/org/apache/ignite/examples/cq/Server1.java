ad8642dbeb6e4fc49bf3935a55ba8ca28e5bac01
Ilya Kazakov <kazakov.ilya@gmail.com>
2021-01-06 13:59:58 +0800

    GG-32292 CQ SystemView shows full data for each cq

--- examples/src/main/java/org/apache/ignite/examples/cq/Server1.java
+++
@@ -1,9 +0,0 @@
-package org.apache.ignite.examples.cq;
-
-import org.apache.ignite.Ignition;
-
-public class Server1 {
-    public static void main(String[] args) {
-        Ignition.start();
-    }
-}
