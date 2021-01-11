d2c12db4b0d1c600fa19c02860daefe2c4cc943b
Pavel Tupitsyn <ptupitsyn@apache.org>
2021-01-11 20:37:18 +0300

    GG-32218 [IGNITE-13734] .NET: Register service return type on method invocation
    (cherry picked from commit 1979daaec9ac0fd957ea6c31561abbd0e6ed11fc)

--- modules/core/src/test/java/org/apache/ignite/platform/model/ACL.java
+++ modules/core/src/test/java/org/apache/ignite/platform/model/ACL.java
@@ -14,37 +14,9 @@
  * limitations under the License.
  */
 
-package org.apache.ignite.platform;
+package org.apache.ignite.platform.model;
 
-import java.util.Objects;
-
-/** Test key object. */
-public class Key {
-    /** */
-    private long id;
-
-    /** */
-    public Key(long id) {
-        this.id = id;
-    }
-
-    /** */
-    public long getId() {
-        return id;
-    }
-
-    /** {@inheritDoc} */
-    @Override public boolean equals(Object o) {
-        if (this == o)
-            return true;
-        if (o == null || getClass() != o.getClass())
-            return false;
-        Key key = (Key)o;
-        return id == key.id;
-    }
-
-    /** {@inheritDoc} */
-    @Override public int hashCode() {
-        return Objects.hash(id);
-    }
+/** Test enum. */
+public enum ACL {
+    ALLOW, DENY
 }
