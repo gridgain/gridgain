b249b82b75ce1e8e494d9358199093b2336ff7ee
Pavel Tupitsyn <ptupitsyn@apache.org>
2021-01-25 20:19:07 +0300

    Fix licenses

--- modules/platforms/dotnet/Apache.Ignite.Core.Tests/Examples/ExamplePaths.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests/Examples/ExamplePaths.cs
@@ -1,12 +1,11 @@
 ﻿/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ * Copyright 2019 GridGain Systems, Inc. and Contributors.
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the GridGain Community Edition License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.gridgain.com/products/software/community-edition/gridgain-community-edition-license
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -27,7 +26,7 @@ namespace Apache.Ignite.Core.Tests.Examples
     {
         /** */
         public const string SharedProjFileName = "Shared.csproj";
-        
+
         /** */
         public static readonly string SourcesPath =
             Path.Combine(Impl.Common.IgniteHome.Resolve(), "modules", "platforms", "dotnet", "examples");
@@ -52,7 +51,7 @@ namespace Apache.Ignite.Core.Tests.Examples
             var targetFw = GetTargetFramework(projFile);
             var name = Path.GetFileNameWithoutExtension(projFile);
             var path = Path.GetDirectoryName(projFile);
-            
+
             return Path.Combine(path, "bin", "Debug", targetFw, $"{name}.dll");
         }
 
