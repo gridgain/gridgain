281ea8636466edfd36a1d76c2c77a960bfc3e653
GridGain Teamcity <ggtc@gridgain.com>
2021-02-18 16:44:58 +0300

    8.7.34 release

--- modules/platforms/dotnet/Apache.Ignite.Core.Tests.TestDll2/Properties/AssemblyInfo.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests.TestDll2/Properties/AssemblyInfo.cs
@@ -36,6 +36,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.11.0.51807")]
-[assembly: AssemblyFileVersion("2.11.0.51807")]
-[assembly: AssemblyInformationalVersion("2.11.0")]
+[assembly: AssemblyVersion("8.7.34.53033")]
+[assembly: AssemblyFileVersion("8.7.34.53033")]
+[assembly: AssemblyInformationalVersion("8.7.34")]
