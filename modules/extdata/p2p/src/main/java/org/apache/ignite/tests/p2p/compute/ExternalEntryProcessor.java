660a5d795e888ebbfa2f69a0ff84d52456ceeda1
Ilya Kasnacheev <ilya.kasnacheev@gmail.com>
2021-04-02 18:30:16 +0300

    GG-32500 Test for scenario where cache.invoke() triggers failure handler.

---
+++ modules/extdata/p2p/src/main/java/org/apache/ignite/tests/p2p/compute/ExternalEntryProcessor.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2019 GridGain Systems, Inc. and Contributors.
+ *
+ * Licensed under the GridGain Community Edition License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.gridgain.com/products/software/community-edition/gridgain-community-edition-license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.ignite.tests.p2p.compute;
+
+import javax.cache.processor.EntryProcessor;
+import javax.cache.processor.EntryProcessorException;
+import javax.cache.processor.MutableEntry;
+import org.apache.ignite.Ignite;
+import org.apache.ignite.IgniteLogger;
+import org.apache.ignite.resources.IgniteInstanceResource;
+import org.apache.ignite.resources.LoggerResource;
+
+/** */
+public class ExternalEntryProcessor implements EntryProcessor<Object, Object, Object> {
+    /** */
+    @IgniteInstanceResource
+    Ignite ignite;
+
+    /** Logger. */
+    @LoggerResource
+    private IgniteLogger log;
+
+    /** */
+    @Override
+    public Object process(MutableEntry<Object, Object> entry, Object... arguments) throws EntryProcessorException {
+        log.info("!!!!! I am entry processor " + entry.getKey() + " on " + ignite.name());
+
+        return 42;
+    }
+}
