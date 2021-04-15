f9a1d96b692bf243252d98153bb10038b1c729c0
Vladislav Pyatkov <vldpyatkov@gmail.com>
2021-04-15 21:45:55 +0300

    WIP

--- modules/core/src/test/java/org/apache/ignite/spi/communication/tcp/SpyTcpCommunicationSpi.java
+++ modules/core/src/test/java/org/apache/ignite/spi/communication/tcp/SpyTcpCommunicationSpi.java
@@ -2,6 +2,9 @@ package org.apache.ignite.spi.communication.tcp;
 
 import org.apache.ignite.IgniteException;
 import org.apache.ignite.cluster.ClusterNode;
+import org.apache.ignite.internal.GridJobCancelRequest;
+import org.apache.ignite.internal.managers.communication.GridIoMessage;
+import org.apache.ignite.internal.util.typedef.internal.U;
 import org.apache.ignite.lang.IgniteInClosure;
 import org.apache.ignite.plugin.extensions.communication.Message;
 import org.apache.ignite.spi.IgniteSpiException;
@@ -11,7 +14,13 @@ public class SpyTcpCommunicationSpi extends TcpCommunicationSpi {
     @Override public void sendMessage(ClusterNode node, Message msg, IgniteInClosure<IgniteException> ackC)
         throws IgniteSpiException {
 
-        log.info("Send message to node [node=" + node + ", " + "msg=" + msg + ']');
+        if (msg instanceof GridIoMessage) {
+//            log.info("Send message to node [node=" + node + ", " + "msg=" + ((GridIoMessage)msg).message() + ']');
+
+            if (((GridIoMessage)msg).message() instanceof GridJobCancelRequest)
+                U.dumpStack(log, "GridJobCancelRequest caught [node=" + node + ", " +
+                    "msg=" + ((GridIoMessage)msg).message() + ']');
+        }
 
         super.sendMessage(node, msg, ackC);
     }
