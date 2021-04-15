9eca692d0617468401a829fc66c024fe66785e98
Pavel Tupitsyn <ptupitsyn@apache.org>
2021-04-15 21:17:39 +0300

    Update modules/core/src/main/java/org/apache/ignite/client/ClientCache.java
    Co-authored-by: Igor Sapego <isapego@apache.org>

--- modules/core/src/main/java/org/apache/ignite/client/ClientCache.java
+++ modules/core/src/main/java/org/apache/ignite/client/ClientCache.java
@@ -757,8 +757,8 @@ public interface ClientCache<K, V> {
      * notified about client disconnected event via {@link ClientDisconnectListener} interface if you need it.
      *
      * @param cacheEntryListenerConfiguration a factory and related configuration for creating the listener.
-     * @throws IllegalArgumentException is the same CacheEntryListenerConfiguration is used more than once or
-     *          if some unsupported by thin client properties are set.
+     * @throws IllegalArgumentException if the same CacheEntryListenerConfiguration is used more than once or
+     *          if some properties are unsupported by thin client.
      * @see CacheEntryListener
      */
     public void registerCacheEntryListener(CacheEntryListenerConfiguration<K, V> cacheEntryListenerConfiguration);
