17d61305c2c1652790dd34670a67bd5509c8c8f7
tledkov <tledkov@gridgain.com>
2021-04-21 10:22:46 +0300

    GG-33093 Revert "GG-27961 hide sensitive data on log messages for long running query and huge results query according with the property IGNITE_TO_STRING_INCLUDE_SENSITIVE"

--- modules/indexing/src/test/java/org/apache/ignite/testsuites/IgniteBinaryCacheQueryTestSuite2.java
+++ modules/indexing/src/test/java/org/apache/ignite/testsuites/IgniteBinaryCacheQueryTestSuite2.java
@@ -61,7 +61,6 @@ import org.apache.ignite.internal.processors.database.baseline.IgniteStableBasel
 import org.apache.ignite.internal.processors.query.BasicSqlTest;
 import org.apache.ignite.internal.processors.query.CreateIndexOnInvalidDataTypeTest;
 import org.apache.ignite.internal.processors.query.DisabledSqlFunctionsTest;
-import org.apache.ignite.internal.processors.query.SensitiveInformationForLongRunningQueryTest;
 import org.apache.ignite.internal.processors.query.oom.MemoryTrackerOnReducerTest;
 import org.apache.ignite.internal.processors.query.timeout.DefaultQueryTimeoutTestSuite;
 import org.apache.ignite.internal.processors.query.DmlBatchSizeDeadlockTest;
@@ -223,7 +222,6 @@ import org.junit.runners.Suite;
     LocalQueryLazyTest.class,
 
     LongRunningQueryTest.class,
-    SensitiveInformationForLongRunningQueryTest.class,
 
     SqlStatisticsUserQueriesFastTest.class,
     SqlStatisticsUserQueriesLongTest.class,
