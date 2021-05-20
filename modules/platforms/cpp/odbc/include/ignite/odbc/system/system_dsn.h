117e925ded94c86301bd47ac3b2b97084d0d93de
Igor Sapego <isapego@apache.org>
2021-05-20 14:11:34 +0300

    GG-33150 Correctly handle windows in ODBC on Windows (#1938)

---
+++ modules/platforms/cpp/odbc/include/ignite/odbc/system/system_dsn.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2021 GridGain Systems, Inc. and Contributors.
+ *
+ * Licensed under the GridGain Community Edition License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.gridgain.com/products/software/community-edition/gridgain-community-edition-license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef _IGNITE_ODBC_SYSTEM_SYSTEM_DSN
+#define _IGNITE_ODBC_SYSTEM_SYSTEM_DSN
+
+#include <ignite/odbc/system/odbc_constants.h>
+
+namespace ignite
+{
+    namespace odbc
+    {
+        namespace config
+        {
+            class Configuration;
+        }
+    }
+}
+
+/**
+ * Display connection window for user to configure connection parameters.
+ *
+ * @param windowParent Parent window handle.
+ * @param config Output configuration.
+ * @return True on success and false on fail.
+ */
+bool DisplayConnectionWindow(void* windowParent, ignite::odbc::config::Configuration& config);
+
+#endif //_IGNITE_ODBC_SYSTEM_SYSTEM_DSN
