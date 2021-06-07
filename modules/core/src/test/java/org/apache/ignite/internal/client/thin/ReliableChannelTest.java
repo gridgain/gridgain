733cf5c0c02dd3730e9ba716aa41b60b11ebfd42
Pavel Tupitsyn <ptupitsyn@apache.org>
2021-06-07 21:05:10 +0300

    GG-33288 [IGNITE-14719] Thin clients: enable partition awareness by default (Java, .NET, C++)

--- modules/core/src/test/java/org/apache/ignite/internal/client/thin/ReliableChannelTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/client/thin/ReliableChannelTest.java
@@ -172,7 +172,9 @@ public class ReliableChannelTest {
      */
     @Test
     public void testNodeChannelsAreNotCleaned() {
-        ClientConfiguration ccfg = new ClientConfiguration().setAddresses(dfltAddrs);
+        ClientConfiguration ccfg = new ClientConfiguration()
+                .setAddresses(dfltAddrs)
+                .setPartitionAwarenessEnabled(false);
 
         ReliableChannel rc = new ReliableChannel(chFactory, ccfg, null);
 
@@ -235,7 +237,9 @@ public class ReliableChannelTest {
             .nextAddresesResponse(dfltAddrs)
             .nextAddresesResponse("127.0.0.1:10803", "127.0.0.1:10804");
 
-        ClientConfiguration ccfg = new ClientConfiguration().setAddressesFinder(finder);
+        ClientConfiguration ccfg = new ClientConfiguration()
+                .setAddressesFinder(finder)
+                .setPartitionAwarenessEnabled(false);
 
         ReliableChannel rc = new ReliableChannel(chFactory, ccfg, null);
 
