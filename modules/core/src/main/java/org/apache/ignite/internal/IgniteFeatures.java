7a77c9380e9274c7fe5aefb3c69097df7eceff9e
vmalin <vmalinovskiy@gridgain.com>
2021-08-22 22:34:54 +0300

    Revert "GG-33493 Security context is created on every operation from the thin client"
    This reverts commit 962bd2486515405db0a9b206557c9ed059e52b1c.

--- modules/core/src/main/java/org/apache/ignite/internal/IgniteFeatures.java
+++ modules/core/src/main/java/org/apache/ignite/internal/IgniteFeatures.java
@@ -27,7 +27,6 @@ import org.apache.ignite.internal.processors.ru.RollingUpgradeStatus;
 import org.apache.ignite.internal.processors.schedule.IgniteNoopScheduleProcessor;
 import org.apache.ignite.internal.util.typedef.F;
 import org.apache.ignite.lang.IgnitePredicate;
-import org.apache.ignite.plugin.security.SecuritySubject;
 import org.apache.ignite.spi.communication.tcp.TcpCommunicationSpi;
 import org.apache.ignite.spi.communication.tcp.messages.HandshakeWaitMessage;
 import org.apache.ignite.spi.discovery.DiscoverySpi;
@@ -224,10 +223,7 @@ public enum IgniteFeatures {
     SNAPSHOT_OPERATIONS_CHAINING(59),
 
     /** Previous snapshot SFTP upload had a race condition, so it must be disabled in mixed-cluster with older versions. */
-    SNAPSHOT_SFTP_UPLOAD_V2(60),
-
-    /** Certificates array in {@link SecuritySubject} implementations is deserialized lazily. */
-    LAZY_CERTIFICATE_DESERIALIZATION(61);
+    SNAPSHOT_SFTP_UPLOAD_V2(60);
 
     /**
      * Unique feature identifier.
