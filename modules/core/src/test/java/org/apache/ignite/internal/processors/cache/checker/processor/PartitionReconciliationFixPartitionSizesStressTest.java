681760a81b503dfeb4ff27b41d715d212021ef3c
sergeyuttsel <uttsel@gmail.com>
2021-09-07 16:28:38 +0300

    GG-32303 Attempt to fix PartitionReconciliationFixPartitionSizesStressTest. Other reconciliation tests are muted.

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/checker/processor/PartitionReconciliationFixPartitionSizesStressTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/checker/processor/PartitionReconciliationFixPartitionSizesStressTest.java
@@ -296,10 +296,18 @@ public class PartitionReconciliationFixPartitionSizesStressTest extends Partitio
         for (IgniteInternalFuture fut : loadFuts)
             fut.get();
 
+        awaitPartitionMapExchange();
+
+        cacheNames.forEach(cacheName -> assertPartitionsSame(idleVerify(grid(0), cacheName)));
+
         for (long i = startKey; i < endKey; i++) {
             for (IgniteCache<Object, Object> cache : caches)
                 cache.put(i, i);
         }
+
+        awaitPartitionMapExchange();
+
+        cacheNames.forEach(cacheName -> assertPartitionsSame(idleVerify(grid(0), cacheName)));
 
         long allKeysCountForCacheGroup;
         long allKeysCountForCache;
