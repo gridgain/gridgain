cc6b0bd72b5898d3861c3fd9dc0d9c22568ebceb
Eduard Rakhmankulov <erixonich@gmail.com>
2021-09-24 15:05:38 +0300

    GG-33835 fixed unused import

--- modules/core/src/main/java/org/apache/ignite/internal/util/typedef/X.java
+++ modules/core/src/main/java/org/apache/ignite/internal/util/typedef/X.java
@@ -28,7 +28,6 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.IdentityHashMap;
 import java.util.List;
