bda4ad3ed4a6e70c532ea424c962d28b6bc3f8a1
slukyanov <slukyanov@gridgain.com>
2021-10-14 20:38:13 +0300

    GG-34044: Removed consistency check for LocalDeploymentSpi.

---
+++ modules/core/src/test/java/org/apache/ignite/spi/deployment/local/LocalDeploymentSpiConsistencyCheckTest.java
@@ -0,0 +1,164 @@
+/*
+ * Copyright 2021 GridGain Systems, Inc. and Contributors.
+ *
+ * Licensed under the GridGain Community Edition License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.gridgain.com/products/software/community-edition/gridgain-community-edition-license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.ignite.spi.deployment.local;
+
+import org.apache.ignite.configuration.IgniteConfiguration;
+import org.apache.ignite.spi.IgniteSpiAdapter;
+import org.apache.ignite.spi.IgniteSpiConsistencyChecked;
+import org.apache.ignite.spi.IgniteSpiException;
+import org.apache.ignite.spi.IgniteSpiMultipleInstancesSupport;
+import org.apache.ignite.spi.deployment.DeploymentListener;
+import org.apache.ignite.spi.deployment.DeploymentResource;
+import org.apache.ignite.spi.deployment.DeploymentSpi;
+import org.apache.ignite.testframework.GridTestUtils;
+import org.apache.ignite.testframework.ListeningTestLogger;
+import org.apache.ignite.testframework.LogListener;
+import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.function.Consumer;
+
+@RunWith(Parameterized.class)
+public class LocalDeploymentSpiConsistencyCheckTest extends GridCommonAbstractTest {
+
+    /** */
+    @Parameterized.Parameter(0)
+    public DeploymentSpi spi1;
+
+    /** */
+    @Parameterized.Parameter(1)
+    public DeploymentSpi spi2;
+
+    /** */
+    @Parameterized.Parameter(2)
+    public boolean isSecondClient;
+
+    /** */
+    @Parameterized.Parameter(3)
+    public boolean firstWarns;
+
+    /** */
+    @Parameterized.Parameter(4)
+    public boolean secondFails;
+
+    /** */
+    @Parameterized.Parameters(name = "spi1={0}, spi2={1}, isSecondClient={2}, firstWarns={3}, secondFails={4}")
+    public static Collection<Object[]> testData() {
+        return Arrays.asList(new Object[][]{
+            { new LocalDeploymentSpi(),        new InconsistentDeploymentSpi(), false, false, false },
+            { new LocalDeploymentSpi(),          new ConsistentDeploymentSpi(), false, false,  true },
+            { new LocalDeploymentSpi(), new ConsistentNoClientsDeploymentSpi(), false, false,  true },
+            { new InconsistentDeploymentSpi(),        new LocalDeploymentSpi(), false, false, false },
+            { new ConsistentDeploymentSpi(),          new LocalDeploymentSpi(), false,  true, false },
+            { new ConsistentNoClientsDeploymentSpi(), new LocalDeploymentSpi(), false,  true, false },
+
+            { new LocalDeploymentSpi(),        new InconsistentDeploymentSpi(),  true, false, false },
+            { new LocalDeploymentSpi(),          new ConsistentDeploymentSpi(),  true, false,  true },
+            { new LocalDeploymentSpi(), new ConsistentNoClientsDeploymentSpi(),  true, false, false },
+            { new InconsistentDeploymentSpi(),        new LocalDeploymentSpi(),  true, false, false },
+            { new ConsistentDeploymentSpi(),          new LocalDeploymentSpi(),  true,  true, false },
+            { new ConsistentNoClientsDeploymentSpi(), new LocalDeploymentSpi(),  true, false, false },
+        });
+    }
+
+    @After
+    public void tearDown() {
+        stopAllGrids();
+    }
+
+    @Test
+    @SuppressWarnings("ThrowableNotThrown")
+    public void test() throws Exception {
+        LogListener listener1 =
+            LogListener.matches(">>> Remote SPI with the same name is not configured").build();
+
+        LogListener listener2 =
+            LogListener.matches(">>> Remote SPI with the same name is not configured").build();
+
+        startGrid(0, (Consumer<IgniteConfiguration>) cfg ->
+            cfg.setDeploymentSpi(spi1).setGridLogger(new ListeningTestLogger(log, listener1)));
+
+        Consumer<IgniteConfiguration> node2Cfg = cfg ->
+            cfg.setDeploymentSpi(spi2).setGridLogger(new ListeningTestLogger(log, listener2))
+                .setClientMode(isSecondClient);
+
+        if (!secondFails) {
+            startGrid(1, node2Cfg);
+
+            assertEquals(2, grid(1).cluster().nodes().size());
+        } else {
+            GridTestUtils.assertThrowsAnyCause(log, () -> startGrid(1, node2Cfg), IgniteSpiException.class,
+                "Remote SPI with the same name is not configured");
+        }
+
+        assertEquals(firstWarns, listener1.check());
+        assertFalse(listener2.check());
+    }
+
+    @IgniteSpiMultipleInstancesSupport(true)
+    private static class InconsistentDeploymentSpi extends TestNoopDeploymentSpi { }
+
+    @IgniteSpiMultipleInstancesSupport(true)
+    @IgniteSpiConsistencyChecked(optional = false, checkClient = false)
+    private static class ConsistentNoClientsDeploymentSpi extends TestNoopDeploymentSpi { }
+
+    @IgniteSpiMultipleInstancesSupport(true)
+    @IgniteSpiConsistencyChecked(optional = false)
+    private static class ConsistentDeploymentSpi extends TestNoopDeploymentSpi { }
+
+    private static abstract class TestNoopDeploymentSpi extends IgniteSpiAdapter implements DeploymentSpi {
+
+        @Override
+        public void spiStart(String igniteInstanceName) throws IgniteSpiException {
+            // No-op.
+        }
+
+        @Override
+        public void spiStop() throws IgniteSpiException {
+            // No-op.
+        }
+
+        @Override
+        public DeploymentResource findResource(String rsrcName) {
+            return null;
+        }
+
+        @Override
+        public boolean register(ClassLoader ldr, Class<?> rsrc) throws IgniteSpiException {
+            return false;
+        }
+
+        @Override
+        public boolean unregister(String rsrcName) {
+            return false;
+        }
+
+        @Override
+        public void setListener(DeploymentListener lsnr) {
+            // No-op.
+        }
+
+        @Override
+        public String toString() {
+            return this.getClass().getSimpleName();
+        }
+    }
+}
