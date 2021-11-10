a09db841aa02d64923f7aeb636228ccdbb671a14
vladimirermakov <vladermakov07@gmail.com>
2021-11-10 16:41:41 +0530

    GG-34221 Leave only IgniteBinaryCacheQueryTestSuite2 in BinaryMapper tests

--- modules/indexing/src/test/java/org/apache/ignite/testsuites/IgniteBinarySimpleNameMapperCacheQueryTestSuite.java
+++ modules/indexing/src/test/java/org/apache/ignite/testsuites/IgniteBinarySimpleNameMapperCacheQueryTestSuite.java
@@ -26,7 +26,6 @@ import org.junit.runners.Suite;
  */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        IgniteBinaryCacheQueryTestSuite.class,
         IgniteBinaryCacheQueryTestSuite2.class
 })
 public class IgniteBinarySimpleNameMapperCacheQueryTestSuite {
