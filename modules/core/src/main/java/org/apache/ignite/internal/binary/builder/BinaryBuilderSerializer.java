7e248635172ec9ca2ce25f215e63200d5f0dfedf
Vladimir Ermakov <85303706+vladErmakov07@users.noreply.github.com>
2021-11-11 13:57:09 +0530

    GG-34146 Support binary HANLDEs by BinaryObjectBuilder (#2163)
    * GG-34146 Added handle check when writing collections and maps
    
    Co-authored-by: Andrew V. Mashenkov <AMashenkov@users.noreply.github.com>

--- modules/core/src/main/java/org/apache/ignite/internal/binary/builder/BinaryBuilderSerializer.java
+++ modules/core/src/main/java/org/apache/ignite/internal/binary/builder/BinaryBuilderSerializer.java
@@ -69,6 +69,9 @@ class BinaryBuilderSerializer {
         }
 
         if (val instanceof BinaryBuilderSerializationAware) {
+            if (writer.tryWriteAsHandle(val))
+                return;
+            
             ((BinaryBuilderSerializationAware)val).writeTo(writer, this);
 
             return;
@@ -141,6 +144,9 @@ class BinaryBuilderSerializer {
         if (forceCol || BinaryUtils.isSpecialCollection(val.getClass())) {
             Collection<?> c = (Collection<?>)val;
 
+            if (writer.tryWriteAsHandle(c))
+                return;
+            
             writer.writeByte(GridBinaryMarshaller.COL);
             writer.writeInt(c.size());
 
@@ -156,6 +162,9 @@ class BinaryBuilderSerializer {
 
         if (forceMap || BinaryUtils.isSpecialMap(val.getClass())) {
             Map<?, ?> map = (Map<?, ?>)val;
+
+            if (writer.tryWriteAsHandle(map))
+                return;
 
             writer.writeByte(GridBinaryMarshaller.MAP);
             writer.writeInt(map.size());
