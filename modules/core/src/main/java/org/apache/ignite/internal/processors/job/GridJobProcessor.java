f5531932213fb0bcfc80f5718d1a70fb8a5c7b24
Semyon Danilov <samvimes@yandex.ru>
2021-11-22 14:10:14 +0300

    GG-33884 [IGNITE-14725] Server node fail when client node send unsupported class version (#2143)
    (cherry picked from commit 93460a374748e122073e0a82515c8bfa7dbd38c9)
    
    Co-authored-by: Nikolay <nizhikov@apache.org>

--- modules/core/src/main/java/org/apache/ignite/internal/processors/job/GridJobProcessor.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/job/GridJobProcessor.java
@@ -67,8 +67,8 @@ import org.apache.ignite.internal.managers.systemview.walker.ComputeJobViewWalke
 import org.apache.ignite.internal.processors.GridProcessorAdapter;
 import org.apache.ignite.internal.processors.affinity.AffinityTopologyVersion;
 import org.apache.ignite.internal.processors.cache.GridCacheContext;
-import org.apache.ignite.internal.processors.cache.distributed.dht.topology.GridDhtLocalPartition;
 import org.apache.ignite.internal.processors.cache.distributed.dht.GridReservable;
+import org.apache.ignite.internal.processors.cache.distributed.dht.topology.GridDhtLocalPartition;
 import org.apache.ignite.internal.processors.jobmetrics.GridJobMetricsSnapshot;
 import org.apache.ignite.internal.processors.metric.MetricRegistry;
 import org.apache.ignite.internal.processors.metric.impl.AtomicLongMetric;
@@ -1291,7 +1291,7 @@ public class GridJobProcessor extends GridProcessorAdapter {
                     // If exception occurs on job initialization, deployment is released in job listener.
                     releaseDep = false;
 
-                    if (job.initialize(dep, dep.deployedClass(req.getTaskClassName()))) {
+                    if (job.initialize(dep, dep.deployedClass(req.getTaskClassName()).get1())) {
                         // Internal jobs will always be executed synchronously.
                         if (job.isInternal()) {
                             // This is an internal job and can be executed inside busy lock
