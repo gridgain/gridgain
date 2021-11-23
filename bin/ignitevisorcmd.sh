b03db4ac1280ff2d34ac5a5fa67310a976c1b3ce
Semyon Danilov <samvimes@yandex.ru>
2021-11-23 16:04:45 +0300

    GG-34220 Support for JDK17

--- bin/ignitevisorcmd.sh
+++ bin/ignitevisorcmd.sh
@@ -133,7 +133,7 @@ elif [ $version -gt 8 ] && [ $version -lt 11 ]; then
         --add-modules=java.xml.bind \
         ${JVM_OPTS}"
 
-elif [ $version -ge 11 ] ; then
+elif [ "${version}" -ge 11 ] && [ "${version}" -lt 15 ]; then
     JVM_OPTS="\
         --add-exports=java.base/jdk.internal.misc=ALL-UNNAMED \
         --add-exports=java.base/sun.nio.ch=ALL-UNNAMED \
@@ -142,6 +142,20 @@ elif [ $version -ge 11 ] ; then
         --add-exports=java.base/sun.reflect.generics.reflectiveObjects=ALL-UNNAMED \
         --add-opens=jdk.management/com.sun.management.internal=ALL-UNNAMED \
         --illegal-access=permit \
+        ${JVM_OPTS}"
+
+elif [ "${version}" -ge 15 ] ; then
+    JVM_OPTS="\
+        --add-opens=java.base/jdk.internal.misc=ALL-UNNAMED \
+        --add-opens=java.base/sun.nio.ch=ALL-UNNAMED \
+        --add-opens=java.management/com.sun.jmx.mbeanserver=ALL-UNNAMED \
+        --add-opens=jdk.internal.jvmstat/sun.jvmstat.monitor=ALL-UNNAMED \
+        --add-opens=java.base/sun.reflect.generics.reflectiveObjects=ALL-UNNAMED \
+        --add-opens=jdk.management/com.sun.management.internal=ALL-UNNAMED \
+        --add-opens=java.base/java.io=ALL-UNNAMED \
+        --add-opens=java.base/java.nio=ALL-UNNAMED \
+        --add-opens=java.base/java.util=ALL-UNNAMED \
+        --add-opens=java.base/java.lang=ALL-UNNAMED \
         ${JVM_OPTS}"
 fi
 
--- bin/wal-reader.sh
+++ bin/wal-reader.sh
@@ -120,7 +120,7 @@ if [ "$version" -gt 8 ] && [ "$version" -lt 11 ]; then
         --add-modules=java.xml.bind \
         ${CONTROL_JVM_OPTS}"
 
-elif [ "$version" -ge 11 ] ; then
+elif [ "${version}" -ge 11 ] && [ "${version}" -lt 15 ]; then
     CONTROL_JVM_OPTS="\
         --add-exports=java.base/jdk.internal.misc=ALL-UNNAMED \
         --add-exports=java.base/sun.nio.ch=ALL-UNNAMED \
@@ -129,6 +129,20 @@ elif [ "$version" -ge 11 ] ; then
         --add-exports=java.base/sun.reflect.generics.reflectiveObjects=ALL-UNNAMED \
         --add-opens=jdk.management/com.sun.management.internal=ALL-UNNAMED \
         --illegal-access=permit \
+        ${CONTROL_JVM_OPTS}"
+
+elif [ "${version}" -ge 15 ] ; then
+    CONTROL_JVM_OPTS="\
+        --add-opens=java.base/jdk.internal.misc=ALL-UNNAMED \
+        --add-opens=java.base/sun.nio.ch=ALL-UNNAMED \
+        --add-opens=java.management/com.sun.jmx.mbeanserver=ALL-UNNAMED \
+        --add-opens=jdk.internal.jvmstat/sun.jvmstat.monitor=ALL-UNNAMED \
+        --add-opens=java.base/sun.reflect.generics.reflectiveObjects=ALL-UNNAMED \
+        --add-opens=jdk.management/com.sun.management.internal=ALL-UNNAMED \
+        --add-opens=java.base/java.io=ALL-UNNAMED \
+        --add-opens=java.base/java.nio=ALL-UNNAMED \
+        --add-opens=java.base/java.util=ALL-UNNAMED \
+        --add-opens=java.base/java.lang=ALL-UNNAMED \
         ${CONTROL_JVM_OPTS}"
 fi
 
