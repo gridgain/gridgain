b850ba53e50ed5d45848bdeb881e7e266b6bf7aa
Roman Puchkovskiy <roman.puchkovskiy@gmail.com>
2021-12-12 18:34:58 +0400

    GG-34397 - fix a test

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/persistence/tree/io/TrackingPageIOTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/persistence/tree/io/TrackingPageIOTest.java
@@ -70,6 +70,8 @@ public class TrackingPageIOTest {
 
         buf.order(ByteOrder.nativeOrder());
 
+        PageIO.setType(GridUnsafe.bufferAddress(buf), PageIO.T_PAGE_UPDATE_TRACKING);
+
         return buf;
     }
 
