12f79fc51ba5fb10a3a324981f31ea295b9bba95
Roman Puchkovskiy <roman.puchkovskiy@gmail.com>
2021-12-20 11:33:34 +0400

    GG-34463 [IGNITE-15867] Socket shutdown called twice in GridNioServer (#2249)
    Cherry-picked from 3c28080671ad9dc274657f7712294b58942773e3.

--- modules/core/src/test/java/org/apache/ignite/testsuites/IgniteUtilSelfTestSuite.java
+++ modules/core/src/test/java/org/apache/ignite/testsuites/IgniteUtilSelfTestSuite.java
@@ -24,8 +24,10 @@ import org.apache.ignite.internal.util.HostAndPortRangeTest;
 import org.apache.ignite.internal.util.IgniteDevOnlyLogTest;
 import org.apache.ignite.internal.util.IgniteExceptionRegistrySelfTest;
 import org.apache.ignite.internal.util.IgniteUtilsSelfTest;
+import org.apache.ignite.internal.util.IgniteUtilsUnitTest;
 import org.apache.ignite.internal.util.nio.GridNioDelimitedBufferSelfTest;
 import org.apache.ignite.internal.util.nio.GridNioSelfTest;
+import org.apache.ignite.internal.util.nio.GridNioServerTest;
 import org.apache.ignite.internal.util.nio.GridNioSessionMetaKeySelfTest;
 import org.apache.ignite.internal.util.nio.GridNioSslSelfTest;
 import org.apache.ignite.internal.util.nio.impl.GridNioFilterChainSelfTest;
@@ -70,6 +72,7 @@ import org.junit.runners.Suite;
     GridThreadPoolExecutorServiceSelfTest.class,
     IgniteThreadPoolSizeTest.class,
     IgniteUtilsSelfTest.class,
+    IgniteUtilsUnitTest.class,
     IgniteVersionUtilsSelfTest.class,
     GridSpinReadWriteLockSelfTest.class,
     GridQueueSelfTest.class,
@@ -110,6 +113,7 @@ import org.junit.runners.Suite;
     // NIO.
     GridNioSessionMetaKeySelfTest.class,
     GridNioSelfTest.class,
+    GridNioServerTest.class,
     GridNioFilterChainSelfTest.class,
     GridNioSslSelfTest.class,
     GridNioDelimitedBufferSelfTest.class,
