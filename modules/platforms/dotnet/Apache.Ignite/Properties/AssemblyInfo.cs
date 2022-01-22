54ac9060332689e41e4d1ec38a8a1845c7a1ea40
GridGain Teamcity <ggtc@gridgain.com>
2022-01-22 13:03:33 +0300

    8.8.13-p1 release

--- modules/platforms/dotnet/Apache.Ignite/Properties/AssemblyInfo.cs
+++ modules/platforms/dotnet/Apache.Ignite/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("0f9702ec-da7d-4ce5-b4b7-73310c885355")]
 
-[assembly: AssemblyVersion("8.8.127.50721")]
-[assembly: AssemblyFileVersion("8.8.127.50721")]
-[assembly: AssemblyInformationalVersion("8.8.127")]
+[assembly: AssemblyVersion("8.8.13.61141")]
+[assembly: AssemblyFileVersion("8.8.13.61141")]
+[assembly: AssemblyInformationalVersion("8.8.13.1")]
