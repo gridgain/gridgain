c344283877aba2148d2db43fc2022fba103115d8
Pavel Tupitsyn <ptupitsyn@apache.org>
2022-02-07 18:26:24 +0300

    Exclude Scala and Spark from build script

--- modules/platforms/dotnet/build.ps1
+++ modules/platforms/dotnet/build.ps1
@@ -80,7 +80,7 @@ param (
     [switch]$clean,
     [ValidateSet("Release", "Debug")]
     [string]$configuration="Release",
-    [string]$mavenOpts="-U -P-lgpl,-scala,-examples,-test,-benchmarks -Dmaven.javadoc.skip=true",
+    [string]$mavenOpts="-U -P-lgpl,-scala,-all-scala,-spark-2.4,-examples,-test,-benchmarks -Dmaven.javadoc.skip=true",
 	[string]$jarDirs="modules\indexing\target,modules\core\target,modules\h2\target,modules\spring\target",
 	[string]$version="",
 	[string]$versionSuffix=""
