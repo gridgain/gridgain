33978712f8180d89b885c3217dfa4a7e2850b03e
Pavel Tupitsyn <ptupitsyn@apache.org>
2022-02-14 12:39:30 +0300

    GG-34492 [IGNITE-16180] Thin Client get service description implemented (#2334)

---
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/platform/client/service/ClientServiceGetDescriptorsRequest.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2022 GridGain Systems, Inc. and Contributors.
+ *
+ * Licensed under the GridGain Community Edition License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.gridgain.com/products/software/community-edition/gridgain-community-edition-license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.ignite.internal.processors.platform.client.service;
+
+import org.apache.ignite.binary.BinaryRawReader;
+import org.apache.ignite.internal.processors.platform.client.ClientConnectionContext;
+import org.apache.ignite.internal.processors.platform.client.ClientRequest;
+import org.apache.ignite.internal.processors.platform.client.ClientResponse;
+
+/**
+ * Request to get service descriptors.
+ */
+public class ClientServiceGetDescriptorsRequest extends ClientRequest {
+    /**
+     * Constructor.
+     *
+     * @param reader Reader.
+     */
+    public ClientServiceGetDescriptorsRequest(BinaryRawReader reader) {
+        super(reader);
+    }
+
+    /** {@inheritDoc} */
+    @Override public ClientResponse process(ClientConnectionContext ctx) {
+        return new ClientServiceDescriptorsResponse(requestId(), ctx.kernalContext().service().serviceDescriptors());
+    }
+}
