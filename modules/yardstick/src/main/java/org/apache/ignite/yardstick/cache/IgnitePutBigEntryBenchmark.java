74e9ef5c43f7c649ba228c6bf5a50a14915458f5
Ivan Artiukhov <iartukhov@gridgain.com>
2022-02-16 17:12:00 +0300

    add IgnitePutBigEntryBenchmark

---
+++ modules/yardstick/src/main/java/org/apache/ignite/yardstick/cache/IgnitePutBigEntryBenchmark.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2022 GridGain Systems, Inc. and Contributors.
+ *
+ * Licensed under the GridGain Community Edition License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.gridgain.com/products/software/community-edition/gridgain-community-edition-license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.ignite.yardstick.cache;
+
+import org.apache.ignite.IgniteCache;
+import org.apache.ignite.yardstick.cache.model.RandomBytesValue;
+
+import java.util.Map;
+
+/**
+ * Put an object consisting of large byte array into an atomic cache
+ */
+public class IgnitePutBigEntryBenchmark extends IgnitePutBenchmark {
+    /** {@inheritDoc} */
+    @Override public boolean test(Map<Object, Object> ctx) throws Exception {
+        int key = nextRandom(args.range());
+
+        IgniteCache<Integer, Object> cache = cacheForOperation();
+
+        cache.put(key, new RandomBytesValue(args.objectSize()));
+
+        return true;
+    }
+}
