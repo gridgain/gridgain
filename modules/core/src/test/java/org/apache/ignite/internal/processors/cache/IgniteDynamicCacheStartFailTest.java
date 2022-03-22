5c1c9f641aec1a91e7b593b78b82d9a613f1632c
Slava Koptilin <slava.koptilin@gmail.com>
2022-03-22 18:51:31 +0300

    GG-34725 Fixed an issue that lead to cluster instability

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/IgniteDynamicCacheStartFailTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/IgniteDynamicCacheStartFailTest.java
@@ -16,19 +16,7 @@
 
 package org.apache.ignite.internal.processors.cache;
 
-import javax.cache.CacheException;
-import org.apache.ignite.configuration.CacheConfiguration;
 import org.apache.ignite.configuration.IgniteConfiguration;
-import org.apache.ignite.internal.IgniteEx;
-import org.apache.ignite.internal.IgniteInternalFuture;
-import org.apache.ignite.internal.TestRecordingCommunicationSpi;
-import org.apache.ignite.internal.processors.affinity.AffinityTopologyVersion;
-import org.apache.ignite.internal.processors.cache.distributed.dht.preloader.GridDhtPartitionsSingleMessage;
-import org.apache.ignite.testframework.GridTestUtils;
-import org.junit.Test;
-
-import static org.apache.ignite.testframework.GridTestUtils.assertThrows;
-import static org.apache.ignite.testframework.GridTestUtils.waitForCondition;
 
 /**
  * Tests the recovery after a dynamic cache start failure.
@@ -38,8 +26,6 @@ public class IgniteDynamicCacheStartFailTest extends IgniteAbstractDynamicCacheS
     @Override protected IgniteConfiguration getConfiguration(String gridName) throws Exception {
         IgniteConfiguration cfg = super.getConfiguration(gridName);
 
-        cfg.setCommunicationSpi(new TestRecordingCommunicationSpi());
-
         return cfg;
     }
 
@@ -48,47 +34,5 @@ public class IgniteDynamicCacheStartFailTest extends IgniteAbstractDynamicCacheS
         startGrids(gridCount());
 
         awaitPartitionMapExchange();
-    }
-
-    @Test
-    public void testAAA() throws Exception {
-        IgniteEx client = startClientGrid(gridCount());
-
-        awaitPartitionMapExchange();
-
-        TestRecordingCommunicationSpi spi1 = TestRecordingCommunicationSpi.spi(grid(1));
-
-        spi1.blockMessages((node, msg) -> msg instanceof GridDhtPartitionsSingleMessage);
-
-        String cacheName = "test-npe-cache";
-
-        IgniteInternalFuture<?> startFut = GridTestUtils.runAsync(() -> {
-            CacheConfiguration<?, ?> cfg =
-                createCacheConfigsWithBrokenCacheStore(true, 0, 0, 1, false).get(0);
-
-            cfg.setName(cacheName);
-
-            client.getOrCreateCache(cfg);
-        });
-
-        spi1.waitForBlocked();
-
-        AffinityTopologyVersion currVer = grid(0).context().discovery().topologyVersionEx();
-        AffinityTopologyVersion expVer = currVer.nextMinorVersion();
-
-        IgniteInternalFuture<?> stopFut = GridTestUtils.runAsync(() -> {
-            client.destroyCache(cacheName);
-        });
-
-        assertTrue(
-            "Failed to wait for DynamicCacheChangeBatch message (destroy)",
-            waitForCondition(() -> grid(0).context().discovery().topologyVersionEx().equals(expVer), getTestTimeout()));
-
-        spi1.stopBlock();
-
-        // Creating a new cache should lead to a exception.
-        assertThrows(log, () -> startFut.get(getTestTimeout()), CacheException.class, null);
-
-        stopFut.get(getTestTimeout());
     }
 }
