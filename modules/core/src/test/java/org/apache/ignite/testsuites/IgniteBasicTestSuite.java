07cacb01c2937e794798315d494b57cdcb72c7c1
Slava Koptilin <slava.koptilin@gmail.com>
2022-04-05 17:51:50 +0300

    GG-33647 Added new sys variable IGNITE_PARTITION_RELEASE_FUTURE_WARN_LIMIT that allows to reduce a number diagnostic messages

--- modules/core/src/test/java/org/apache/ignite/testsuites/IgniteBasicTestSuite.java
+++ modules/core/src/test/java/org/apache/ignite/testsuites/IgniteBasicTestSuite.java
@@ -54,6 +54,7 @@ import org.apache.ignite.internal.MarshallerContextLockingSelfTest;
 import org.apache.ignite.internal.TransactionsMXBeanImplTest;
 import org.apache.ignite.internal.managers.IgniteDiagnosticMessagesMultipleConnectionsTest;
 import org.apache.ignite.internal.managers.IgniteDiagnosticMessagesTest;
+import org.apache.ignite.internal.managers.IgniteDiagnosticPartitionReleaseFutureLimitTest;
 import org.apache.ignite.internal.managers.discovery.IncompleteDeserializationExceptionTest;
 import org.apache.ignite.internal.mxbean.IgniteStandardMXBeanTest;
 import org.apache.ignite.internal.pagemem.wal.record.WALRecordSerializationTest;
@@ -263,6 +264,7 @@ import org.junit.runners.Suite;
 
     IgniteDiagnosticMessagesTest.class,
     IgniteDiagnosticMessagesMultipleConnectionsTest.class,
+    IgniteDiagnosticPartitionReleaseFutureLimitTest.class,
 
     IgniteRejectConnectOnNodeStopTest.class,
 
