7fdfdf71ca05f115cccf0a5393a9b3e04a5c6334
Igor Sapego <isapego@apache.org>
2022-05-24 14:40:59 +0400

    IGNITE-16877 C++ thin: Implement ScanQuery
    This closes #10033
    
    (cherry picked from commit b121a905850c2398c82706cdcc164bf0bcaa815c)

--- modules/platforms/cpp/thin-client/include/ignite/impl/thin/cache/cache_client_proxy.h
+++ modules/platforms/cpp/thin-client/include/ignite/impl/thin/cache/cache_client_proxy.h
@@ -20,9 +20,11 @@
 #include <ignite/common/concurrent.h>
 
 #include <ignite/thin/cache/query/query_fields_cursor.h>
+#include <ignite/thin/cache/query/query_scan.h>
 #include <ignite/thin/cache/query/query_sql_fields.h>
 
 #include <ignite/impl/thin/cache/continuous/continuous_query_client_holder.h>
+#include <ignite/impl/thin/cache/query/query_cursor_proxy.h>
 
 namespace ignite
 {
@@ -58,7 +60,7 @@ namespace ignite
                     /**
                      * Constructor.
                      */
-                    CacheClientProxy(const common::concurrent::SharedPointer<void>& impl) :
+                    explicit CacheClientProxy(const common::concurrent::SharedPointer<void>& impl) :
                         impl(impl)
                     {
                         // No-op.
@@ -297,6 +299,14 @@ namespace ignite
                      */
                     ignite::thin::cache::query::QueryFieldsCursor Query(
                             const ignite::thin::cache::query::SqlFieldsQuery& qry);
+
+                    /**
+                     * Perform scan query.
+                     *
+                     * @param qry Query.
+                     * @return Query cursor proxy.
+                     */
+                    query::QueryCursorProxy Query(const ignite::thin::cache::query::ScanQuery& qry);
 
                     /**
                      * Starts the continuous query execution
