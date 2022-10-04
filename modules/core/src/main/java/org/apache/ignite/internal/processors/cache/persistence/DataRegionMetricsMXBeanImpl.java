apache/ignite

IGNITE-17728 Improve size-related metrics (#10259)

modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/DataRegionMetricsMXBeanImpl.java
@@ -76,13 +76,18 @@ class DataRegionMetricsMXBeanImpl implements DataRegionMetricsMXBean {
     }
 
     /** {@inheritDoc} */
+    @Override public long getTotalAllocatedSize() {
+        return memMetrics.getTotalAllocatedSize();
+    }
+
+    /** {@inheritDoc} */
     @Override public long getTotalUsedPages() {
         return memMetrics.getTotalUsedPages();
     }
 
     /** {@inheritDoc} */
-    @Override public long getTotalAllocatedSize() {
-        return memMetrics.getTotalAllocatedSize();
+    @Override public long getTotalUsedSize() {
+        return memMetrics.getTotalUsedSize();
     }
 
     /** {@inheritDoc} */

gridgain/gridgain

modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/DataRegionMetricsMXBeanImpl.java
@@ -67,13 +67,13 @@ class DataRegionMetricsMXBeanImpl implements DataRegionMetricsMXBean {
     }
 
     /** {@inheritDoc} */
-    @Override public long getTotalUsedPages() {
-        return memMetrics.getTotalUsedPages();
+    @Override public long getTotalAllocatedSize() {
+        return memMetrics.getTotalAllocatedSize();
     }
 
     /** {@inheritDoc} */
-    @Override public long getTotalAllocatedSize() {
-        return memMetrics.getTotalAllocatedSize();
+    @Override public long getTotalUsedPages() {
+        return memMetrics.getTotalUsedPages();
     }
 
     /** {@inheritDoc} */