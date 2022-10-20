4e42c933c245e58c3fb6b67eee0b06ed36cf3210
Pavel Tupitsyn <ptupitsyn@apache.org>
2022-10-20 09:56:41 +0300

    wip PlatformWaitForRebalanceTask

---
+++ modules/core/src/test/java/org/apache/ignite/platform/PlatformWaitForRebalanceTask.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2022 GridGain Systems, Inc. and Contributors.
+ *
+ * Licensed under the GridGain Community Edition License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.gridgain.com/products/software/community-edition/gridgain-community-edition-license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.ignite.platform;
+
+import org.apache.ignite.Ignite;
+import org.apache.ignite.IgniteException;
+import org.apache.ignite.cluster.ClusterNode;
+import org.apache.ignite.compute.ComputeJob;
+import org.apache.ignite.compute.ComputeJobAdapter;
+import org.apache.ignite.compute.ComputeJobResult;
+import org.apache.ignite.compute.ComputeTaskAdapter;
+import org.apache.ignite.internal.IgniteEx;
+import org.apache.ignite.internal.IgniteInterruptedCheckedException;
+import org.apache.ignite.internal.processors.affinity.AffinityTopologyVersion;
+import org.apache.ignite.internal.processors.cache.distributed.dht.topology.GridDhtPartitionTopology;
+import org.apache.ignite.internal.util.typedef.F;
+import org.apache.ignite.internal.util.typedef.internal.CU;
+import org.apache.ignite.resources.IgniteInstanceResource;
+import org.apache.ignite.testframework.GridTestUtils;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Task that waits for rebalance to complete.
+ */
+public class PlatformWaitForRebalanceTask extends ComputeTaskAdapter<Object[], Boolean> {
+    /** {@inheritDoc} */
+    @NotNull
+    @Override public Map<? extends ComputeJob, ClusterNode> map(List<ClusterNode> subgrid, Object[] args)
+            throws IgniteException {
+        return Collections.singletonMap(new Job(args), F.first(subgrid));
+    }
+
+    /** {@inheritDoc} */
+    @Nullable @Override public Boolean reduce(List<ComputeJobResult> results) throws IgniteException {
+        return results.get(0).getData();
+    }
+
+    /**
+     * Job.
+     */
+    private static class Job extends ComputeJobAdapter {
+        /** Expected version. */
+        private final AffinityTopologyVersion topVer;
+
+        private final String cacheName;
+
+        private final long timeout;
+
+        /** Ignite. */
+        @IgniteInstanceResource
+        private Ignite ignite;
+
+        /**
+         * Ctor.
+         *
+         * @param args Args.
+         */
+        private Job(Object[] args) {
+            topVer = (AffinityTopologyVersion) args[0];
+            cacheName = (String) args[1];
+            timeout = (Long) args[2];
+        }
+
+        /** {@inheritDoc} */
+        @Override public Boolean execute() throws IgniteException {
+            final GridDhtPartitionTopology top =
+                    ((IgniteEx) ignite).context().cache().context().cacheContext(CU.cacheId(cacheName)).topology();
+
+            try {
+                return GridTestUtils.waitForCondition(() -> top.rebalanceFinished(topVer), timeout);
+            } catch (IgniteInterruptedCheckedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
