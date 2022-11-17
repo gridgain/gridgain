ecc1891d8356a89a1a3b1e9a880fdca2c518a48a
Aleksey Plekhanov <plehanov.alex@gmail.com>
2022-11-17 17:09:09 +0200

    GG-35831 [IGNITE-13653] Remove warning for unordered bulk operation on ATOMIC cache.
    Signed-off-by: Slava Koptilin <slava.koptilin@gmail.com>
    (cherry picked from commit 2057083725f40f75e86e87c56eb98ffac5f2d76e)

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/GridCacheAdapter.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/GridCacheAdapter.java
@@ -4429,6 +4429,9 @@ public abstract class GridCacheAdapter<K, V> implements IgniteInternalCache<K, V
      * @param m Map to examine.
      */
     protected void warnIfUnordered(Map<?, ?> m, BulkOperation op) {
+        if (ctx.atomic())
+            return;
+
         if (m == null || m.size() <= 1)
             return;
 
@@ -4453,6 +4456,9 @@ public abstract class GridCacheAdapter<K, V> implements IgniteInternalCache<K, V
      * @param coll Collection to examine.
      */
     protected void warnIfUnordered(Collection<?> coll, BulkOperation op) {
+        if (ctx.atomic())
+            return;
+
         if (coll == null || coll.size() <= 1)
             return;
 
