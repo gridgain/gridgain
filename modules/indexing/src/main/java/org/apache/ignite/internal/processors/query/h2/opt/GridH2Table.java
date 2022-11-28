e4fccf70386d39b454639cd712b4fe74f1bd9e5f
Ilya Korol <llivezking@gmail.com>
2022-11-28 13:13:43 +0300

    Issue-17700 Added extensive logging in index selection and for TableStatistics updates
    Also added compatibility cost function getCostRangeIndex_8_7_6_x which honors passed rowCount (to simulate same behaviour as 8.5.25)

--- modules/indexing/src/main/java/org/apache/ignite/internal/processors/query/h2/opt/GridH2Table.java
+++ modules/indexing/src/main/java/org/apache/ignite/internal/processors/query/h2/opt/GridH2Table.java
@@ -1466,6 +1466,16 @@ public class GridH2Table extends TableBase {
             long primaryRowCnt = cacheSize(CachePeekMode.PRIMARY);
             long totalRowCnt = cacheSize(CachePeekMode.PRIMARY, CachePeekMode.BACKUP);
 
+            if (IGNITE_SHOW_ORDER_OF_INDEXES) {
+                System.out.println(">>> Refresh statistics: " + this.identifierStr + " [" +
+                        "statsTotalRowCnt=" + statsTotalRowCnt + ", " +
+                        "curTotalRowCnt=" + curTotalRowCnt + ", " +
+                        "primaryRowCnt=" + primaryRowCnt + ", " +
+                        "totalRowCnt=" + totalRowCnt +
+                        "]"
+                );
+            }
+
             size.reset();
             size.add(totalRowCnt);
 
