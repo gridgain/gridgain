39469c6df0c41cfebfafcac9f68fa23746a17ab3
Kirill Tkalenko <tkalkirill@yandex.ru>
2022-12-20 09:55:10 +0300

    GG-35837 IndexPagesMetricsPageDisplacementTest.testPageDisplacement constantly fails on master (#2667)

--- modules/indexing/src/test/java/org/apache/ignite/internal/metric/IndexPagesMetricsPageDisplacementTest.java
+++ modules/indexing/src/test/java/org/apache/ignite/internal/metric/IndexPagesMetricsPageDisplacementTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 GridGain Systems, Inc. and Contributors.
+ * Copyright 2022 GridGain Systems, Inc. and Contributors.
  *
  * Licensed under the GridGain Community Edition License (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,9 +20,11 @@ import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.util.ArrayList;
 import java.util.List;
-import org.apache.ignite.IgniteCache;
+import java.util.Objects;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 import org.apache.ignite.IgniteCheckedException;
-import org.apache.ignite.client.Person;
+import org.apache.ignite.cache.query.annotations.QuerySqlField;
 import org.apache.ignite.cluster.ClusterState;
 import org.apache.ignite.configuration.CacheConfiguration;
 import org.apache.ignite.configuration.DataRegionConfiguration;
@@ -33,15 +35,22 @@ import org.apache.ignite.internal.IgniteEx;
 import org.apache.ignite.internal.pagemem.PageIdAllocator;
 import org.apache.ignite.internal.pagemem.PageIdUtils;
 import org.apache.ignite.internal.pagemem.PageMemory;
+import org.apache.ignite.internal.processors.cache.IgniteInternalCache;
 import org.apache.ignite.internal.processors.cache.persistence.DataRegion;
 import org.apache.ignite.internal.processors.cache.persistence.file.FilePageStore;
 import org.apache.ignite.internal.processors.cache.persistence.file.FilePageStoreManager;
 import org.apache.ignite.internal.processors.cache.persistence.pagemem.PageMetrics;
 import org.apache.ignite.internal.processors.cache.persistence.tree.io.PageIO;
 import org.apache.ignite.internal.processors.cache.persistence.wal.crc.IgniteDataIntegrityViolationException;
+import org.apache.ignite.internal.util.IgniteUtils;
+import org.apache.ignite.internal.util.typedef.internal.S;
 import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
 import org.junit.Test;
 
+import static java.util.stream.Collectors.joining;
+import static org.apache.ignite.internal.util.IgniteUtils.KB;
+import static org.apache.ignite.internal.util.IgniteUtils.MB;
+import static org.apache.ignite.testframework.GridTestUtils.getFieldValue;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.junit.Assert.assertThat;
@@ -50,39 +59,20 @@ import static org.junit.Assert.assertThat;
  * Test class for checking that index page metrics stay correct in case of page displacement.
  */
 public class IndexPagesMetricsPageDisplacementTest extends GridCommonAbstractTest {
-    /** */
-    private static final String TEST_CACHE_NAME = "test";
-
-    /** */
-    private IgniteEx grid;
-
-    /** */
-    private IgniteCache<Integer, Person> cache;
-
-    /** */
-    private IndexPageCounter idxPageCounter;
-
     /** {@inheritDoc} */
     @Override protected void beforeTest() throws Exception {
-        cleanPersistenceDir();
-
         super.beforeTest();
 
-        grid = startGrid();
-        grid.cluster().state(ClusterState.ACTIVE);
+        stopAllGrids();
 
-        CacheConfiguration<Integer, Person> cacheConfiguration = new CacheConfiguration<Integer, Person>(TEST_CACHE_NAME)
-            .setIndexedTypes(Integer.class, Person.class);
-        cache = grid.createCache(cacheConfiguration);
-
-        idxPageCounter = new IndexPageCounter(grid, true);
+        cleanPersistenceDir();
     }
 
     /** {@inheritDoc} */
     @Override protected void afterTest() throws Exception {
         super.afterTest();
 
-        grid.close();
+        stopAllGrids();
 
         cleanPersistenceDir();
     }
@@ -91,11 +81,18 @@ public class IndexPagesMetricsPageDisplacementTest extends GridCommonAbstractTes
     @Override protected IgniteConfiguration getConfiguration(String igniteInstanceName) throws Exception {
         return super.getConfiguration(igniteInstanceName)
             .setFailureHandler(new StopNodeFailureHandler())
+            .setCacheConfiguration(
+                new CacheConfiguration<>(DEFAULT_CACHE_NAME).setIndexedTypes(Integer.class, Person.class)
+            )
             .setDataStorageConfiguration(
                 new DataStorageConfiguration()
+                    // Default values are not suitable for us because bigger concurrency level means bigger
+                    // segment sizes which means no page replacement.
+                    .setPageSize((int)(4 * KB))
+                    .setConcurrencyLevel(4)
                     .setDefaultDataRegionConfiguration(
                         new DataRegionConfiguration()
-                            .setMaxSize(12 * 1024 * 1024) // 12 MB
+                            .setMaxSize(12 * MB)
                             .setPersistenceEnabled(true)
                     )
             );
@@ -107,32 +104,51 @@ public class IndexPagesMetricsPageDisplacementTest extends GridCommonAbstractTes
      * go down.
      */
     @Test
-    public void testPageDisplacement() throws IgniteCheckedException {
-        int grpId = grid.cachex(TEST_CACHE_NAME).context().groupId();
+    public void testPageDisplacement() throws Exception {
+        IgniteEx grid = startGrid(0);
+
+        grid.cluster().state(ClusterState.ACTIVE);
+
+        IndexPageCounter idxPageCounter = new IndexPageCounter(grid, true);
+
+        IgniteInternalCache<Integer, Person> cache = grid.cachex(DEFAULT_CACHE_NAME);
+
+        int grpId = cache.context().groupId();
 
         DataRegion dataRegion = grid.context().cache().context().database().dataRegion(null);
         PageMetrics pageMetrics = dataRegion.metrics().cacheGrpPageMetrics(grpId);
+
+        // Test may start to flaky if the size of the PageMemoryImpl.Segment increases.
+        assertEquals(
+            dataRegion.config().getMaxSize() + IgniteUtils.checkpointBufferSize(dataRegion.config()),
+            LongStream.of(getFieldValue(dataRegion.pageMemory(), "sizes")).sum()
+        );
 
         int personId = 0;
         long idxPagesOnDisk;
         long idxPagesInMemory;
 
+        // To make the index pages larger (and their number) and have a higher chance of replacing them.
+        String namePrefix = IntStream.range(0, (int)KB).mapToObj(i -> "a").collect(joining());
+
         do {
             // insert data into the cache until some index pages get displaced to the storage
             for (int i = 0; i < 100; i++, personId++)
-                cache.put(personId, new Person(personId, "foobar"));
+                cache.put(
+                    personId,
+                    new Person(personId, namePrefix + "_name_" + personId, namePrefix + "_surname_" + personId)
+                );
 
             forceCheckpoint(grid);
 
-            idxPagesOnDisk = getIdxPagesOnDisk(grpId).size();
+            idxPagesOnDisk = getIdxPagesOnDisk(grid, grpId).size();
             idxPagesInMemory = idxPageCounter.countIdxPagesInMemory(grpId);
 
             assertThat(pageMetrics.indexPages().value(), is(idxPagesInMemory));
-
         } while (idxPagesOnDisk <= idxPagesInMemory);
 
         // load pages back into memory and check that the metric value has increased
-        touchIdxPages(grpId);
+        touchIdxPages(grid, grpId);
 
         long allIdxPagesInMemory = idxPageCounter.countIdxPagesInMemory(grpId);
 
@@ -144,11 +160,11 @@ public class IndexPagesMetricsPageDisplacementTest extends GridCommonAbstractTes
      * Acquires all pages inside the index partition for the given cache group in order to load them back into
      * memory if some of them had been displaced to the storage earlier.
      */
-    private void touchIdxPages(int grpId) throws IgniteCheckedException {
+    private void touchIdxPages(IgniteEx grid, int grpId) throws IgniteCheckedException {
         DataRegion dataRegion = grid.context().cache().context().database().dataRegion(null);
         PageMemory pageMemory = dataRegion.pageMemory();
 
-        for (long pageId : getIdxPagesOnDisk(grpId))
+        for (long pageId : getIdxPagesOnDisk(grid, grpId))
             // acquire, but not release all pages, so that they don't get displaced back to the storage
             pageMemory.acquirePage(grpId, pageId);
     }
@@ -156,7 +172,7 @@ public class IndexPagesMetricsPageDisplacementTest extends GridCommonAbstractTes
     /**
      * Returns IDs of index pages currently residing in the storage.
      */
-    private List<Long> getIdxPagesOnDisk(int grpId) throws IgniteCheckedException {
+    private List<Long> getIdxPagesOnDisk(IgniteEx grid, int grpId) throws IgniteCheckedException {
         FilePageStoreManager pageStoreMgr = (FilePageStoreManager) grid.context().cache().context().pageStore();
         FilePageStore pageStore = (FilePageStore) pageStoreMgr.getStore(grpId, PageIdAllocator.INDEX_PARTITION);
 
@@ -186,5 +202,79 @@ public class IndexPagesMetricsPageDisplacementTest extends GridCommonAbstractTes
         }
 
         return result;
+    }
+
+    /**
+     * A person entity used for the tests. More indexes are used so that there are more index pages.
+     */
+    private static class Person {
+        /** Id. */
+        @QuerySqlField(index = true)
+        private Integer id;
+
+        /** Name. */
+        @QuerySqlField(index = true)
+        private String name;
+
+        /** Name. */
+        @QuerySqlField(index = true)
+        private String surname;
+
+        /** */
+        public Person(Integer id, String name, String surname) {
+            this.id = id;
+            this.name = name;
+            this.surname = surname;
+        }
+
+        /** */
+        public Integer getId() {
+            return id;
+        }
+
+        /** */
+        public String getName() {
+            return name;
+        }
+
+        /** */
+        public String getSurname() {
+            return surname;
+        }
+
+        /** */
+        public void setId(Integer id) {
+            this.id = id;
+        }
+
+        /** */
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        /** */
+        public void setSurname(String surname) {
+            this.surname = surname;
+        }
+
+        /** {@inheritDoc} */
+        @Override public int hashCode() {
+            return Objects.hash(id, name, surname);
+        }
+
+        /** {@inheritDoc} */
+        @Override public boolean equals(Object obj) {
+            if (!(obj instanceof Person))
+                return false;
+
+            Person other = (Person)obj;
+
+            return other.id.equals(id) && other.name.equals(name) && other.surname.equals(surname);
+        }
+
+        /** {@inheritDoc} */
+        @Override public String toString() {
+            return S.toString(Person.class, this);
+        }
     }
 }
