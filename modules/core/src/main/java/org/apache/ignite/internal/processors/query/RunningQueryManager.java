0e0c2d048a175993216aa38fceeeb282f3e6ae08
Pavel Pereslegin <xxtern@gmail.com>
2022-12-22 19:04:04 +0300

    GG-36023 TC check.

--- modules/core/src/main/java/org/apache/ignite/internal/processors/query/RunningQueryManager.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/query/RunningQueryManager.java
@@ -41,10 +41,9 @@ import org.apache.ignite.internal.processors.closure.GridClosureProcessor;
 import org.apache.ignite.internal.processors.metric.MetricRegistry;
 import org.apache.ignite.internal.processors.metric.impl.AtomicLongMetric;
 import org.apache.ignite.internal.processors.metric.impl.LongAdderMetric;
-import org.apache.ignite.internal.util.typedef.internal.A;
 import org.apache.ignite.internal.processors.tracing.Span;
+import org.apache.ignite.internal.util.typedef.internal.A;
 import org.apache.ignite.internal.util.typedef.internal.S;
-import org.apache.ignite.internal.util.typedef.internal.U;
 import org.apache.ignite.spi.systemview.view.SqlQueryHistoryView;
 import org.apache.ignite.spi.systemview.view.SqlQueryView;
 import org.jetbrains.annotations.Nullable;
@@ -293,8 +292,8 @@ public class RunningQueryManager {
             if (failed)
                 qrySpan.addTag(ERROR, failReason::getMessage);
 
-            if (qry.memoryMetricProvider() instanceof AutoCloseable)
-                U.close((AutoCloseable)qry.memoryMetricProvider(), log);
+//            if (qry.memoryMetricProvider() instanceof AutoCloseable)
+//                U.close((AutoCloseable)qry.memoryMetricProvider(), log);
 
             if (log.isDebugEnabled()) {
                 log.debug("User's query " + (failReason == null ? "completed " : "failed ") +
