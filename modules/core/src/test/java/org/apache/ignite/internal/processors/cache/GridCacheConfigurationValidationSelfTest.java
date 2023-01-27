a1606b955ddb0ecb7e8d98cbe04d390009827f01
Kirill Tkalenko <tkalkirill@yandex.ru>
2023-01-27 11:37:19 +0300

    GG-35935 wip

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/GridCacheConfigurationValidationSelfTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/GridCacheConfigurationValidationSelfTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 GridGain Systems, Inc. and Contributors.
+ * Copyright 2023 GridGain Systems, Inc. and Contributors.
  *
  * Licensed under the GridGain Community Edition License (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,20 +16,27 @@
 
 package org.apache.ignite.internal.processors.cache;
 
+import java.util.Collection;
+import javax.cache.CacheException;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.cache.affinity.rendezvous.RendezvousAffinityFunction;
 import org.apache.ignite.configuration.CacheConfiguration;
+import org.apache.ignite.configuration.DataRegionConfiguration;
+import org.apache.ignite.configuration.DataStorageConfiguration;
 import org.apache.ignite.configuration.IgniteConfiguration;
+import org.apache.ignite.internal.IgniteEx;
 import org.apache.ignite.internal.processors.datastructures.DataStructuresProcessor;
 import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
 import org.junit.Test;
 
-import java.util.Collection;
-
-import static org.apache.ignite.cache.CacheMode.*;
+import static org.apache.ignite.cache.CacheMode.LOCAL;
+import static org.apache.ignite.cache.CacheMode.PARTITIONED;
+import static org.apache.ignite.cache.CacheMode.REPLICATED;
 import static org.apache.ignite.cache.CacheRebalanceMode.ASYNC;
 import static org.apache.ignite.cache.CacheRebalanceMode.SYNC;
 import static org.apache.ignite.cache.CacheWriteSynchronizationMode.FULL_SYNC;
+import static org.apache.ignite.cluster.ClusterState.ACTIVE;
+import static org.apache.ignite.testframework.GridTestUtils.assertThrows;
 
 /**
  * Attribute validation self test.
@@ -74,41 +81,58 @@ public class GridCacheConfigurationValidationSelfTest extends GridCommonAbstract
     /** */
     private static final String CACHE_NAME_WITH_SPECIAL_CHARACTERS_PARTITIONED = ":_&:: (partitioned)";
 
-    /**
-     * Constructs test.
-     */
-    public GridCacheConfigurationValidationSelfTest() {
-        super(/* don't start grid */ false);
+    /** */
+    private static final String REGION_NAME = "test";
+
+    /** */
+    private static final String DEFAULT_CACHE_GROUP_NAME = DEFAULT_CACHE_NAME + "_grp";
+
+    /** {@inheritDoc} */
+    @Override protected void beforeTest() throws Exception {
+        super.beforeTest();
+
+        stopAllGrids();
+
+        cleanPersistenceDir();
+    }
+
+    /** {@inheritDoc} */
+    @Override protected void afterTest() throws Exception {
+        super.afterTest();
+
+        stopAllGrids();
+
+        cleanPersistenceDir();
     }
 
     /** {@inheritDoc} */
     @Override protected IgniteConfiguration getConfiguration(String igniteInstanceName) throws Exception {
-        IgniteConfiguration cfg = super.getConfiguration(igniteInstanceName);
+        IgniteConfiguration cfg = super.getConfiguration(igniteInstanceName)
+            .setDataStorageConfiguration(
+                new DataStorageConfiguration().setDataRegionConfigurations(
+                    new DataRegionConfiguration().setName(REGION_NAME)
+                )
+            );
 
         // Default cache config.
-        CacheConfiguration dfltCacheCfg = defaultCacheConfiguration();
-
-        dfltCacheCfg.setCacheMode(PARTITIONED);
-        dfltCacheCfg.setRebalanceMode(ASYNC);
-        dfltCacheCfg.setWriteSynchronizationMode(FULL_SYNC);
-        dfltCacheCfg.setAffinity(new RendezvousAffinityFunction());
-        dfltCacheCfg.setIndexedTypes(
-            Integer.class, String.class
-        );
-        dfltCacheCfg.setName(CACHE_NAME_WITH_SPECIAL_CHARACTERS_PARTITIONED);
+        CacheConfiguration<?, ?> dfltCacheCfg = defaultCacheConfiguration()
+            .setCacheMode(PARTITIONED)
+            .setRebalanceMode(ASYNC)
+            .setWriteSynchronizationMode(FULL_SYNC)
+            .setAffinity(new RendezvousAffinityFunction())
+            .setIndexedTypes(Integer.class, String.class)
+            .setName(CACHE_NAME_WITH_SPECIAL_CHARACTERS_PARTITIONED);
 
         // Non-default cache configuration.
-        CacheConfiguration namedCacheCfg = defaultCacheConfiguration();
-
-        namedCacheCfg.setCacheMode(PARTITIONED);
-        namedCacheCfg.setRebalanceMode(ASYNC);
-        namedCacheCfg.setWriteSynchronizationMode(FULL_SYNC);
-        namedCacheCfg.setAffinity(new RendezvousAffinityFunction());
+        CacheConfiguration<?, ?> namedCacheCfg = defaultCacheConfiguration()
+            .setCacheMode(PARTITIONED)
+            .setRebalanceMode(ASYNC)
+            .setWriteSynchronizationMode(FULL_SYNC)
+            .setAffinity(new RendezvousAffinityFunction());
 
         // Local cache configuration.
-        CacheConfiguration localCacheCfg = defaultCacheConfiguration();
-
-        localCacheCfg.setCacheMode(LOCAL);
+        CacheConfiguration<?, ?> localCacheCfg = defaultCacheConfiguration()
+            .setCacheMode(LOCAL);
 
         // Modify cache config according to test parameters.
         if (igniteInstanceName.contains(WRONG_PRELOAD_MODE_IGNITE_INSTANCE_NAME))
@@ -150,11 +174,9 @@ public class GridCacheConfigurationValidationSelfTest extends GridCommonAbstract
 
     /**
      * This test method does not require remote nodes.
-     *
-     * @throws Exception If failed.
      */
     @Test
-    public void testDuplicateCacheConfigurations() throws Exception {
+    public void testDuplicateCacheConfigurations() {
         // This grid should not start.
         startInvalidGrid(DUP_CACHES_IGNITE_INSTANCE_NAME);
 
@@ -167,36 +189,31 @@ public class GridCacheConfigurationValidationSelfTest extends GridCommonAbstract
      */
     @Test
     public void testCacheAttributesValidation() throws Exception {
-        try {
-            startGrid(0);
+        startGrid(0);
 
-            // This grid should not start.
-            startInvalidGrid(WRONG_PRELOAD_MODE_IGNITE_INSTANCE_NAME);
+        // This grid should not start.
+        startInvalidGrid(WRONG_PRELOAD_MODE_IGNITE_INSTANCE_NAME);
 
-            // This grid should not start.
-            startInvalidGrid(WRONG_CACHE_MODE_IGNITE_INSTANCE_NAME);
+        // This grid should not start.
+        startInvalidGrid(WRONG_CACHE_MODE_IGNITE_INSTANCE_NAME);
 
-            // This grid should not start.
-            startInvalidGrid(WRONG_AFFINITY_IGNITE_INSTANCE_NAME);
+        // This grid should not start.
+        startInvalidGrid(WRONG_AFFINITY_IGNITE_INSTANCE_NAME);
 
-            // This grid should not start.
-            startInvalidGrid(WRONG_AFFINITY_MAPPER_IGNITE_INSTANCE_NAME);
+        // This grid should not start.
+        startInvalidGrid(WRONG_AFFINITY_MAPPER_IGNITE_INSTANCE_NAME);
 
-            // This grid should not start.
-            startInvalidGrid(RESERVED_FOR_DATASTRUCTURES_CACHE_NAME_IGNITE_INSTANCE_NAME);
+        // This grid should not start.
+        startInvalidGrid(RESERVED_FOR_DATASTRUCTURES_CACHE_NAME_IGNITE_INSTANCE_NAME);
 
-            // This grid should not start.
-            startInvalidGrid(RESERVED_FOR_DATASTRUCTURES_CACHE_GROUP_NAME_IGNITE_INSTANCE_NAME);
+        // This grid should not start.
+        startInvalidGrid(RESERVED_FOR_DATASTRUCTURES_CACHE_GROUP_NAME_IGNITE_INSTANCE_NAME);
 
-            // This grid should not start.
-            startInvalidGrid(RESERVED_FOR_VOLATILE_DATASTRUCTURES_CACHE_GROUP_NAME_IGNITE_INSTANCE_NAME);
+        // This grid should not start.
+        startInvalidGrid(RESERVED_FOR_VOLATILE_DATASTRUCTURES_CACHE_GROUP_NAME_IGNITE_INSTANCE_NAME);
 
-            // This grid will start normally.
-            startGrid(1);
-        }
-        finally {
-            stopAllGrids();
-        }
+        // This grid will start normally.
+        startGrid(1);
     }
 
     /**
@@ -204,29 +221,63 @@ public class GridCacheConfigurationValidationSelfTest extends GridCommonAbstract
      */
     @Test
     public void testCacheNames() throws Exception {
-        try {
-            startGridsMultiThreaded(2);
+        startGridsMultiThreaded(2);
 
-            Collection<String> names = grid(0).cacheNames();
+        Collection<String> names = grid(0).cacheNames();
 
-            assertEquals(3, names.size());
+        assertEquals(3, names.size());
 
-            for (String name : names)
-                assertTrue(name.equals(CACHE_NAME_WITH_SPECIAL_CHARACTERS_PARTITIONED)
-                        || name.equals(CACHE_NAME_WITH_SPECIAL_CHARACTERS_REPLICATED)
-                        || DEFAULT_CACHE_NAME.equals(name));
+        for (String name : names)
+            assertTrue(name.equals(CACHE_NAME_WITH_SPECIAL_CHARACTERS_PARTITIONED)
+                || name.equals(CACHE_NAME_WITH_SPECIAL_CHARACTERS_REPLICATED)
+                || DEFAULT_CACHE_NAME.equals(name));
 
-            client = true;
+        client = true;
 
-            Ignite client = startGrid(2);
+        Ignite client = startGrid(2);
 
-            names = client.cacheNames();
+        names = client.cacheNames();
 
-            assertEquals(3, names.size());
-        }
-        finally {
-            stopAllGrids();
-        }
+        assertEquals(3, names.size());
+    }
+
+    /**
+     * @throws Exception If failed.
+     */
+    @Test
+    public void testCachesOfSameGroupButWithDifferentRegions() throws Exception {
+        IgniteEx server = startGrid(0);
+
+        server.cluster().state(ACTIVE);
+
+        client = true;
+
+        IgniteEx client = startGrid(1);
+
+        awaitPartitionMapExchange();
+
+        client.getOrCreateCache(
+            new CacheConfiguration<>(DEFAULT_CACHE_NAME + 0)
+                .setGroupName(DEFAULT_CACHE_GROUP_NAME)
+        );
+
+        CacheConfiguration<Object, Object> cacheCfg = new CacheConfiguration<>(DEFAULT_CACHE_NAME + 1)
+            .setGroupName(DEFAULT_CACHE_GROUP_NAME)
+            .setDataRegionName(REGION_NAME);
+
+        assertThrows(
+            log,
+            () -> client.getOrCreateCache(cacheCfg),
+            CacheException.class,
+            "Data region mismatch for caches related to the same group"
+        );
+
+        assertThrows(
+            log,
+            () -> server.getOrCreateCache(cacheCfg),
+            CacheException.class,
+            "Data region mismatch for caches related to the same group"
+        );
     }
 
     /**
@@ -235,14 +286,7 @@ public class GridCacheConfigurationValidationSelfTest extends GridCommonAbstract
      * @param name Name of the grid which will have invalid configuration.
      */
     private void startInvalidGrid(String name) {
-        try {
-            startGrid(name);
-
-            assert false : "Exception should have been thrown.";
-        }
-        catch (Exception e) {
-            info("Caught expected exception: " + e);
-        }
+        assertThrows(log, () -> startGrid(name), Exception.class, null);
     }
 
     /**
