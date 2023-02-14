158c53bf0e613bcbda1a403d30f5f647c22d8139
Sergey Chugunov <sergey.chugunov@gmail.com>
2023-02-14 13:38:46 +0300

    GG-36055 Javadoc comments extended to make semantics on new methods clearer

--- modules/control-utility/src/main/java/org/apache/ignite/internal/commandline/util/TopologyUtils.java
+++ modules/control-utility/src/main/java/org/apache/ignite/internal/commandline/util/TopologyUtils.java
@@ -33,6 +33,10 @@ public final class TopologyUtils {
      * Obtains ID of coordinator node from the topology
      * visible through {@link GridClientCompute client compute} interface at the moment of method call.
      *
+     * The method relies on the discovery implementation detail that coordinator role is always assigned
+     * to the oldest server node in topology which means
+     * the {@link GridClientNode#order() order} of the node is minimal among all alive server nodes in topology.
+     *
      * @param compute {@link GridClientCompute} instance to obtain list of nodes in topology.
      * @return Id of coordinator node or null if topology is empty.
      * @throws GridClientException If client doesn't have an actual topology version.
@@ -48,8 +52,11 @@ public final class TopologyUtils {
     }
 
     /**
-     * Obtains any server node from the topology
+     * Obtains any connectable server node from the topology
      * visible through {@link GridClientCompute client compute} interface at the moment of method call.
+     *
+     * Method returns only a node which is connectable for the local client which means a direct p2p connection can be
+     * established from the client to the node.
      *
      * @param compute {@link GridClientCompute} instance to obtain list of nodes in topology.
      * @return {@link Optional} object containing any connectable server node.
