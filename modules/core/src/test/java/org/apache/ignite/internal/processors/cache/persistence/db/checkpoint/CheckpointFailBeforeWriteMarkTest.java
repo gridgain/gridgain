d6e2176694ac280e82121ebb217e01d38d6497dc
Kirill Tkalenko <tkalkirill@yandex.ru>
2023-03-22 10:31:36 +0300

    GG-36418 wip

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/persistence/db/checkpoint/CheckpointFailBeforeWriteMarkTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/persistence/db/checkpoint/CheckpointFailBeforeWriteMarkTest.java
@@ -86,7 +86,7 @@ public class CheckpointFailBeforeWriteMarkTest extends GridCommonAbstractTest {
 
         storageCfg.getDefaultDataRegionConfiguration()
             .setPersistenceEnabled(true)
-            .setMaxSize((isCompression ? 70 : 20) * 1024 * 1024);
+            .setMaxSize((isCompression ? 70 : 30) * 1024 * 1024);
 
         cfg.setDataStorageConfiguration(storageCfg)
             .setCacheConfiguration(new CacheConfiguration<>(DEFAULT_CACHE_NAME)
