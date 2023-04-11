387e68ffcb1533357bcf2a13a4c04501205111ad
Roman Puchkovskiy <roman.puchkovskiy@gmail.com>
2023-04-11 19:22:51 +0400

    Less logging

--- modules/core/src/main/java/org/apache/ignite/internal/managers/encryption/CacheGroupPageScanner.java
+++ modules/core/src/main/java/org/apache/ignite/internal/managers/encryption/CacheGroupPageScanner.java
@@ -233,7 +233,7 @@ public class CacheGroupPageScanner implements CheckpointListener {
             singleExecSvc.submit(grpScan);
 
             if (log.isInfoEnabled())
-                log.info("Scheduled reencryption [grpId=" + grpId + "]", new Exception("Tracking"));
+                log.info("Scheduled reencryption [grpId=" + grpId + "]");
 
             grps.put(grpId, grpScan);
 
