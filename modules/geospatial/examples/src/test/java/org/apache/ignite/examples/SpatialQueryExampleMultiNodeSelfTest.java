31ce8b99ccfd61f8d56ffc5d305515229d6b1836
Vadim Kolodin <vkolodin@gridgain.com>
2023-05-31 11:36:01 +0300

    GG-36707 Geospatial examples module

---
+++ modules/geospatial/examples/src/test/java/org/apache/ignite/examples/SpatialQueryExampleMultiNodeSelfTest.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2019 GridGain Systems, Inc. and Contributors.
+ *
+ * Licensed under the GridGain Community Edition License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.gridgain.com/products/software/community-edition/gridgain-community-edition-license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.ignite.examples;
+
+import org.apache.ignite.examples.datagrid.SpatialQueryExample;
+
+/**
+ * * Tests {@link SpatialQueryExample} in the multi node mode.
+ */
+public class SpatialQueryExampleMultiNodeSelfTest extends SpatialQueryExampleSelfTest {
+    /** */
+    static final String MODULE_PATH = System.getProperty("user.dir");
+
+    /** {@inheritDoc} */
+    @Override protected void beforeTest() throws Exception {
+        for (int i = 0; i < RMT_NODES_CNT; i++)
+            startGrid("node-" + i, MODULE_PATH + "/config/example-ignite.xml");
+    }
+}
