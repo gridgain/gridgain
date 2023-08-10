d29bb75aed953e9c21fe6731718b631c8c6fd286
Roman Puchkovskiy <roman.puchkovskiy@gmail.com>
2023-08-10 17:59:27 +0400

    GG-37101 / add new tests to a suite

--- modules/core/src/test/java/org/apache/ignite/testsuites/IgniteMarshallerSelfTestSuite.java
+++ modules/core/src/test/java/org/apache/ignite/testsuites/IgniteMarshallerSelfTestSuite.java
@@ -17,6 +17,7 @@
 package org.apache.ignite.testsuites;
 
 import org.apache.ignite.internal.direct.stream.v2.DirectByteBufferStreamImplV2ByteOrderSelfTest;
+import org.apache.ignite.internal.direct.stream.v2.DirectByteBufferStreamImplV2Test;
 import org.apache.ignite.internal.marshaller.optimized.OptimizedMarshallerEnumSelfTest;
 import org.apache.ignite.internal.marshaller.optimized.OptimizedMarshallerNodeFailoverTest;
 import org.apache.ignite.internal.marshaller.optimized.OptimizedMarshallerPooledSelfTest;
@@ -33,6 +34,7 @@ import org.apache.ignite.marshaller.jdk.GridJdkMarshallerSelfTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
+
 /**
  * Test suite for all marshallers.
  */
@@ -48,6 +50,7 @@ import org.junit.runners.Suite;
     OptimizedMarshallerNodeFailoverTest.class,
     OptimizedMarshallerSerialPersistentFieldsSelfTest.class,
     DirectByteBufferStreamImplV2ByteOrderSelfTest.class,
+    DirectByteBufferStreamImplV2Test.class,
     GridHandleTableSelfTest.class,
     OptimizedMarshallerPooledSelfTest.class,
     MarshallerEnumDeadlockMultiJvmTest.class
