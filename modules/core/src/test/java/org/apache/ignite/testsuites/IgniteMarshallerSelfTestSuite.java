6bd0e5a172b511de61db544375383a39e77a947f
Roman Puchkovskiy <roman.puchkovskiy@gmail.com>
2023-08-11 13:19:31 +0400

    Update modules/core/src/test/java/org/apache/ignite/testsuites/IgniteMarshallerSelfTestSuite.java
    Co-authored-by: Kirill Tkalenko <tkalkirill@yandex.ru>

--- modules/core/src/test/java/org/apache/ignite/testsuites/IgniteMarshallerSelfTestSuite.java
+++ modules/core/src/test/java/org/apache/ignite/testsuites/IgniteMarshallerSelfTestSuite.java
@@ -34,7 +34,6 @@ import org.apache.ignite.marshaller.jdk.GridJdkMarshallerSelfTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-
 /**
  * Test suite for all marshallers.
  */
