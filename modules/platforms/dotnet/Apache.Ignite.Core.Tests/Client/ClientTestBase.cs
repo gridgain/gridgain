94b1b64483d49f4c667ec560c9ee7e7f565b6ddb
Pavel Tupitsyn <ptupitsyn@apache.org>
2023-09-19 12:19:17 +0300

    Fix merge

--- modules/platforms/dotnet/Apache.Ignite.Core.Tests/Client/ClientTestBase.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests/Client/ClientTestBase.cs
@@ -83,9 +83,8 @@ namespace Apache.Ignite.Core.Tests.Client
             _enableSsl = enableSsl;
             _enablePartitionAwareness = enablePartitionAwareness;
             _enableServerListLogging = enableServerListLogging;
-            _serverListLoggerLevels =
-                serverListLoggerLevels ?? new[] { LogLevel.Trace, LogLevel.Debug, LogLevel.Warn, LogLevel.Error };
-            UseBinaryArray = useBinaryArray;
+            _serverListLoggerLevels = serverListLoggerLevels ??
+                                      new[] { LogLevel.Trace, LogLevel.Debug, LogLevel.Warn, LogLevel.Error };
         }
 
         /// <summary>
