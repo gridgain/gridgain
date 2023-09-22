gridgain/gridgain

commit f9f78a8a592711061cc0b07ab6aea4a0c2463d17

GG-36788 [IGNITE-19421] Added extended debug logging of client connections (#2883)

modules/platforms/dotnet/Apache.Ignite.Core.Tests/Config/Log/dotnet-log4j.xml
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 
 <!--
- Copyright 2019 GridGain Systems, Inc. and Contributors.
+ Copyright 2023 GridGain Systems, Inc. and Contributors.
 
  Licensed under the GridGain Community Edition License (the "License");
  you may not use this file except in compliance with the License.
@@ -16,127 +16,70 @@
  limitations under the License.
 -->
 
-<!DOCTYPE log4j:configuration PUBLIC "-//APACHE//DTD LOG4J 1.2//EN"
-    "http://logging.apache.org/log4j/1.2/apidocs/org/apache/log4j/xml/doc-files/log4j.dtd">
-<!--
-    Default log4j configuration for Ignite.
--->
-<log4j:configuration xmlns:log4j="http://jakarta.apache.org/log4j/" debug="false">
-    <!--
-        Logs System.out messages to console.
-    -->
-    <appender name="CONSOLE" class="org.apache.log4j.ConsoleAppender">
-        <!-- Log to STDOUT. -->
-        <param name="Target" value="System.out"/>
+<Configuration>
+    <Appenders>
+        <Console name="CONSOLE" target="SYSTEM_OUT">
+            <LevelRangeFilter minLevel="INFO" maxLevel="DEBUG"/>
+            <PatternLayout pattern="[%d{dd-MM-yyyy HH:mm:ss}][%-5p][%t][%c{1}] %m%n"/>
+        </Console>
 
-        <!-- Log from DEBUG and higher. -->
-        <param name="Threshold" value="DEBUG"/>
+        <Console name="CONSOLE_ERR" target="SYSTEM_ERR">
+            <PatternLayout pattern="[%d{dd-MM-yyyy HH:mm:ss}][%-5p][%t][%c{1}] %m%n"/>
+        </Console>
 
-        <!-- The default pattern: Date Priority [Category] Message\n -->
-        <layout class="org.apache.log4j.PatternLayout">
-            <param name="ConversionPattern" value="[%d{dd-MM-yyyy HH:mm:ss}][%-5p][%t][%c{1}] %m%n"/>
-        </layout>
+        <RollingFile name="FILE"
+                     append="true"
+                     fileName="${sys:IGNITE_HOME}/work/log/dotnet-logger-test.log"
+                     filePattern="${sys:IGNITE_HOME}/work/log/dotnet-logger-test.log.%i">
+            <PatternLayout pattern="[%d{ISO8601}][%-5p][%t][%c{1}] %m%n"/>
+            <Policies>
+                <SizeBasedTriggeringPolicy size="10 MB"/>
+            </Policies>
+            <DefaultRolloverStrategy max="10"/>
+        </RollingFile>
+    </Appenders>
 
-        <!-- Do not log beyond INFO level. -->
-        <filter class="org.apache.log4j.varia.LevelRangeFilter">
-            <param name="levelMin" value="DEBUG"/>
-            <param name="levelMax" value="INFO"/>
-        </filter>
-    </appender>
+    <Loggers>
+        <!--
+            Uncomment to enable Ignite debugging.
+        -->
+        <!--
+            <Logger name="org.apache.ignite" level="DEBUG"/>
+        -->
 
-    <!--
-        Logs all System.err messages to console.
-    -->
-    <appender name="CONSOLE_ERR" class="org.apache.log4j.ConsoleAppender">
-        <!-- Log to STDERR. -->
-        <param name="Target" value="System.err"/>
+        <!--
+            Uncomment this category to enable cache query execution tracing.
+        -->
+        <!--
+            <Logger name="org.apache.ignite.cache.queries" level="DEBUG"/>
+        -->
 
-        <!-- Log from WARN and higher. -->
-        <param name="Threshold" value="WARN"/>
+        <!--
+            Uncomment to enable DGC tracing.
+        -->
+        <!--
+            <Logger name="org.apache.ignite.grid.kernal.processors.cache.GridCacheDgcManager.trace" level="DEBUG"/>
+        -->
 
-        <!-- The default pattern: Date Priority [Category] Message\n -->
-        <layout class="org.apache.log4j.PatternLayout">
-            <param name="ConversionPattern" value="[%d{dd-MMM-yyyy HH:mm:ss}][%-5p][%t][%c{1}] %m%n"/>
-        </layout>
-    </appender>
+        <!--
+            Uncomment to disable courtesy notice.
+        -->
+        <!--
+           <Logger name="org.apache.ignite.CourtesyConfigNotice" level="OFF"/>
+        -->
 
-    <!--
-        Logs all output to specified file.
-        By default, the logging goes to IGNITE_HOME/work/log folder
+        <Logger name="org.springframework" level="WARN"/>
 
-        Note, this appender is disabled by default.
-        To enable, uncomment the section below and also FILE appender in the <root> element.
-    -->
-    <appender name="FILE" class="org.apache.log4j.RollingFileAppender">
-        <param name="Threshold" value="DEBUG"/>
-        <param name="File" value="${IGNITE_HOME}/work/log/dotnet-logger-test.log"/>
-        <param name="Append" value="true"/>
-        <param name="MaxFileSize" value="10MB"/>
-        <param name="MaxBackupIndex" value="10"/>
-        <layout class="org.apache.log4j.PatternLayout">
-            <param name="ConversionPattern" value="[%d{ISO8601}][%-5p][%t][%c{1}] %m%n"/>
-        </layout>
-    </appender>
+        <Logger name="org.eclipse.jetty" level="FATAL"/>
 
-    <!--
-        Uncomment to enable Ignite debugging.
-    -->
-    <!--
-    <category name="org.apache.ignite">
-        <level value="DEBUG"/>
-    </category>
-    -->
+        <Logger name="com.amazonaws" level="WARN"/>
 
-    <!--
-        Uncomment this category to enable cache
-        query execution tracing.
-    -->
-    <!--
-    <category name="org.apache.ignite.cache.queries">
-        <level value="DEBUG"/>
-    </category>
-    -->
+        <Logger name="org.apache.ignite.internal.processors.odbc.ClientListenerNioListener" level="TRACE"/>
 
-    <!--
-        Uncomment to enable DGC tracing.
-    -->
-    <!--
-    <category name="org.apache.ignite.grid.kernal.processors.cache.GridCacheDgcManager.trace">
-        <level value="DEBUG"/>
-    </category>
-    -->
-
-    <!--
-        Uncomment to disable courtesy notice.
-    -->
-    <!--
-    <category name="org.apache.ignite.CourtesyConfigNotice">
-        <level value="OFF"/>
-    </category>
-    -->
-
-    <category name="org.springframework">
-        <level value="WARN"/>
-    </category>
-
-    <category name="org.eclipse.jetty">
-        <level value="FATAL"/>
-    </category>
-
-    <category name="com.amazonaws">
-        <level value="WARN"/>
-    </category>
-
-    <!-- Default settings. -->
-    <root>
-        <!-- Print out all info by default. -->
-        <level value="DEBUG"/>
-
-        <!-- Append to console. -->
-        <appender-ref ref="CONSOLE"/>
-        <appender-ref ref="CONSOLE_ERR"/>
-
-        <!-- Uncomment to enable logging to a file. -->
-        <appender-ref ref="FILE"/>
-    </root>
-</log4j:configuration>
+        <Root level="DEBUG">
+            <AppenderRef ref="CONSOLE" level="DEBUG"/>
+            <AppenderRef ref="CONSOLE_ERR" level="WARN"/>
+            <AppenderRef ref="FILE" level="DEBUG"/>
+        </Root>
+    </Loggers>
+</Configuration>

modules/core/src/main/java/org/apache/ignite/internal/processors/odbc/ClientListenerMetrics.java
@@ -137,7 +137,7 @@ public class ClientListenerMetrics {
      * @param clientType Client type.
      * @return Label for a client.
      */
-    private String clientTypeLabel(byte clientType) {
+    public static String clientTypeLabel(byte clientType) {
         switch (clientType) {
             case ODBC_CLIENT:
                 return "odbc";

modules/core/src/main/java/org/apache/ignite/internal/processors/odbc/ClientListenerNioListener.java
@@ -52,6 +52,8 @@ import org.apache.ignite.internal.util.typedef.internal.U;
 import org.apache.ignite.plugin.security.SecurityException;
 import org.jetbrains.annotations.Nullable;
 
+import static org.apache.ignite.internal.processors.odbc.ClientListenerMetrics.clientTypeLabel;
+
 /**
  * Client message listener.
  */
@@ -194,10 +196,10 @@ public class ClientListenerNioListener extends GridNioServerListenerAdapter<Clie
         try {
             long startTime = 0;
 
-            if (log.isDebugEnabled()) {
+            if (log.isTraceEnabled()) {
                 startTime = System.nanoTime();
 
-                log.debug("Client request received [reqId=" + req.requestId() + ", addr=" +
+                log.trace("Client request received [reqId=" + req.requestId() + ", addr=" +
                     ses.remoteAddress() + ", req=" + req + ']');
             }
 
@@ -209,13 +211,13 @@ public class ClientListenerNioListener extends GridNioServerListenerAdapter<Clie
             try (OperationSecurityContext s = ctx.security().withContext(connCtx.securityContext())) {
                 ClientListenerResponse resp = handler.handle(req);
 
-                if (resp != null) {
-                    if (log.isDebugEnabled()) {
-                        long dur = (System.nanoTime() - startTime) / 1000;
+            if (resp != null) {
+                if (log.isTraceEnabled()) {
+                    long dur = (System.nanoTime() - startTime) / 1000;
 
-                        log.debug("Client request processed [reqId=" + req.requestId() + ", dur(mcs)=" + dur +
-                            ", resp=" + resp.status() + ']');
-                    }
+                    log.trace("Client request processed [reqId=" + req.requestId() + ", dur(mcs)=" + dur +
+                        ", resp=" + resp.status() + ']');
+                }
 
                     GridNioFuture<?> fut = ses.send(parser.encode(resp));
 
@@ -351,10 +353,26 @@ public class ClientListenerNioListener extends GridNioServerListenerAdapter<Clie
             connCtx.handler().writeHandshake(writer);
 
             metrics.onHandshakeAccept(clientType);
+
+            if (log.isDebugEnabled()) {
+                String login = connCtx.securityContext() == null ? null :
+                    connCtx.securityContext().subject().login().toString();
+
+                log.debug("Client handshake accepted [rmtAddr=" + ses.remoteAddress() +
+                    ", type=" + clientTypeLabel(clientType) + ", ver=" + ver.asString() +
+                    ", login=" + login + ", connId=" + connCtx.connectionId() + ']');
+            }
         }
         catch (IgniteAccessControlException | SecurityException authEx) {
             cancelHandshakeTimeout(ses);
             metrics.onFailedAuth();
+
+            if (log.isDebugEnabled()) {
+                log.debug("Client authentication failed [rmtAddr=" + ses.remoteAddress() +
+                    ", type=" + clientTypeLabel(clientType) + ", ver=" + ver.asString() +
+                    ", err=" + authEx.getMessage() + ']');
+            }
+
             writer.writeBoolean(false);
 
             writer.writeShort((short)0);
@@ -369,7 +387,8 @@ public class ClientListenerNioListener extends GridNioServerListenerAdapter<Clie
                 );
         }
         catch (IgniteCheckedException e) {
-            U.warn(log, "Error during handshake [rmtAddr=" + ses.remoteAddress() + ", msg=" + e.getMessage() + ']');
+            U.warn(log, "Error during handshake [rmtAddr=" + ses.remoteAddress() +
+                ", type=" + clientTypeLabel(clientType) + ", ver=" + ver.asString() + ", msg=" + e.getMessage() + ']');
 
             metrics.onGeneralReject();
 

modules/platforms/dotnet/Apache.Ignite.Core.Tests/Client/ClientTestBase.cs
@@ -83,7 +83,8 @@ namespace Apache.Ignite.Core.Tests.Client
             _enableSsl = enableSsl;
             _enablePartitionAwareness = enablePartitionAwareness;
             _enableServerListLogging = enableServerListLogging;
-            _serverListLoggerLevels = serverListLoggerLevels ?? new[] { LogLevel.Debug, LogLevel.Warn, LogLevel.Error };
+            _serverListLoggerLevels = serverListLoggerLevels ??
+                                      new[] { LogLevel.Trace, LogLevel.Debug, LogLevel.Warn, LogLevel.Error };
         }
 
         /// <summary>

commit e744f428cee3b00b5b18ae85c4ab2464278ec710

GG-36790 [IGNITE-18766] Fix incorrect id check in ClusterGroupAdapter.forNodeId (#2892)
Also revert previous changes to `dotnet-log4j.xml` to fix TestJavaLogger (broken in #2883)

modules/core/src/main/java/org/apache/ignite/internal/cluster/ClusterGroupAdapter.java
@@ -471,7 +471,7 @@ public class ClusterGroupAdapter implements ClusterGroupEx, Externalizable {
                 nodeIds = U.newHashSet(ids.length + 1);
 
                 for (UUID id0 : ids) {
-                    if (contains(id))
+                    if (contains(id0))
                         nodeIds.add(id0);
                 }
 

modules/platforms/dotnet/Apache.Ignite.Core.Tests/Config/Log/dotnet-log4j.xml
@@ -16,70 +16,127 @@
  limitations under the License.
 -->
 
-<Configuration>
-    <Appenders>
-        <Console name="CONSOLE" target="SYSTEM_OUT">
-            <LevelRangeFilter minLevel="INFO" maxLevel="DEBUG"/>
-            <PatternLayout pattern="[%d{dd-MM-yyyy HH:mm:ss}][%-5p][%t][%c{1}] %m%n"/>
-        </Console>
+<!DOCTYPE log4j:configuration PUBLIC "-//APACHE//DTD LOG4J 1.2//EN"
+        "http://logging.apache.org/log4j/1.2/apidocs/org/apache/log4j/xml/doc-files/log4j.dtd">
+<!--
+    Default log4j configuration for Ignite.
+-->
+<log4j:configuration xmlns:log4j="http://jakarta.apache.org/log4j/" debug="false">
+    <!--
+        Logs System.out messages to console.
+    -->
+    <appender name="CONSOLE" class="org.apache.log4j.ConsoleAppender">
+        <!-- Log to STDOUT. -->
+        <param name="Target" value="System.out"/>
 
-        <Console name="CONSOLE_ERR" target="SYSTEM_ERR">
-            <PatternLayout pattern="[%d{dd-MM-yyyy HH:mm:ss}][%-5p][%t][%c{1}] %m%n"/>
-        </Console>
+        <!-- Log from DEBUG and higher. -->
+        <param name="Threshold" value="DEBUG"/>
 
-        <RollingFile name="FILE"
-                     append="true"
-                     fileName="${sys:IGNITE_HOME}/work/log/dotnet-logger-test.log"
-                     filePattern="${sys:IGNITE_HOME}/work/log/dotnet-logger-test.log.%i">
-            <PatternLayout pattern="[%d{ISO8601}][%-5p][%t][%c{1}] %m%n"/>
-            <Policies>
-                <SizeBasedTriggeringPolicy size="10 MB"/>
-            </Policies>
-            <DefaultRolloverStrategy max="10"/>
-        </RollingFile>
-    </Appenders>
+        <!-- The default pattern: Date Priority [Category] Message\n -->
+        <layout class="org.apache.log4j.PatternLayout">
+            <param name="ConversionPattern" value="[%d{dd-MM-yyyy HH:mm:ss}][%-5p][%t][%c{1}] %m%n"/>
+        </layout>
 
-    <Loggers>
-        <!--
-            Uncomment to enable Ignite debugging.
-        -->
-        <!--
-            <Logger name="org.apache.ignite" level="DEBUG"/>
-        -->
+        <!-- Do not log beyond INFO level. -->
+        <filter class="org.apache.log4j.varia.LevelRangeFilter">
+            <param name="levelMin" value="DEBUG"/>
+            <param name="levelMax" value="INFO"/>
+        </filter>
+    </appender>
 
-        <!--
-            Uncomment this category to enable cache query execution tracing.
-        -->
-        <!--
-            <Logger name="org.apache.ignite.cache.queries" level="DEBUG"/>
-        -->
+    <!--
+        Logs all System.err messages to console.
+    -->
+    <appender name="CONSOLE_ERR" class="org.apache.log4j.ConsoleAppender">
+        <!-- Log to STDERR. -->
+        <param name="Target" value="System.err"/>
 
-        <!--
-            Uncomment to enable DGC tracing.
-        -->
-        <!--
-            <Logger name="org.apache.ignite.grid.kernal.processors.cache.GridCacheDgcManager.trace" level="DEBUG"/>
-        -->
+        <!-- Log from WARN and higher. -->
+        <param name="Threshold" value="WARN"/>
 
-        <!--
-            Uncomment to disable courtesy notice.
-        -->
-        <!--
-           <Logger name="org.apache.ignite.CourtesyConfigNotice" level="OFF"/>
-        -->
+        <!-- The default pattern: Date Priority [Category] Message\n -->
+        <layout class="org.apache.log4j.PatternLayout">
+            <param name="ConversionPattern" value="[%d{dd-MMM-yyyy HH:mm:ss}][%-5p][%t][%c{1}] %m%n"/>
+        </layout>
+    </appender>
 
-        <Logger name="org.springframework" level="WARN"/>
+    <!--
+        Logs all output to specified file.
+        By default, the logging goes to IGNITE_HOME/work/log folder
 
-        <Logger name="org.eclipse.jetty" level="FATAL"/>
+        Note, this appender is disabled by default.
+        To enable, uncomment the section below and also FILE appender in the <root> element.
+    -->
+    <appender name="FILE" class="org.apache.log4j.RollingFileAppender">
+        <param name="Threshold" value="DEBUG"/>
+        <param name="File" value="${IGNITE_HOME}/work/log/dotnet-logger-test.log"/>
+        <param name="Append" value="true"/>
+        <param name="MaxFileSize" value="10MB"/>
+        <param name="MaxBackupIndex" value="10"/>
+        <layout class="org.apache.log4j.PatternLayout">
+            <param name="ConversionPattern" value="[%d{ISO8601}][%-5p][%t][%c{1}] %m%n"/>
+        </layout>
+    </appender>
 
-        <Logger name="com.amazonaws" level="WARN"/>
+    <!--
+        Uncomment to enable Ignite debugging.
+    -->
+    <!--
+    <category name="org.apache.ignite">
+        <level value="DEBUG"/>
+    </category>
+    -->
 
-        <Logger name="org.apache.ignite.internal.processors.odbc.ClientListenerNioListener" level="TRACE"/>
+    <!--
+        Uncomment this category to enable cache
+        query execution tracing.
+    -->
+    <!--
+    <category name="org.apache.ignite.cache.queries">
+        <level value="DEBUG"/>
+    </category>
+    -->
 
-        <Root level="DEBUG">
-            <AppenderRef ref="CONSOLE" level="DEBUG"/>
-            <AppenderRef ref="CONSOLE_ERR" level="WARN"/>
-            <AppenderRef ref="FILE" level="DEBUG"/>
-        </Root>
-    </Loggers>
-</Configuration>
+    <!--
+        Uncomment to enable DGC tracing.
+    -->
+    <!--
+    <category name="org.apache.ignite.grid.kernal.processors.cache.GridCacheDgcManager.trace">
+        <level value="DEBUG"/>
+    </category>
+    -->
+
+    <!--
+        Uncomment to disable courtesy notice.
+    -->
+    <!--
+    <category name="org.apache.ignite.CourtesyConfigNotice">
+        <level value="OFF"/>
+    </category>
+    -->
+
+    <category name="org.springframework">
+        <level value="WARN"/>
+    </category>
+
+    <category name="org.eclipse.jetty">
+        <level value="FATAL"/>
+    </category>
+
+    <category name="com.amazonaws">
+        <level value="WARN"/>
+    </category>
+
+    <!-- Default settings. -->
+    <root>
+        <!-- Print out all info by default. -->
+        <level value="DEBUG"/>
+
+        <!-- Append to console. -->
+        <appender-ref ref="CONSOLE"/>
+        <appender-ref ref="CONSOLE_ERR"/>
+
+        <!-- Uncomment to enable logging to a file. -->
+        <appender-ref ref="FILE"/>
+    </root>
+</log4j:configuration>

modules/core/src/test/java/org/apache/ignite/internal/GridProjectionForCachesSelfTest.java
@@ -17,6 +17,7 @@
 package org.apache.ignite.internal;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 import org.apache.ignite.Ignite;
@@ -278,6 +279,19 @@ public class GridProjectionForCachesSelfTest extends GridCommonAbstractTest {
         catch (NullPointerException ignored) {
             // No-op.
         }
+    }
+
+    /**
+     * @throws Exception If failed.
+     */
+    @Test
+    public void testProjectionWithBadId() {
+        ClusterNode locNode = ignite.cluster().localNode();
+
+        ClusterGroup prj = ignite.cluster().forNodeId(UUID.randomUUID(), locNode.id());
+        Collection<ClusterNode> nodes = prj.nodes();
+
+        assertEquals(1, nodes.size());
     }
 
     /**