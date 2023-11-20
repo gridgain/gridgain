8488444ae9a26c913bd0278548625be2823183b6
Pavel Tupitsyn <ptupitsyn@apache.org>
2023-11-20 15:48:00 +0200

    GG-37670 Add handshake timeout check to GridNioSslHandler (#2959)

--- modules/core/src/main/java/org/apache/ignite/IgniteSystemProperties.java
+++ modules/core/src/main/java/org/apache/ignite/IgniteSystemProperties.java
@@ -135,6 +135,7 @@ import static org.apache.ignite.internal.util.IgniteUtils.DFLT_MBEAN_APPEND_CLAS
 import static org.apache.ignite.internal.util.StripedExecutor.DFLT_DATA_STREAMING_EXECUTOR_SERVICE_TASKS_STEALING_THRESHOLD;
 import static org.apache.ignite.internal.util.nio.GridNioRecoveryDescriptor.DFLT_NIO_RECOVERY_DESCRIPTOR_RESERVATION_TIMEOUT;
 import static org.apache.ignite.internal.util.nio.GridNioServer.DFLT_IO_BALANCE_PERIOD;
+import static org.apache.ignite.internal.util.nio.ssl.GridNioSslHandler.DFLT_SSL_HANDSHAKE_TIMEOUT_MS;
 import static org.apache.ignite.internal.util.tostring.GridToStringBuilder.DFLT_SENSITIVE_DATA_LOGGING;
 import static org.apache.ignite.internal.util.tostring.GridToStringBuilder.DFLT_TO_STRING_COLLECTION_LIMIT;
 import static org.apache.ignite.internal.util.tostring.GridToStringBuilder.DFLT_TO_STRING_MAX_LENGTH;
@@ -2207,6 +2208,11 @@ public final class IgniteSystemProperties {
 
     @SystemProperty(value = "Enables compliance with the JCache standard (JSR-107)", defaults = "false")
     public static final String IGNITE_JCACHE_COMPLIANCE = "IGNITE_JCACHE_COMPLIANCE";
+
+    /** SSL handshake timeout. */
+    @SystemProperty(value = "SSL handshake timeout, in milliseconds", type = Long.class,
+            defaults = DFLT_SSL_HANDSHAKE_TIMEOUT_MS + " milliseconds")
+    public static final String IGNITE_SSL_HANDSHAKE_TIMEOUT = "IGNITE_SSL_HANDSHAKE_TIMEOUT";
 
     /**
      * Enforces singleton.
