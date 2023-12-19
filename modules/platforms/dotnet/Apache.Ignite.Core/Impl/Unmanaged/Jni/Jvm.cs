0c2abd0dcca36c0c0ca6eb04664c59317ccbaab9
vmalin <vmalinovskiy@gridgain.com>
2023-12-19 18:08:17 +0200

    GG-38157 [GG-36846] jvmdefaults script doesn't support java 17

--- modules/platforms/dotnet/Apache.Ignite.Core/Impl/Unmanaged/Jni/Jvm.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core/Impl/Unmanaged/Jni/Jvm.cs
@@ -67,6 +67,7 @@ namespace Apache.Ignite.Core.Impl.Unmanaged.Jni
             "--add-opens=java.base/java.lang=ALL-UNNAMED",
             "--add-opens=java.base/java.lang.invoke=ALL-UNNAMED",
             "--add-opens=java.base/java.math=ALL-UNNAMED",
+            "--add-opens=java.base/java.time=ALL-UNNAMED",
             "--add-opens=java.base/sun.security.x509=ALL-UNNAMED",
             "--add-opens=java.sql/java.sql=ALL-UNNAMED"
         };
