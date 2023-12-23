2a29119a927cda9c3757b30b7cb6951b48ab63d6
Slava Koptilin <slava.koptilin@gmail.com>
2023-12-23 11:56:26 +0200

    GG-38180 Fix AssertionError on updating ttl

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/GridCacheMapEntry.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/GridCacheMapEntry.java
@@ -4137,25 +4137,32 @@ public abstract class GridCacheMapEntry extends GridMetadataAwareAdapter impleme
         assert cctx.shared().database().checkpointLockIsHeldByThread() :
             "Checkpoint lock must be held by the thread before acquiring entry lock";
 
+        if (cctx.queries().enabled() || cctx.mvccEnabled() || cctx.cacheObjectContext().compressionStrategy() != null || isNear() ||
+            cctx.group().persistenceEnabled()) {
+            // TODO https://ggsystems.atlassian.net/browse/GG-38158
+
+            // Fast update is not possible for this entry. Need to fallback to writing the whole entry.
+            //  - in general, enabling sql indices should not prevent from using fast update. Can be improved later.
+            //  - mvcc and compression are not supported for fast update yet.
+            //  - near entries only require update on the entry itself.
+            try {
+                unswap(null, true);
+            }
+            catch (IgniteCheckedException e) {
+                U.error(log, "Failed to update TTL: " + e, e);
+
+                return null;
+            }
+
+            updateTtl(ver, ttl);
+
+            return val;
+        }
+
         lockEntry();
 
         try {
             checkObsolete();
-
-            if (cctx.queries().enabled() || cctx.mvccEnabled() || cctx.cacheObjectContext().compressionStrategy() != null || isNear() ||
-                cctx.group().persistenceEnabled()) {
-                // TODO https://ggsystems.atlassian.net/browse/GG-38158
-
-                // Fast update is not possible for this entry. Need to fallback to writing the whole entry.
-                //  - in general, enabling sql indices should not prevent from using fast update. Can be improved later.
-                //  - mvcc and compression are not supported for fast update yet.
-                //  - near entries only require update on the entry itself.
-                unswap(null, true);
-
-                updateTtl(ver, ttl);
-
-                return val;
-            }
 
             CacheObject val = this.val;
             boolean isExpired = false;
