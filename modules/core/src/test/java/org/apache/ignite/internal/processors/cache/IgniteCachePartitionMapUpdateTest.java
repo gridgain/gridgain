a655434347b9235421417f5711a0ced915009440
Slava Koptilin <slava.koptilin@gmail.com>
2024-01-04 12:15:01 +0200

    GG-37995 Fix IgniteCachePartitionMapUpdateSafeLossPolicyTest.testRandom (#3025)

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/IgniteCachePartitionMapUpdateTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/IgniteCachePartitionMapUpdateTest.java
@@ -22,12 +22,12 @@ import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 import org.apache.ignite.cache.PartitionLossPolicy;
 import org.apache.ignite.cluster.ClusterNode;
 import org.apache.ignite.configuration.CacheConfiguration;
 import org.apache.ignite.configuration.IgniteConfiguration;
 import org.apache.ignite.internal.util.typedef.F;
-import org.apache.ignite.internal.util.typedef.internal.U;
 import org.apache.ignite.lang.IgnitePredicate;
 import org.apache.ignite.testframework.GridTestUtils;
 import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
@@ -164,38 +164,53 @@ public class IgniteCachePartitionMapUpdateTest extends GridCommonAbstractTest {
      */
     @Test
     public void testRandom() throws Exception {
-        Random rnd = new Random();
+        long seed = ThreadLocalRandom.current().nextLong();
 
-        final int NODE_CNT = GridTestUtils.SF.applyLB(10, 5);
+        Random rnd = new Random(seed);
 
-        for (int iter = 0; iter < 1; iter++) {
-            log.info("Iteration: " + iter);
+        int NODE_CNT = GridTestUtils.SF.applyLB(10, 5);
+        int numNodesHostCache1 = 0;
+        int numNodesHostCache2 = 0;
 
-            for (int i = 0; i < NODE_CNT; i++) {
-                cache1 = rnd.nextBoolean();
-                cache2 = rnd.nextBoolean();
+        for (int i = 0; i < NODE_CNT; i++) {
+            cache1 = rnd.nextBoolean();
+            cache2 = rnd.nextBoolean();
 
-                log.info("Start node [idx=" + i + ", cache1=" + cache1 + ", cache2=" + cache2 + ']');
+            numNodesHostCache1 += cache1 ? 1 : 0;
+            numNodesHostCache2 += cache2 ? 1 : 0;
 
-                startGrid(i);
-
-                awaitPartitionMapExchange();
+            // At least one node should be available for CACHE1.
+            if ((numNodesHostCache1 < 1) && (NODE_CNT - i <= 1)) {
+                cache1 = true;
+                numNodesHostCache1 += 1;
             }
 
-            LinkedHashSet<Integer> stopSeq = new LinkedHashSet<>();
-
-            while (stopSeq.size() != NODE_CNT)
-                stopSeq.add(rnd.nextInt(NODE_CNT));
-
-            log.info("Stop sequence: " + stopSeq + ", seed=" + U.field(rnd, "seed"));
-
-            for (Integer idx : stopSeq) {
-                log.info("Stop node: " + idx);
-
-                stopGrid(idx);
-
-                awaitPartitionMapExchange();
+            // At least two nodes should be available for CACHE2.
+            if ((numNodesHostCache2 < 2) && (NODE_CNT - i <= 2)) {
+                cache2 = true;
+                numNodesHostCache2 += 1;
             }
+
+            log.info("Start node [idx=" + i + ", cache1=" + cache1 + ", cache2=" + cache2 + ']');
+
+            startGrid(i);
+
+            awaitPartitionMapExchange();
+        }
+
+        LinkedHashSet<Integer> stopSeq = new LinkedHashSet<>();
+
+        while (stopSeq.size() != NODE_CNT)
+            stopSeq.add(rnd.nextInt(NODE_CNT));
+
+        log.info("Stop sequence: " + stopSeq + ", seed=" + seed);
+
+        for (Integer idx : stopSeq) {
+            log.info("Stop node: " + idx);
+
+            stopGrid(idx);
+
+            awaitPartitionMapExchange();
         }
     }
 
