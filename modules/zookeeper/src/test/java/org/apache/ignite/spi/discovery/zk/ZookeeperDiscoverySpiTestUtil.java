50bf519d0390b128033c5f9827388e7ad9d8f03c
sergeyuttsel <uttsel@gmail.com>
2024-01-09 10:18:08 +0300

    GG-37609 Updated zookeeper library from 3.4.14 to 3.8.3 to fix CVE-2023-44981.

--- modules/zookeeper/src/test/java/org/apache/ignite/spi/discovery/zk/ZookeeperDiscoverySpiTestUtil.java
+++ modules/zookeeper/src/test/java/org/apache/ignite/spi/discovery/zk/ZookeeperDiscoverySpiTestUtil.java
@@ -18,15 +18,24 @@ package org.apache.ignite.spi.discovery.zk;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 import org.apache.curator.test.InstanceSpec;
-import org.apache.ignite.spi.discovery.tcp.ipfinder.zk.curator.TestingCluster;
+import org.apache.curator.test.TestingCluster;
 import org.apache.ignite.IgniteException;
 
 /**
  * Utility to run regular Ignite tests with {@link org.apache.ignite.spi.discovery.zk.ZookeeperDiscoverySpi}.
  */
 public class ZookeeperDiscoverySpiTestUtil {
+    /** Property name for Zookeeper's election port bind retry attempts count. */
+    public static final String ZK_ELECTION_PORT_BIND_RETRY = "electionPortBindRetry";
+
+    /** Property name for Zookeeper's 'enable admin server' flag. */
+    public static final String ZK_ENABLE_ADMIN_SERVER = "admin.enableServer";
+
     /**
      * @param instances Number of instances in cluster.
      * @return Test cluster.
@@ -36,7 +45,7 @@ public class ZookeeperDiscoverySpiTestUtil {
     }
 
     /**
-     * @param instances Number of instances in.
+     * @param instances Number of instances in cluster.
      * @param firstInstanceIdx First instance index.
      * @return Test cluster.
      */
@@ -58,10 +67,26 @@ public class ZookeeperDiscoverySpiTestUtil {
                     throw new IgniteException("Failed to create directory for test Zookeeper server: " + file.getAbsolutePath());
             }
 
-            specs.add(new InstanceSpec(file, -1, -1, -1, true, -1, -1, 500));
+            specs.add(new InstanceSpec(file, -1, -1, -1, true, -1, -1, 500, clusterCustomProperties()));
         }
 
         return new TestingCluster(specs);
+    }
+
+    /**
+     *
+     */
+    private static Map<String, Object> clusterCustomProperties() {
+        Map<String, Object> props = new HashMap<>();
+
+        // In container environment, especially in Kubernetes, this value should be increased or set to 0
+        // (infinite retry) to overcome issues related to DNS name resolving.
+        props.putIfAbsent(ZK_ELECTION_PORT_BIND_RETRY, "0");
+
+        // Disable the AdminServer
+        props.putIfAbsent(ZK_ENABLE_ADMIN_SERVER, "false");
+
+        return props;
     }
 
     /**
