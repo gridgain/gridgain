c70a03e0d687277c32bd7c6eff68bc2b34034b05
Pavel Tupitsyn <ptupitsyn@apache.org>
2024-02-22 09:04:20 +0200

    Fix testRetryPolicyConvertOpAllOperationsSupported

--- modules/core/src/test/java/org/apache/ignite/client/ReliabilityTest.java
+++ modules/core/src/test/java/org/apache/ignite/client/ReliabilityTest.java
@@ -352,7 +352,7 @@ public class ReliabilityTest extends AbstractThinClientTest {
 
         String nullOpsNames = nullOps.stream().map(Enum::name).collect(Collectors.joining(", "));
 
-        long expectedNullCnt = 19;
+        long expectedNullCnt = 21;
 
         String msg = nullOps.size()
                 + " operation codes do not have public equivalent. When adding new codes, update ClientOperationType too. Missing ops: "
