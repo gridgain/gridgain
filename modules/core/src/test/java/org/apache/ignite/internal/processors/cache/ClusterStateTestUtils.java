15e6619b0845e6d50184aed6a357b1a7ec641a6d
Joseph Danrev Castroverde <castrojd87@gmail.com>
2024-03-14 17:02:49 -0400

    GG-37140 Update test to assert IllegalStateException

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/ClusterStateTestUtils.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/ClusterStateTestUtils.java
@@ -86,7 +86,7 @@ public class ClusterStateTestUtils {
                     assertNotNull(assertThrows(
                         log,
                         () -> crd.cache(cacheName),
-                        IgniteException.class,
+                        IllegalStateException.class,
                         "Can not perform the operation because the cluster is inactive. Note, that the cluster is considered inactive by default if Ignite Persistent Store is used to let all the nodes join the cluster. To activate the cluster call Ignite.active(true)."
                     ));
 
