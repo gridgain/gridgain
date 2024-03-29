6339b399493d8cd1cae75a21028f0d1a9f6cec74
Eduard Rakhmankulov <erixonich@gmail.com>
2024-03-29 16:33:40 +0300

    GG-38421 made common map with failed addresses with the reasons for clarifications in the logs

--- modules/core/src/main/java/org/apache/ignite/spi/communication/tcp/internal/GridNioServerWrapper.java
+++ modules/core/src/main/java/org/apache/ignite/spi/communication/tcp/internal/GridNioServerWrapper.java
@@ -345,13 +345,12 @@ public class GridNioServerWrapper {
             );
         }
 
-        Set<InetSocketAddress> failedAddrsSet = new HashSet<>();
-        Set<InetSocketAddress> timeoutAddrs = new HashSet<>();
+        Map<InetSocketAddress,List<String>> failedAddrs = new HashMap<>();
         int skippedAddrs = 0;
 
         for (InetSocketAddress addr : addrs) {
             if (addr.isUnresolved()) {
-                failedAddrsSet.add(addr);
+                failedAddrs.computeIfAbsent(addr, k -> new ArrayList<>()).add("Unresolved addr");
 
                 continue;
             }
@@ -513,7 +512,7 @@ public class GridNioServerWrapper {
                         ses = null;
                     }
 
-                    timeoutAddrs.add(addr);
+                    failedAddrs.computeIfAbsent(addr, k -> new ArrayList<>()).add("Timeout exception:" + e.getMessage());
 
                     eRegistrySupplier.get().onException("Handshake timed out (will retry with increased timeout) [connTimeoutStrategy=" + connTimeoutStgy +
                         ", addr=" + addr + ']', e);
@@ -564,7 +563,8 @@ public class GridNioServerWrapper {
 
                     // check if timeout occurred in case of unrecoverable exception
                     if (connTimeoutStgy.checkTimeout()) {
-                        timeoutAddrs.add(addr);
+                        failedAddrs.computeIfAbsent(addr, k -> new ArrayList<>())
+                                .add("Timeout with other exception:" + e.getClass() + ": " + e.getMessage());
 
                         U.warn(log, "Connection timed out (will stop attempts to perform the connect) " +
                             "[node=" + node.id() + ", connTimeoutStgy=" + connTimeoutStgy +
@@ -587,7 +587,8 @@ public class GridNioServerWrapper {
 
                     // Inverse communication protocol works only for client nodes.
                     if (node.isClient() && isNodeUnreachableException(e))
-                        failedAddrsSet.add(addr);
+                        failedAddrs.computeIfAbsent(addr, k -> new ArrayList<>())
+                                .add("Exception:" + e.getClass() + ": " + e.getMessage());
 
                     if (isRecoverableException(e))
                         U.sleep(DFLT_RECONNECT_DELAY);
@@ -624,9 +625,8 @@ public class GridNioServerWrapper {
             // inverse connection so no point in throwing NodeUnreachableException
             if (!cfg.usePairedConnections() || !Boolean.TRUE.equals(node.attribute(attrs.pairedConnection()))) {
                 if (!(Thread.currentThread() instanceof IgniteDiscoveryThread) && locNodeIsSrv) {
-                    if (node.isClient() && (addrs.size() - skippedAddrs == failedAddrsSet.size() + timeoutAddrs.size())) {
-                        String msg = "Failed to connect to all addresses of node " + node.id() + ": "
-                                + "failedAddrsSet=" + failedAddrsSet + "timeoutAddrs=" + timeoutAddrs +
+                    if (node.isClient() && (addrs.size() - skippedAddrs == failedAddrs.size())) {
+                        String msg = "Failed to connect to all addresses of node " + node.id() + ": " + failedAddrs +
                             "; inverse connection will be requested.";
 
                         throw new NodeUnreachableException(msg);
