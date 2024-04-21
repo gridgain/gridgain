42af3a2c37c06b38091c5e539c1316805210bfd0
Alexey Plotnik <plotnik.alexey@gmail.com>
2024-04-21 10:10:21 +1000

    GG-34040 Added cache names validation

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/distributed/CacheDirectoryNameTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/distributed/CacheDirectoryNameTest.java
@@ -97,11 +97,20 @@ public class CacheDirectoryNameTest extends GridCommonAbstractTest {
                 cfg.setName(name);
 
             if (persistenceEnabled) {
-                assertThrows(log, () -> srv.createCache(cfg), IllegalArgumentException.class,
-                    "Cache start failed. Cache or group name contains the characters that are not allowed in file names");
+                if (checkGroup)
+                    assertThrows(log, () -> srv.createCache(cfg), IllegalArgumentException.class,
+                        "Cache start failed. Cache or group name contains the characters that are not allowed in file names");
+                else
+                    assertThrows(log, () -> srv.createCache(cfg), IllegalArgumentException.class,
+                        "Ouch! Argument is invalid: Cache name cannot contain slashes (/), backslashes (\\), line separators (\\n), or null characters (\\0)");
             }
             else {
-                srv.createCache(cfg);
+                if (checkGroup)
+                    srv.createCache(cfg);
+                else
+                    assertThrows(log, () -> srv.createCache(cfg), IllegalArgumentException.class,
+                        "Ouch! Argument is invalid: Cache name cannot contain slashes (/), backslashes (\\), line separators (\\n), or null characters (\\0)");
+
                 srv.destroyCache(cfg.getName());
             }
         }
