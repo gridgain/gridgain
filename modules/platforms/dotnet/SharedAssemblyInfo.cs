7b7007dfc5195a6c761f3377f12602334e4f1c60
GridGain Teamcity <ggtc@gridgain.com>
2024-05-09 08:58:26 +0000

    8.9.5 release

--- modules/platforms/dotnet/SharedAssemblyInfo.cs
+++ modules/platforms/dotnet/SharedAssemblyInfo.cs
@@ -23,6 +23,6 @@ using System.Reflection;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("8.9.127.6496")]
-[assembly: AssemblyFileVersion("8.9.127.6496")]
-[assembly: AssemblyInformationalVersion("8.9.127")]
+[assembly: AssemblyVersion("8.9.5.3105")]
+[assembly: AssemblyFileVersion("8.9.5.3105")]
+[assembly: AssemblyInformationalVersion("8.9.5")]
