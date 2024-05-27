1652171254b0878674551aef4bed115f62f1b577
Slava Koptilin <slava.koptilin@gmail.com>
2024-05-27 18:28:23 +0300

    GG-39314 Fixed an issue that could lead to skipping keys during reconciliation (#3180)

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/checker/processor/PartitionReconciliationFullFixStressTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/checker/processor/PartitionReconciliationFullFixStressTest.java
@@ -45,7 +45,7 @@ public class PartitionReconciliationFullFixStressTest extends PartitionReconcili
      * Makes different variations of input params.
      */
     @Parameterized.Parameters(
-        name = "atomicity = {0}, partitions = {1}, fixModeEnabled = {2}, repairAlgorithm = {3}, parallelism = {4}")
+        name = "atomicity = {0}, partitions = {1}, fixModeEnabled = {2}, repairAlgorithm = {3}, parallelism = {4}, batchSize = {5}")
     public static List<Object[]> parameters() {
         ArrayList<Object[]> params = new ArrayList<>();
 
@@ -58,11 +58,13 @@ public class PartitionReconciliationFullFixStressTest extends PartitionReconcili
         for (CacheAtomicityMode atomicityMode : atomicityModes) {
             for (int parts : partitions)
                 for (RepairAlgorithm repairAlgorithm : repairAlgorithms)
-                    params.add(new Object[] {atomicityMode, parts, true, repairAlgorithm, 4});
+                    params.add(new Object[] {atomicityMode, parts, true, repairAlgorithm, 4, 1000});
+
+            params.add(new Object[] {atomicityMode, partitions[1], true, repairAlgorithms[0], 4, 10});
         }
 
-        params.add(new Object[] {CacheAtomicityMode.ATOMIC, 1, true, MAJORITY, 1});
-        params.add(new Object[] {CacheAtomicityMode.TRANSACTIONAL, 32, true, REMOVE, 1});
+        params.add(new Object[] {CacheAtomicityMode.ATOMIC, 1, true, MAJORITY, 1, 1000});
+        params.add(new Object[] {CacheAtomicityMode.TRANSACTIONAL, 32, true, REMOVE, 1, 1000});
 
         return params;
     }
@@ -72,8 +74,8 @@ public class PartitionReconciliationFullFixStressTest extends PartitionReconcili
      *
      * @throws Exception If failed.
      */
-    @Override @Test
-    public void testReconciliationOfColdKeysUnderLoad() throws Exception {
+    @Test
+    @Override public void testReconciliationOfColdKeysUnderLoad() throws Exception {
         IgniteCache<Integer, String> clientCache = client.cache(DEFAULT_CACHE_NAME);
 
         GridCacheContext[] nodeCacheCtxs = new GridCacheContext[NODES_CNT];
@@ -110,7 +112,7 @@ public class PartitionReconciliationFullFixStressTest extends PartitionReconcili
             }
         }, 6, "rand-loader");
 
-        ReconciliationResult res = partitionReconciliation(ig, fixMode, repairAlgorithm, parallelism, DEFAULT_CACHE_NAME);
+        ReconciliationResult res = partitionReconciliation(ig, fixMode, repairAlgorithm, parallelism, batchSize, DEFAULT_CACHE_NAME);
 
         log.info(">>>> Partition reconciliation finished");
 
