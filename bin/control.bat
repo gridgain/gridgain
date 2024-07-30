bab504ab5010a805d873977bf46b8099e5526ae1
dyang2285 <dan.yang.2285@gmail.com>
2024-07-30 03:18:25 -0400

    Update control.bat to handle JDK 17

--- bin/control.bat
+++ bin/control.bat
@@ -174,6 +174,33 @@ if %JAVA_VERSION% GEQ 9 (
     set CONTROL_JVM_OPTS=%CONTROL_JVM_OPTS% --add-opens=java.base/java.nio=ALL-UNNAMED
 )
 
+if %java_version% GEQ 17 (
+    rem For JDK 17 and above, add necessary opens
+    set value=
+    --add-opens=java.base/jdk.internal.access=ALL-UNNAMED
+    --add-opens=java.base/jdk.internal.misc=ALL-UNNAMED
+    --add-opens=java.base/sun.nio.ch=ALL-UNNAMED
+    --add-opens=java.management/com.sun.jmx.mbeanserver=ALL-UNNAMED
+    --add-opens=jdk.internal.jvmstat/sun.jvmstat.monitor=ALL-UNNAMED
+    --add-opens=java.base/sun.reflect.generics.reflectiveObjects=ALL-UNNAMED
+    --add-opens=java.base/java.io=ALL-UNNAMED
+    --add-opens=java.base/java.net=ALL-UNNAMED
+    --add-opens=java.base/java.nio=ALL-UNNAMED
+    --add-opens=java.base/java.security.cert=ALL-UNNAMED
+    --add-opens=java.base/java.util=ALL-UNNAMED
+    --add-opens=java.base/java.util.concurrent=ALL-UNNAMED
+    --add-opens=java.base/java.util.concurrent.locks=ALL-UNNAMED
+    --add-opens=java.base/java.util.concurrent.atomic=ALL-UNNAMED
+    --add-opens=java.base/java.lang=ALL-UNNAMED
+    --add-opens=java.base/java.lang.invoke=ALL-UNNAMED
+    --add-opens=java.base/java.math=ALL-UNNAMED
+    --add-opens=java.base/java.time=ALL-UNNAMED
+    --add-opens=java.base/sun.security.ssl=ALL-UNNAMED
+    --add-opens=java.base/sun.security.x509=ALL-UNNAMED
+    --add-opens=java.sql/java.sql=ALL-UNNAMED
+    %current_value%
+)
+
 if defined JVM_OPTS (
     echo JVM_OPTS environment variable is set, but will not be used. To pass JVM options use CONTROL_JVM_OPTS
     echo JVM_OPTS=%JVM_OPTS%
