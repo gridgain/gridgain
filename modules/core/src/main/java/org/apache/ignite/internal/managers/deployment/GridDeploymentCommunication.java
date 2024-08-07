e89414f23ae49aea14029befabd4f2cd9bcd63fb
Ivan Bessonov <bessonov.ip@gmail.com>
2024-08-07 15:20:04 +0300

    GG-39128 Fix lambda classname parsing in deployment for Java 21+ (#3249)

--- modules/core/src/main/java/org/apache/ignite/internal/managers/deployment/GridDeploymentCommunication.java
+++ modules/core/src/main/java/org/apache/ignite/internal/managers/deployment/GridDeploymentCommunication.java
@@ -208,8 +208,7 @@ class GridDeploymentCommunication {
             // In case the class loader is ours - skip the check
             // since it was already performed before (and was successful).
             if (!(ldr instanceof GridDeploymentClassLoader)) {
-                // First check for @GridNotPeerDeployable annotation.
-                String clsName = req.resourceName().replace('/', '.');
+                String clsName = clsNameFromResourceName(req.resourceName());
 
                 try {
                     if (clsName.endsWith(CLASS_FILE_EXTENSION))
@@ -217,6 +216,7 @@ class GridDeploymentCommunication {
 
                     Class<?> cls = Class.forName(clsName, true, ldr);
 
+                    // First check for @GridNotPeerDeployable annotation.
                     if (U.getAnnotation(cls, IgniteNotPeerDeployable.class) != null) {
                         String errMsg = "Attempt to peer deploy class that has @IgniteNotPeerDeployable " +
                             "annotation: " + clsName;
@@ -291,6 +291,16 @@ class GridDeploymentCommunication {
         }
 
         sendResponse(nodeId, req.responseTopic(), res);
+    }
+
+    private static String clsNameFromResourceName(String reqResourceName) {
+        String clsName = reqResourceName.replace('/', '.');
+
+        // Java 21+ uses '/' in lambda class names, we must account for that.
+        if (!reqResourceName.contains("$$Lambda/"))
+            return clsName;
+
+        return clsName.replace("$$Lambda.", "$$Lambda/");
     }
 
     /** */
