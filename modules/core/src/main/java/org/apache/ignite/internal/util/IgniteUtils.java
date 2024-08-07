7137fad968213ac9bc24df51038c99ad22a5954b
ibessonov <bessonov.ip@gmail.com>
2024-08-07 12:48:27 +0300

    GG-39128 Fix lambda classname parsing in deployment for Java 21+
    Signed-off-by: ibessonov <bessonov.ip@gmail.com>

--- modules/core/src/main/java/org/apache/ignite/internal/util/IgniteUtils.java
+++ modules/core/src/main/java/org/apache/ignite/internal/util/IgniteUtils.java
@@ -10412,9 +10412,15 @@ public abstract class IgniteUtils {
      *      {@code null} if passed in name is not related to lambda.
      */
     @Nullable public static String lambdaEnclosingClassName(String clsName) {
-        int idx = clsName.indexOf("$$Lambda$");
+        int idx0 = clsName.indexOf("$$Lambda$"); // Java 8+
+        int idx1 = clsName.indexOf("$$Lambda/"); // Java 21+
 
-        return idx != -1 ? clsName.substring(0, idx) : null;
+        // If both are "-1".
+        if (idx0 == idx1)
+            return null;
+
+        // One of indexes is -1, other one is a valid value.
+        return clsName.substring(0, idx0 >= 0 ? idx0 : idx1);
     }
 
     /**
