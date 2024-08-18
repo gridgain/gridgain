08809057254a3946412ad3ad944d667ed170134d
Mark Jackson <mark.jackson@gridgain.com>
2024-08-18 19:05:10 -0400

    GG-39034 [IGNITE-18546] Add cache clear command to control.sh  (#3139)
    * GG-39034 [IGNITE-18546] Add cache clear command to control.sh
    
    Signed-off-by: Mark Jackson <mark.jackson@gridgain.com>
    
    * GG-39034 [IGNITE-18546] Add confirmation for clear command
    
    Signed-off-by: Mark Jackson <mark.jackson@gridgain.com>
    
    * GG-39034 fixed logging compatibility errors from AI port
    
    Signed-off-by: Mark Jackson <mark.jackson@gridgain.com>
    
    * GG-39034 updated to CE licence
    
    * GG-39034 moved classes from org.apache.ignite.internal.processors.cache to org.apache.ignite.internal.visor.cache
    
    * GG-39034 added classes to classpath
    
    * GG-39034 switched VisorDataTransferObject to IgniteDataTransferObject
    
    * GG-39034 annotated ignite instance directly
    
    * GG-39034 changed cache clearing to now happens async
    
    * GG-39034 task updated to return the cleared and non-existent caches
    
    * GG-39034 corrected spelling mistakes in class names
    
    * GG-39034 updated test to pass security checks
    
    * GG-39034 ACTUALLY fixed the test to pass security checks
    
    * GG-39034 fixed code style errors
    
    * GG-39034 changed cache names to better match the rest of the tests
    
    * GG-39034 switched a second instance of VisorDataTransferObject to IgniteDataTransferObject
    
    ---------
    
    Signed-off-by: Mark Jackson <mark.jackson@gridgain.com>
    Co-authored-by: Maksim Timonin <timonin.maxim@gmail.com>

---
+++ modules/core/src/main/java/org/apache/ignite/internal/visor/cache/ClearCachesTask.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright 2024 GridGain Systems, Inc. and Contributors.
+ *
+ * Licensed under the GridGain Community Edition License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.gridgain.com/products/software/community-edition/gridgain-community-edition-license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.ignite.internal.visor.cache;
+
+import org.apache.ignite.Ignite;
+import org.apache.ignite.IgniteCache;
+import org.apache.ignite.IgniteException;
+import org.apache.ignite.compute.ComputeJobContext;
+import org.apache.ignite.internal.processors.task.GridInternal;
+import org.apache.ignite.internal.util.future.GridCompoundFuture;
+import org.apache.ignite.internal.util.future.GridFutureAdapter;
+import org.apache.ignite.internal.visor.VisorJob;
+import org.apache.ignite.internal.visor.VisorOneNodeTask;
+import org.apache.ignite.resources.IgniteInstanceResource;
+import org.apache.ignite.resources.JobContextResource;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/** Clears specified caches. */
+@GridInternal
+public class ClearCachesTask extends VisorOneNodeTask<ClearCachesTaskArg, ClearCachesTaskResult> {
+    /** */
+    private static final long serialVersionUID = 0L;
+
+    /** */
+    @Override protected VisorJob<ClearCachesTaskArg, ClearCachesTaskResult> job(ClearCachesTaskArg arg) {
+        return new ClearCacheJob(arg, debug);
+    }
+
+    /** Job clears specified caches. */
+    private static class ClearCacheJob extends VisorJob<ClearCachesTaskArg, ClearCachesTaskResult> {
+        /** */
+        private static final long serialVersionUID = 0L;
+
+        /** Local Ignite instance. */
+        @IgniteInstanceResource
+        private Ignite ignite;
+
+        private GridCompoundFuture opFut;
+
+        @JobContextResource
+        private ComputeJobContext jobCtx;
+
+        private List<String> clearedCaches = new ArrayList<>();
+        private List<String> nonExistentCaches = new ArrayList<>();
+
+        /** */
+        private ClearCacheJob(ClearCachesTaskArg arg, boolean debug) {
+            super(arg, debug);
+        }
+
+        /** {@inheritDoc} */
+        @Override protected ClearCachesTaskResult run(@Nullable ClearCachesTaskArg arg) throws IgniteException {
+            if (opFut == null) {
+                opFut = new GridCompoundFuture<>();
+
+                for (String cache: arg.caches()) {
+                    IgniteCache<?, ?> ignCache = ignite.cache(cache);
+
+                    if (ignCache == null) {
+                        nonExistentCaches.add(cache);
+                        continue;
+                    }
+                    else {
+                        GridFutureAdapter<?> fut = new GridFutureAdapter<>();
+
+                        ignCache.clearAsync().listen(asyncClearFut -> {
+                            try {
+                                asyncClearFut.get();
+                            }
+                            catch (IgniteException e) {
+                                fut.onDone(e);
+
+                                return;
+                            }
+
+                            clearedCaches.add(cache);
+
+                            fut.onDone();
+                        });
+
+                        opFut.add(fut);
+                    }
+                }
+
+                jobCtx.holdcc();
+
+                opFut.listen(f -> jobCtx.callcc());
+
+                opFut.markInitialized();
+
+                return null;
+            }
+
+            assert opFut.isDone();
+
+            return new ClearCachesTaskResult(clearedCaches, nonExistentCaches);
+        }
+    }
+}
+
