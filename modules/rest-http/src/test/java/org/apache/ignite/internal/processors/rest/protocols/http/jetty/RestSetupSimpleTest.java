5c92cd9d7464469af258ddf30718d05aff2d242f
Ilya Korol <llivezking@gmail.com>
2024-10-13 17:35:16 +0300

    GG-39103 Add couple of tests for destcache and clear REST endpoints

--- modules/rest-http/src/test/java/org/apache/ignite/internal/processors/rest/protocols/http/jetty/RestSetupSimpleTest.java
+++ modules/rest-http/src/test/java/org/apache/ignite/internal/processors/rest/protocols/http/jetty/RestSetupSimpleTest.java
@@ -18,12 +18,19 @@ package org.apache.ignite.internal.processors.rest.protocols.http.jetty;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
+import org.apache.ignite.Ignite;
+import org.apache.ignite.IgniteCache;
+import org.apache.ignite.IgniteEvents;
+import org.apache.ignite.cache.CachePeekMode;
 import org.apache.ignite.configuration.ConnectorConfiguration;
 import org.apache.ignite.configuration.IgniteConfiguration;
+import org.apache.ignite.events.EventType;
 import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
 import org.junit.Test;
 
@@ -34,11 +41,26 @@ public class RestSetupSimpleTest extends GridCommonAbstractTest {
     /** Jetty port. */
     private static final int JETTY_PORT = 8080;
 
+    /** Mapper */
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+
+    /** Auxiliar type reference for reading HTTP responses */
+    private static final TypeReference<Map<String, Object>> RESPONSE_TYPE = new TypeReference<Map<String, Object>>() {
+        //
+    };
+
     /** {@inheritDoc} */
     @Override protected IgniteConfiguration getConfiguration(String igniteInstanceName) throws Exception {
         IgniteConfiguration configuration = super.getConfiguration(igniteInstanceName);
 
         configuration.setConnectorConfiguration(new ConnectorConfiguration());
+
+        configuration.setIncludeEventTypes(
+            EventType.EVT_CACHE_ENTRY_DESTROYED
+            , EventType.EVT_CACHE_ENTRY_DESTROYED
+            , EventType.EVT_CACHE_ENTRY_EVICTED
+            , EventType.EVT_CACHE_OBJECT_REMOVED
+        );
 
         return configuration;
     }
@@ -58,15 +80,94 @@ public class RestSetupSimpleTest extends GridCommonAbstractTest {
         conn.connect();
 
         try (InputStreamReader streamReader = new InputStreamReader(conn.getInputStream())) {
-            ObjectMapper objMapper = new ObjectMapper();
-            Map<String, Object> myMap = objMapper.readValue(streamReader,
-                new TypeReference<Map<String, Object>>() {
-                });
+            Map<String, Object> myMap = readResponse(streamReader);
 
             log.info("Version command response is: " + myMap);
 
             assertTrue(myMap.containsKey("response"));
             assertEquals(0, myMap.get("successStatus"));
         }
+    }
+
+    /**
+     * Runs version command using GridJettyRestProtocol.
+     */
+    @Test
+    public void destroyClear() throws Exception {
+        final String CACHE_NAME = "foo";
+
+        Ignite grid = grid(0);
+
+        IgniteCache<Integer, Integer> cache = grid.getOrCreateCache(CACHE_NAME);
+
+        for (int i = 0; i < 10; i++) {
+            cache.put(i, i);
+        }
+
+        AtomicBoolean removalHappened = new AtomicBoolean(false);
+        grid.events().localListen(evt -> {
+                removalHappened.set(true);
+                return true;
+            }, EventType.EVT_CACHE_ENTRY_DESTROYED
+            , EventType.EVT_CACHE_ENTRY_DESTROYED
+            , EventType.EVT_CACHE_ENTRY_EVICTED
+            , EventType.EVT_CACHE_OBJECT_REMOVED);
+
+        URLConnection conn = new URL("http://localhost:" + JETTY_PORT + "/ignite?cmd=destcache&cacheName=" + CACHE_NAME).openConnection();
+
+        conn.connect();
+
+        try (InputStreamReader streamReader = new InputStreamReader(conn.getInputStream())) {
+            Map<String, Object> myMap = readResponse(streamReader);
+
+            log.info("Version command response is: " + myMap);
+
+            assertEquals(0, myMap.get("successStatus"));
+        }
+
+        assertNull(grid.cache(CACHE_NAME));
+        assertFalse(removalHappened.get());
+    }
+
+    @Test
+    public void cacheClear() throws Exception {
+        final String CACHE_NAME = "foo";
+
+        Ignite grid = grid(0);
+
+        IgniteCache<Integer, Integer> cache = grid.getOrCreateCache(CACHE_NAME);
+
+        for (int i = 0; i < 10; i++) {
+            cache.put(i, i);
+        }
+
+        AtomicBoolean removalHappened = new AtomicBoolean(false);
+        grid.events().localListen(evt -> {
+                removalHappened.set(true);
+                return true;
+            }, EventType.EVT_CACHE_ENTRY_DESTROYED
+            , EventType.EVT_CACHE_ENTRY_DESTROYED
+            , EventType.EVT_CACHE_ENTRY_EVICTED
+            , EventType.EVT_CACHE_OBJECT_REMOVED);
+
+        URLConnection conn = new URL("http://localhost:" + JETTY_PORT + "/ignite?cmd=clear&cacheName=" + CACHE_NAME).openConnection();
+
+        conn.connect();
+
+        try (InputStreamReader streamReader = new InputStreamReader(conn.getInputStream())) {
+            Map<String, Object> myMap = readResponse(streamReader);
+
+            log.info("Version command response is: " + myMap);
+
+            assertEquals(0, myMap.get("successStatus"));
+        }
+
+        assertNotNull(grid.cache(CACHE_NAME));
+        assertEquals(0, grid.cache(CACHE_NAME).size(CachePeekMode.PRIMARY));
+        assertFalse(removalHappened.get());
+    }
+
+    private static Map<String, Object> readResponse(InputStreamReader streamReader) throws IOException {
+        return MAPPER.readValue(streamReader, RESPONSE_TYPE);
     }
 }
