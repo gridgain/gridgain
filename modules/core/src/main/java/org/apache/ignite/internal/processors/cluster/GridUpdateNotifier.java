7a3e00d3c031c1fa62df5cb5466540ec6f569fa1
Pavel Tupitsyn <ptupitsyn@apache.org>
2024-11-14 18:46:34 +0200

    Update notifier endpoint

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cluster/GridUpdateNotifier.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cluster/GridUpdateNotifier.java
@@ -76,7 +76,7 @@ public class GridUpdateNotifier {
     private static final int WORKER_THREAD_SLEEP_TIME = 5000;
 
     /** Default url for request GridGain updates. */
-    public static final String DEFAULT_GRIDGAIN_UPDATES_URL = "https://www.gridgain.com/notifier/update";
+    public static final String DEFAULT_GRIDGAIN_UPDATES_URL = "https://www.gridgain.com/notifier/update/v2";
 
     /** Grid version. */
     private final String ver;
