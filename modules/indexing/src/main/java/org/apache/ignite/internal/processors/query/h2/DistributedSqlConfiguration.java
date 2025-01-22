865db7669d9eb821eed35b6ccc1068f2b83923c2
Andrey Novikov <anovikov@gridgain.com>
2025-01-22 20:40:49 +0700

    GG-41730 Fixed checkstyle.

--- modules/indexing/src/main/java/org/apache/ignite/internal/processors/query/h2/DistributedSqlConfiguration.java
+++ modules/indexing/src/main/java/org/apache/ignite/internal/processors/query/h2/DistributedSqlConfiguration.java
@@ -253,7 +253,7 @@ public class DistributedSqlConfiguration {
     public boolean isDisableCreateLuceneIndexForStringValueType() {
         Boolean ret = disableCreateLuceneIndexForStringValueType.get();
 
-        return ret != null ? ret :dfltDisableCreateLuceneIndexForString;
+        return ret != null ? ret : dfltDisableCreateLuceneIndexForString;
     }
 
     /** */
