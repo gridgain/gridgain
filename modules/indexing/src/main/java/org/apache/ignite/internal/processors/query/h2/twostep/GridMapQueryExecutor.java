26aa11fae279779d025cbdc2686197862bfd07a1
Dan Yang <dan.yang.2285@gmail.com>
2025-02-01 19:27:22 -0500

    Final fix on Jan 31 - Passed tests - Enhance query execution error logging

--- modules/indexing/src/main/java/org/apache/ignite/internal/processors/query/h2/twostep/GridMapQueryExecutor.java
+++ modules/indexing/src/main/java/org/apache/ignite/internal/processors/query/h2/twostep/GridMapQueryExecutor.java
@@ -210,13 +210,14 @@ public class GridMapQueryExecutor {
             .append("\nLocal Node ID: ").append(ctx.localNodeId())
             .append("\nRemote Node ID: ").append(remoteNodeId);
 
-
         if (error != null) {
             logMessage.append("\nError: ").append(error.getMessage());
         }
 
         // Log the error with the constructed message
+        System.out.println("🔥 ACTUAL LOGGER CALL: " + logMessage);// Jan 31 - Debugging statement
         log.error(logMessage.toString(), error);
+
     }
 
 
