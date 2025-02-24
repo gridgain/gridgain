990b5d062a37c1229692a2e915b3c29a9cac48b4
manini-zs <manini@zettascape.com>
2025-02-24 18:09:28 +0530

    Added a fix for convert

--- modules/core/src/main/java/org/apache/ignite/cache/QueryEntity.java
+++ modules/core/src/main/java/org/apache/ignite/cache/QueryEntity.java
@@ -706,6 +706,7 @@ public class QueryEntity implements Serializable {
 
                 vectIdx.setIndexType(idx.type());
                 vectIdx.setFieldNames(idx.fields(), true);
+                vectIdx.setSimilarityFunction(idx.similarityFunction());
 
                 idxs.add(vectIdx);
             }
