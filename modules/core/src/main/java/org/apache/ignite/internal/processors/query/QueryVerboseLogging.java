302137346a1287b8800c32dc67f5a3458f40e41d
Ilya Korol <llivezking@gmail.com>
2025-04-01 10:49:13 +0300

    GG-42854 Fix codestyle and licenses

--- modules/core/src/main/java/org/apache/ignite/internal/processors/query/QueryVerboseLogging.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/query/QueryVerboseLogging.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019 GridGain Systems, Inc. and Contributors.
+ *
+ * Licensed under the GridGain Community Edition License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.gridgain.com/products/software/community-edition/gridgain-community-edition-license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.ignite.internal.processors.query;
 
 import org.apache.ignite.IgniteLogger;
@@ -25,7 +41,6 @@ import java.util.regex.Pattern;
 
 import static java.time.temporal.ChronoField.HOUR_OF_DAY;
 import static java.time.temporal.ChronoField.MICRO_OF_SECOND;
-import static java.time.temporal.ChronoField.MILLI_OF_SECOND;
 import static java.time.temporal.ChronoField.MINUTE_OF_HOUR;
 import static java.time.temporal.ChronoField.SECOND_OF_MINUTE;
 
@@ -288,8 +303,7 @@ public class QueryVerboseLogging {
             }
         }
 
-        @Override
-        public String toString() {
+        @Override public String toString() {
             return report.toString();
         }
     }
