25d5787a22773fa08a78f35975f70904c7f4a66c
Vladimir Malinovskii <vmalin90@gmail.com>
2025-07-17 14:20:51 +0300

    GG-44284 CWE-78 in H2

--- modules/h2/src/main/java/org/gridgain/internal/h2/tools/Server.java
+++ modules/h2/src/main/java/org/gridgain/internal/h2/tools/Server.java
@@ -647,86 +647,9 @@ public class Server extends Tool implements Runnable, ShutdownHandler {
      * @param url the URL to open
      */
     public static void openBrowser(String url) throws Exception {
-        try {
-            String osName = StringUtils.toLowerEnglish(
-                    Utils.getProperty("os.name", "linux"));
-            Runtime rt = Runtime.getRuntime();
-            String browser = Utils.getProperty(SysProperties.H2_BROWSER, null);
-            if (browser == null) {
-                // under Linux, this will point to the default system browser
-                try {
-                    browser = System.getenv("BROWSER");
-                } catch (SecurityException se) {
-                    // ignore
-                }
-            }
-            if (browser != null) {
-                if (browser.startsWith("call:")) {
-                    browser = browser.substring("call:".length());
-                    Utils.callStaticMethod(browser, url);
-                } else if (browser.contains("%url")) {
-                    String[] args = StringUtils.arraySplit(browser, ',', false);
-                    for (int i = 0; i < args.length; i++) {
-                        args[i] = StringUtils.replaceAll(args[i], "%url", url);
-                    }
-                    rt.exec(args);
-                } else if (osName.contains("windows")) {
-                    rt.exec(new String[] { "cmd.exe", "/C",  browser, url });
-                } else {
-                    rt.exec(new String[] { browser, url });
-                }
-                return;
-            }
-            try {
-                Class<?> desktopClass = Class.forName("java.awt.Desktop");
-                // Desktop.isDesktopSupported()
-                Boolean supported = (Boolean) desktopClass.
-                    getMethod("isDesktopSupported").
-                    invoke(null, new Object[0]);
-                URI uri = new URI(url);
-                if (supported) {
-                    // Desktop.getDesktop();
-                    Object desktop = desktopClass.getMethod("getDesktop").
-                        invoke(null);
-                    // desktop.browse(uri);
-                    desktopClass.getMethod("browse", URI.class).
-                        invoke(desktop, uri);
-                    return;
-                }
-            } catch (Exception e) {
-                // ignore
-            }
-            if (osName.contains("windows")) {
-                rt.exec(new String[] { "rundll32", "url.dll,FileProtocolHandler", url });
-            } else if (osName.contains("mac") || osName.contains("darwin")) {
-                // Mac OS: to open a page with Safari, use "open -a Safari"
-                Runtime.getRuntime().exec(new String[] { "open", url });
-            } else {
-                String[] browsers = { "xdg-open", "chromium", "google-chrome",
-                        "firefox", "mozilla-firefox", "mozilla", "konqueror",
-                        "netscape", "opera", "midori" };
-                boolean ok = false;
-                for (String b : browsers) {
-                    try {
-                        rt.exec(new String[] { b, url });
-                        ok = true;
-                        break;
-                    } catch (Exception e) {
-                        // ignore and try the next
-                    }
-                }
-                if (!ok) {
-                    // No success in detection.
-                    throw new Exception(
-                            "Browser detection failed, and java property 'h2.browser' " +
-                            "and environment variable BROWSER are not set to a browser executable.");
-                }
-            }
-        } catch (Exception e) {
-            throw new Exception(
-                    "Failed to start a browser to open the URL " +
-            url + ": " + e.getMessage());
-        }
+        // The code of this method has been removed because it caused CWE-78.
+        // This method is inherited from H2 and is not needed for Gridgain.
+        // The method itself was left to avoid refactoring.
     }
 
     /**
