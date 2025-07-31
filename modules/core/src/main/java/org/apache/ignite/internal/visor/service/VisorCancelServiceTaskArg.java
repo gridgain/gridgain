cf496bd196421df7203ca478c8db234295866754
amashenkov <amashenkov@apache.org>
2025-07-31 16:01:11 +0300

    styles

--- modules/core/src/main/java/org/apache/ignite/internal/visor/service/VisorCancelServiceTaskArg.java
+++ modules/core/src/main/java/org/apache/ignite/internal/visor/service/VisorCancelServiceTaskArg.java
@@ -1,12 +1,11 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ * Copyright 2025 GridGain Systems, Inc. and Contributors.
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the GridGain Community Edition License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.gridgain.com/products/software/community-edition/gridgain-community-edition-license
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.apache.ignite.internal.visor.service;
 import java.io.IOException;
 import java.io.ObjectInput;
@@ -31,32 +31,38 @@ public class VisorCancelServiceTaskArg extends VisorDataTransferObject {
 
     /** Service name. */
     private String name;
+
     /**
      * Default constructor.
      */
     public VisorCancelServiceTaskArg() {
         // No-op.
     }
+
     /**
      * @param name Service name.
      */
     public VisorCancelServiceTaskArg(String name) {
         this.name = name;
     }
+
     /**
      * @return Service name.
      */
     public String getName() {
         return name;
     }
+
     /** {@inheritDoc} */
     @Override protected void writeExternalData(ObjectOutput out) throws IOException {
         U.writeString(out, name);
     }
+
     /** {@inheritDoc} */
     @Override protected void readExternalData(byte protoVer, ObjectInput in) throws IOException, ClassNotFoundException {
         name = U.readString(in);
     }
+
     /** {@inheritDoc} */
     @Override public String toString() {
         return S.toString(VisorCancelServiceTaskArg.class, this);
