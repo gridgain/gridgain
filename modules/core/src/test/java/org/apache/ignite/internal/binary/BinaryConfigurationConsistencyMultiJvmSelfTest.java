bb40a033af417d120723135b47772b157fb4f2b7
Aleksandr Polovtsev <alex.polovtcev@gmail.com>
2025-08-22 17:02:27 +0300

    GG-39046 Fix binary config validation when validation is skipped

---
+++ modules/core/src/test/java/org/apache/ignite/internal/binary/BinaryConfigurationConsistencyMultiJvmSelfTest.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2025 GridGain Systems, Inc. and Contributors.
+ *
+ * Licensed under the GridGain Community Edition License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.gridgain.com/products/software/community-edition/gridgain-community-edition-license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.ignite.internal.binary;
+
+import org.apache.ignite.configuration.BinaryConfiguration;
+import org.apache.ignite.configuration.IgniteConfiguration;
+import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.apache.ignite.IgniteSystemProperties.IGNITE_SKIP_CONFIGURATION_CONSISTENCY_CHECK;
+
+public class BinaryConfigurationConsistencyMultiJvmSelfTest extends GridCommonAbstractTest {
+    private int curNodeIdx;
+
+    @Override protected boolean isMultiJvm() {
+        return true;
+    }
+
+    @Override protected List<String> additionalRemoteJvmArgs() {
+        List<String> defaultArgs = super.additionalRemoteJvmArgs();
+
+        if (curNodeIdx == 0) {
+            return defaultArgs;
+        } else {
+            defaultArgs.add("-D" + IGNITE_SKIP_CONFIGURATION_CONSISTENCY_CHECK + "=true");
+        }
+
+        return defaultArgs;
+    }
+
+    /** {@inheritDoc} */
+    @Override protected IgniteConfiguration getConfiguration(String igniteInstanceName) throws Exception {
+        IgniteConfiguration cfg = super.getConfiguration(igniteInstanceName);
+
+        cfg.setBinaryConfiguration(new BinaryConfiguration());
+
+        return cfg;
+    }
+
+    /**
+     * Tests a situation when nodes having the same binary configuration but different
+     * {@link org.apache.ignite.IgniteSystemProperties#IGNITE_SKIP_CONFIGURATION_CONSISTENCY_CHECK} flag values try
+     * to join the cluster. It is expected that they must succeed.
+     */
+    @Test
+    public void testSkipConsistencyCheckMismatch() throws Exception {
+        startGrid(curNodeIdx++);
+
+        startGrid(curNodeIdx);
+    }
+}
