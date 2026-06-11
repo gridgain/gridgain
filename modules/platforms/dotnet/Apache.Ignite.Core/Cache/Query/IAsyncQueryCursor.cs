f6d8f5aba61289c2bd386153cc9da24837b6ce5a
Pavel Tupitsyn <ptupitsyn@apache.org>
2026-06-11 14:46:52 +0300

    GG-48493 .NET: IAsyncQueryCursor

---
+++ modules/platforms/dotnet/Apache.Ignite.Core/Cache/Query/IAsyncQueryCursor.cs
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2026 GridGain Systems, Inc. and Contributors.
+ *
+ * Licensed under the GridGain Community Edition License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.gridgain.com/products/software/community-edition/gridgain-community-edition-license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Apache.Ignite.Core.Cache.Query;
+
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+/// <summary>
+/// Asynchronous extension of <see cref="IQueryCursor{T}"/>,
+/// can be consumed in a non-blocking fashion with <c>await foreach</c>.
+/// </summary>
+/// <typeparam name="T">Element type.</typeparam>
+[SuppressMessage("Microsoft.Naming", "CA1710:IdentifiersShouldHaveCorrectSuffix")]
+public interface IAsyncQueryCursor<T> : IQueryCursor<T>, IAsyncEnumerable<T>
+{
+    // No-op.
+}
